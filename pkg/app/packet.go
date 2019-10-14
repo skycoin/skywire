ea48f3e69cc138af858b9c5e738eb1972f5c181d
Sir Darkrengarius <darkren92@gmail.com>
2019-10-14 08:28:36 +0300

    Remove old app module
    Former-commit-id: e6538b8fad174c3e8858b8e8df7f2c1a2e877e73 [formerly 3987ab88a46950f717a0740763c6136e18e3fdd2]
    Former-commit-id: e563bf3d5d615d0a45859aae3dee99033dfe6f2d

--- pkg/app/packet.go
+++
@@ -1,9 +0,0 @@
-package app
-
-import "github.com/skycoin/skywire/pkg/routing"
-
-// Packet represents message exchanged between App and Node.
-type Packet struct {
-	Desc    routing.RouteDescriptor `json:"desc"`
-	Payload []byte                  `json:"payload"`
-}
