a997d9a26321c10145bcdd53432f64c2064a2e1a
Senyoret1 <34079003+Senyoret1@users.noreply.github.com>
2019-10-28 20:23:21 -0400

    Small window alert

--- static/skywire-manager-src/src/app/app.component.ts
+++ static/skywire-manager-src/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Location } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   showFooter = false;
