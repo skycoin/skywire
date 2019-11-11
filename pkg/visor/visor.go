e1685c3edd9259ad84f530d22248d097cd84f255
Evan Lin <evanlinjin@pm.me>
2019-11-11 17:23:20 +1300

    Ensure node.StartApp always returns.
    Former-commit-id: 483e507643141ec329468a0312f1e66d5121feb9 [formerly b62c92a69be37d68a918d5cbb101bc0ace4d49d1]
    Former-commit-id: c40e6226c2fc1b843fb0d0ccd0de6e1249c9e8d2

--- pkg/visor/visor.go
+++ pkg/visor/visor.go
@@ -400,18 +400,25 @@ func (node *Node) Apps() []*AppState {
 
 // StartApp starts registered App.
 func (node *Node) StartApp(appName string) error {
-	for _, app := range node.appsConf {
-		if app.App == appName {
-			startCh := make(chan struct{})
-			go func(app AppConfig) {
-				if err := node.SpawnApp(&app, startCh); err != nil {
-					node.logger.Warnf("Failed to start app %s: %s", appName, err)
-				}
-			}(app)
-
-			<-startCh
-			return nil
+	for _, appC := range node.appsConf {
+		if appC.App != appName {
+			continue
 		}
+
+		done := make(chan struct{}, 1)
+		var err error
+
+		go func(appC AppConfig) {
+			if err = node.SpawnApp(&appC, done); err != nil {
+				done <- struct{}{}
+				node.logger.Warnf("Failed to start app %s: %s", appName, err)
+			}
+		}(appC)
+
+		<-done
+		close(done)
+
+		return err
 	}
 
 	return ErrUnknownApp
