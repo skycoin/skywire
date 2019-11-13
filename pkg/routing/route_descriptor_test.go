b9130e0a93e5cc58187f2423a6c56f65be113f98
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2019-11-13 19:28:52 +0300

    Tests for RouteDescriptor
    Former-commit-id: a5f0cf260119d4de963673a19863da3d52cce075 [formerly 4f0e72b03ea4a7b73fa5c4639710ecc638a15b95]
    Former-commit-id: 403ccfcc53511f5f3f2dec1ebebf514ad8ae8aa7

---
+++ pkg/routing/route_descriptor_test.go
@@ -0,0 +1,31 @@
+package routing
+
+import (
+	"testing"
+
+	"github.com/SkycoinProject/dmsg/cipher"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRouteDescriptor(t *testing.T) {
+	pk1, _ := cipher.GenerateKeyPair()
+	pk2, _ := cipher.GenerateKeyPair()
+	port1 := Port(1)
+	port2 := Port(2)
+
+	rd := NewRouteDescriptor(pk1, pk2, port1, port2)
+
+	require.Len(t, rd, routeDescriptorSize)
+	require.Equal(t, pk1, rd.SrcPK())
+	require.Equal(t, pk2, rd.DstPK())
+	require.Equal(t, port1, rd.SrcPort())
+	require.Equal(t, port2, rd.DstPort())
+
+	inverted := rd.Invert()
+
+	require.Len(t, inverted, routeDescriptorSize)
+	require.Equal(t, pk2, inverted.SrcPK())
+	require.Equal(t, pk1, inverted.DstPK())
+	require.Equal(t, port2, inverted.SrcPort())
+	require.Equal(t, port1, inverted.DstPort())
+}
