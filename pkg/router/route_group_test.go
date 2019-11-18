0e54231ef18f26a78a1453afbe4e888599a2459b
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2019-11-18 23:01:36 +0300

    Implement basic RouteGroup tests
    Former-commit-id: 66a6955a58c3d8f3aaddc281dda7d7f4771f8440 [formerly ffe80e0610149b8b625c3eb4214e502b8890abe4]
    Former-commit-id: a6fecd0f5f94a93ba1e718fa40990b50a690de12

--- pkg/router/route_group_test.go
+++ pkg/router/route_group_test.go
@@ -1,15 +1,110 @@
 package router
 
 import (
+	"context"
+	"net"
 	"testing"
+	"time"
 
 	"github.com/SkycoinProject/dmsg/cipher"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/net/nettest"
 
 	"github.com/SkycoinProject/skywire-mainnet/pkg/routing"
 )
 
 func TestNewRouteGroup(t *testing.T) {
+	_, rg := prepare()
+	require.NotNil(t, rg)
+}
+
+func TestRouteGroup_Close(t *testing.T) {
+	_, rg := prepare()
+	require.NotNil(t, rg)
+
+	require.False(t, rg.isClosed())
+	require.NoError(t, rg.Close())
+	require.True(t, rg.isClosed())
+}
+
+// TODO: implement better tests
+func TestRouteGroup_Read(t *testing.T) {
+	_, rg := prepare()
+	require.NotNil(t, rg)
+
+	buf := make([]byte, defaultReadChBufSize)
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(3*time.Second))
+	defer cancel()
+
+	errCh := make(chan error, 1)
+	go func() {
+		_, err := rg.Read(buf)
+		errCh <- err
+	}()
+
+	var err error
+	select {
+	case <-ctx.Done():
+		err = ctx.Err()
+	case err = <-errCh:
+	}
+	require.Equal(t, context.DeadlineExceeded, err)
+}
+
+// TODO: implement better tests
+func TestRouteGroup_Write(t *testing.T) {
+	_, rg := prepare()
+	require.NotNil(t, rg)
+
+	buf := make([]byte, defaultReadChBufSize)
+	_, err := rg.Write(buf)
+	require.Equal(t, ErrNoTransports, err)
+}
+
+func TestRouteGroup_LocalAddr(t *testing.T) {
+	desc, rg := prepare()
+	require.Equal(t, desc.Src(), rg.LocalAddr())
+}
+
+func TestRouteGroup_RemoteAddr(t *testing.T) {
+	desc, rg := prepare()
+	require.Equal(t, desc.Dst(), rg.RemoteAddr())
+}
+
+func TestRouteGroup_SetReadDeadline(t *testing.T) {
+	_, rg := prepare()
+	now := time.Now()
+
+	require.NoError(t, rg.SetReadDeadline(now))
+	require.Equal(t, now.UnixNano(), rg.readDeadline)
+}
+
+func TestRouteGroup_SetWriteDeadline(t *testing.T) {
+	_, rg := prepare()
+	now := time.Now()
+
+	require.NoError(t, rg.SetWriteDeadline(now))
+	require.Equal(t, now.UnixNano(), rg.writeDeadline)
+}
+
+func TestRouteGroup_SetDeadline(t *testing.T) {
+	_, rg := prepare()
+	now := time.Now()
+
+	require.NoError(t, rg.SetDeadline(now))
+	require.Equal(t, now.UnixNano(), rg.readDeadline)
+	require.Equal(t, now.UnixNano(), rg.writeDeadline)
+}
+
+func TestRouteGroup_TestConn(t *testing.T) {
+	mp := func() (c1, c2 net.Conn, stop func(), err error) {
+		// TODO: implement
+		return
+	}
+	nettest.TestConn(t, mp)
+}
+
+func prepare() (routing.RouteDescriptor, *RouteGroup) {
 	rt := routing.NewTable(routing.DefaultConfig())
 
 	pk1, _ := cipher.GenerateKeyPair()
@@ -19,9 +114,5 @@ func TestNewRouteGroup(t *testing.T) {
 	desc := routing.NewRouteDescriptor(pk1, pk2, port1, port2)
 
 	rg := NewRouteGroup(DefaultRouteGroupConfig(), rt, desc)
-	require.NotNil(t, rg)
-
-	require.False(t, rg.isClosed())
-
-	require.NoError(t, rg.Close())
+	return desc, rg
 }
