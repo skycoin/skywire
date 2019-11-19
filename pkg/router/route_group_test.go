1f22a3dc9f0f56f599f2b598dd5fcd414ba888df
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2019-11-19 18:10:17 +0300

    Improve RouteGroup tests
    Former-commit-id: 5e2d9f8cbd1210c351242c2716571a57d6012849

--- pkg/router/route_group_test.go
+++ pkg/router/route_group_test.go
@@ -17,12 +17,12 @@ import (
 )
 
 func TestNewRouteGroup(t *testing.T) {
-	_, rg := prepare()
+	rg := createRouteGroup()
 	require.NotNil(t, rg)
 }
 
 func TestRouteGroup_Close(t *testing.T) {
-	_, rg := prepare()
+	rg := createRouteGroup()
 	require.NotNil(t, rg)
 
 	require.False(t, rg.isClosed())
@@ -30,12 +30,13 @@ func TestRouteGroup_Close(t *testing.T) {
 	require.True(t, rg.isClosed())
 }
 
-// TODO: implement better tests
 func TestRouteGroup_Read(t *testing.T) {
-	msg := []byte("hello")
-	buf := make([]byte, len(msg))
+	msg1 := []byte("hello1")
+	msg2 := []byte("hello2")
+	buf1 := make([]byte, len(msg1))
+	buf2 := make([]byte, len(msg2))
 
-	_, rg1 := prepare()
+	rg1 := createRouteGroup()
 	require.NotNil(t, rg1)
 
 	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(3*time.Second))
@@ -43,7 +44,7 @@ func TestRouteGroup_Read(t *testing.T) {
 
 	errCh := make(chan error, 1)
 	go func() {
-		_, err := rg1.Read(buf)
+		_, err := rg1.Read(buf1)
 		errCh <- err
 	}()
 
@@ -56,15 +57,130 @@ func TestRouteGroup_Read(t *testing.T) {
 	require.Equal(t, context.DeadlineExceeded, err)
 	require.NoError(t, rg1.Close())
 
-	_, rg1 = prepare()
-	_, rg2 := prepare()
+	rg1 = createRouteGroup()
+	rg2 := createRouteGroup()
 
+	_, _, closeFunc := createTransports(t, rg1, rg2)
+	defer closeFunc()
+
+	go func() {
+		rg1.readCh <- msg1
+		rg2.readCh <- msg2
+	}()
+
+	n, err := rg1.Read(buf1)
+	require.NoError(t, err)
+	require.Equal(t, msg1, buf1)
+	require.Equal(t, len(msg1), n)
+
+	n, err = rg2.Read(buf2)
+	require.NoError(t, err)
+	require.Equal(t, msg2, buf2)
+	require.Equal(t, len(msg2), n)
+}
+
+func TestRouteGroup_Write(t *testing.T) {
+	msg1 := []byte("hello1")
+	msg2 := []byte("hello2")
+
+	rg1 := createRouteGroup()
+	require.NotNil(t, rg1)
+
+	_, err := rg1.Write(msg1)
+	require.Equal(t, ErrNoTransports, err)
+	require.NoError(t, rg1.Close())
+
+	rg1 = createRouteGroup()
+	rg2 := createRouteGroup()
+
+	m1, m2, closeFunc := createTransports(t, rg1, rg2)
+	defer closeFunc()
+
+	_, err = rg1.Write(msg1)
+	require.NoError(t, err)
+
+	_, err = rg2.Write(msg2)
+	require.NoError(t, err)
+
+	recv, err := m1.ReadPacket()
+	require.NoError(t, err)
+	require.Equal(t, msg2, recv.Payload())
+
+	recv, err = m2.ReadPacket()
+	require.NoError(t, err)
+	require.Equal(t, msg1, recv.Payload())
+}
+
+func TestRouteGroup_LocalAddr(t *testing.T) {
+	rg := createRouteGroup()
+	require.Equal(t, rg.desc.Src(), rg.LocalAddr())
+}
+
+func TestRouteGroup_RemoteAddr(t *testing.T) {
+	rg := createRouteGroup()
+	require.Equal(t, rg.desc.Dst(), rg.RemoteAddr())
+}
+
+func TestRouteGroup_SetReadDeadline(t *testing.T) {
+	rg := createRouteGroup()
+	now := time.Now()
+
+	require.NoError(t, rg.SetReadDeadline(now))
+	require.Equal(t, now.UnixNano(), rg.readDeadline)
+}
+
+func TestRouteGroup_SetWriteDeadline(t *testing.T) {
+	rg := createRouteGroup()
+	now := time.Now()
+
+	require.NoError(t, rg.SetWriteDeadline(now))
+	require.Equal(t, now.UnixNano(), rg.writeDeadline)
+}
+
+func TestRouteGroup_SetDeadline(t *testing.T) {
+	rg := createRouteGroup()
+	now := time.Now()
+
+	assert.NoError(t, rg.SetDeadline(now))
+	assert.Equal(t, now.UnixNano(), rg.readDeadline)
+	assert.Equal(t, now.UnixNano(), rg.writeDeadline)
+}
+
+func TestRouteGroup_TestConn(t *testing.T) {
+	mp := func() (c1, c2 net.Conn, stop func(), err error) {
+		c1 = createRouteGroup()
+		c2 = createRouteGroup()
+		stop = func() {
+			assert.NoError(t, c1.Close())
+			assert.NoError(t, c2.Close())
+		}
+		err = nil
+		return
+	}
+	nettest.TestConn(t, mp)
+}
+
+func createRouteGroup() *RouteGroup {
+	rt := routing.NewTable(routing.DefaultConfig())
+
+	pk1, _ := cipher.GenerateKeyPair()
+	pk2, _ := cipher.GenerateKeyPair()
+	port1 := routing.Port(1)
+	port2 := routing.Port(2)
+	desc := routing.NewRouteDescriptor(pk1, pk2, port1, port2)
+
+	rg := NewRouteGroup(DefaultRouteGroupConfig(), rt, desc)
+	return rg
+}
+
+func createTransports(t *testing.T, rg1, rg2 *RouteGroup) (m1, m2 *transport.Manager, closeFunc func()) {
 	tpDisc := transport.NewDiscoveryMock()
 	keys := snettest.GenKeyPairs(2)
-	nEnv := snettest.NewEnv(t, keys)
-	defer nEnv.Teardown()
 
-	_, _, tp1, tp2, err := transport.CreateTransportPair(tpDisc, keys, nEnv)
+	nEnv := snettest.NewEnv(t, keys)
+
+	m1, m2, tp1, tp2, err := transport.CreateTransportPair(tpDisc, keys, nEnv)
+	require.NoError(t, err)
 	require.NotNil(t, tp1)
 	require.NotNil(t, tp2)
 	require.NotNil(t, tp1.Entry)
@@ -83,81 +199,7 @@ func TestRouteGroup_Read(t *testing.T) {
 	rg2.tps = append(rg2.tps, tp2)
 	rg2.fwd = append(rg2.fwd, rule2)
 
-	_, err = rg1.Write(msg)
-	require.NoError(t, err)
-
-	_, err = rg2.Read(buf)
-	require.NoError(t, err)
-}
-
-// TODO: implement better tests
-func TestRouteGroup_Write(t *testing.T) {
-	_, rg := prepare()
-	require.NotNil(t, rg)
-
-	buf := make([]byte, defaultReadChBufSize)
-	_, err := rg.Write(buf)
-	require.Equal(t, ErrNoTransports, err)
-}
-
-func TestRouteGroup_LocalAddr(t *testing.T) {
-	desc, rg := prepare()
-	require.Equal(t, desc.Src(), rg.LocalAddr())
-}
-
-func TestRouteGroup_RemoteAddr(t *testing.T) {
-	desc, rg := prepare()
-	require.Equal(t, desc.Dst(), rg.RemoteAddr())
-}
-
-func TestRouteGroup_SetReadDeadline(t *testing.T) {
-	_, rg := prepare()
-	now := time.Now()
-
-	require.NoError(t, rg.SetReadDeadline(now))
-	require.Equal(t, now.UnixNano(), rg.readDeadline)
-}
-
-func TestRouteGroup_SetWriteDeadline(t *testing.T) {
-	_, rg := prepare()
-	now := time.Now()
-
-	require.NoError(t, rg.SetWriteDeadline(now))
-	require.Equal(t, now.UnixNano(), rg.writeDeadline)
-}
-
-func TestRouteGroup_SetDeadline(t *testing.T) {
-	_, rg := prepare()
-	now := time.Now()
-
-	assert.NoError(t, rg.SetDeadline(now))
-	assert.Equal(t, now.UnixNano(), rg.readDeadline)
-	assert.Equal(t, now.UnixNano(), rg.writeDeadline)
-}
-
-func TestRouteGroup_TestConn(t *testing.T) {
-	mp := func() (c1, c2 net.Conn, stop func(), err error) {
-		_, c1 = prepare()
-		_, c2 = prepare()
-		stop = func() {
-			assert.NoError(t, c1.Close())
-			assert.NoError(t, c2.Close())
-		}
-		err = nil
-		return
+	return m1, m2, func() {
+		nEnv.Teardown()
 	}
-	nettest.TestConn(t, mp)
-}
-
-func prepare() (routing.RouteDescriptor, *RouteGroup) {
-	rt := routing.NewTable(routing.DefaultConfig())
-
-	pk1, _ := cipher.GenerateKeyPair()
-	pk2, _ := cipher.GenerateKeyPair()
-	port1 := routing.Port(1)
-	port2 := routing.Port(2)
-	desc := routing.NewRouteDescriptor(pk1, pk2, port1, port2)
-
-	rg := NewRouteGroup(DefaultRouteGroupConfig(), rt, desc)
-	return desc, rg
 }
