3a14524e100508328581bdb8e5f45cc455afe7c2
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2019-12-11 22:10:45 +0300

    Cleanup router
    Former-commit-id: 3da2ef04956cacac6584a7a0884e048889279d54

--- pkg/util/atomicbool/atomicbool.go
+++ pkg/util/atomicbool/atomicbool.go
@@ -1,10 +1,11 @@
 package atomicbool
 
+// Copied from https://golang.org/src/internal/poll/fd_plan9.go#L14
+
 import (
 	"sync/atomic"
 )
 
-// Copied from https://golang.org/src/internal/poll/fd_plan9.go#L14
 type Bool int32
 
 func (b *Bool) IsSet() bool { return atomic.LoadInt32((*int32)(b)) != 0 }
