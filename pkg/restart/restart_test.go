8741693b3f6dc6e5c08b0845e592927b8ff8fb08
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2019-12-17 21:56:39 +0300

    Implement tests for visor restart from hypervisor
    Former-commit-id: f6a2c0e45f5ddaedadc5dbbf1f059d29bb31fb14

---
+++ pkg/restart/restart_test.go
@@ -0,0 +1,79 @@
+package restart
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/SkycoinProject/skycoin/src/util/logging"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCaptureContext(t *testing.T) {
+	cc, err := CaptureContext()
+	require.NoError(t, err)
+
+	wd, err := os.Getwd()
+	assert.NoError(t, err)
+
+	require.Equal(t, wd, cc.workingDirectory)
+	require.Equal(t, DefaultCheckDelay, cc.checkDelay)
+	require.Equal(t, os.Args, cc.args)
+	require.Nil(t, cc.log)
+	require.True(t, cc.needsExit)
+}
+
+func TestContext_RegisterLogger(t *testing.T) {
+	cc, err := CaptureContext()
+	require.NoError(t, err)
+	require.Nil(t, cc.log)
+
+	logger := logging.MustGetLogger("test")
+	cc.RegisterLogger(logger)
+	require.Equal(t, logger, cc.log)
+}
+
+func TestContext_Restart(t *testing.T) {
+	cc, err := CaptureContext()
+	require.NoError(t, err)
+	assert.NotZero(t, len(cc.args))
+
+	cc.workingDirectory = ""
+	cc.needsExit = false
+
+	t.Run("executable started", func(t *testing.T) {
+		cmd := "touch"
+		path := "/tmp/test_restart"
+		args := []string{cmd, path}
+		cc.args = args
+
+		assert.NoError(t, cc.Restart())
+		assert.NoError(t, os.Remove(path))
+	})
+
+	t.Run("bad args", func(t *testing.T) {
+		cmd := "bad_command"
+		args := []string{cmd}
+		cc.args = args
+
+		// TODO(nkryuchkov): Check if it works on Linux and Windows, if not then change the error text.
+		assert.EqualError(t, cc.Restart(), `exec: "bad_command": executable file not found in $PATH`)
+	})
+
+	t.Run("empty args", func(t *testing.T) {
+		cc.args = nil
+
+		assert.Equal(t, ErrMalformedArgs, cc.Restart())
+	})
+}
+
+func TestContext_SetCheckDelay(t *testing.T) {
+	cc, err := CaptureContext()
+	require.NoError(t, err)
+	require.Equal(t, DefaultCheckDelay, cc.checkDelay)
+
+	const oneSecond = 1 * time.Second
+	cc.SetCheckDelay(oneSecond)
+	require.Equal(t, oneSecond, cc.checkDelay)
+}
