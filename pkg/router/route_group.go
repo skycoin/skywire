daa81c0e8d2106bed3851ecdd17637d92af41084
Sir Darkrengarius <darkren92@gmail.com>
2019-12-20 20:20:56 +0300

    Reset route group timeout

--- pkg/router/route_group.go
+++ pkg/router/route_group.go
@@ -21,7 +21,7 @@ import (
 
 const (
 	defaultRouteGroupKeepAliveInterval = 1 * time.Minute
-	defaultRouteGroupIOTimeout         = 300 * time.Second
+	defaultRouteGroupIOTimeout         = 3 * time.Second
 	defaultReadChBufSize               = 1024
 )
 
