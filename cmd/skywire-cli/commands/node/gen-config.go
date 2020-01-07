94c18b71e6c0aae41688be402891dff9f546dd30
kifen <kifenraph@gmail.com>
2020-01-07 16:52:41 +0100

    enable STCP by default
    Former-commit-id: a0efeda82948d93ba2b8f6ea486877b2963bbc9c

--- cmd/skywire-cli/commands/node/gen-config.go
+++ cmd/skywire-cli/commands/node/gen-config.go
@@ -1,7 +1,9 @@
 package node
 
 import (
+	"errors"
 	"fmt"
+	"net"
 	"path/filepath"
 	"time"
 
@@ -83,6 +85,13 @@ func defaultConfig() *visor.Config {
 	pk, sk := cipher.GenerateKeyPair()
 	conf.Node.StaticPubKey = pk
 	conf.Node.StaticSecKey = sk
+
+	lIPaddr, err := getLocalIPAddress()
+	if err != nil {
+		log.Warn(err)
+	}
+
+	conf.STCP.LocalAddr = lIPaddr
 
 	if testenv {
 		conf.Messaging.Discovery = skyenv.TestDmsgDiscAddr
@@ -183,4 +192,20 @@ func defaultSkysocksClientConfig() visor.AppConfig {
 		AutoStart: false,
 		Port:      routing.Port(skyenv.SkysocksClientPort),
 	}
+}
+
+func getLocalIPAddress() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String() + ":7777", nil
+			}
+		}
+	}
+	return "", errors.New("could not find local IP address")
 }
