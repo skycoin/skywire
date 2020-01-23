7773ba70d954aac6a61d5fa8f8fe8b0471ede97f
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2020-01-24 02:14:57 +0400

    Refactor hypervisor, fix a hypervisor bug

--- cmd/hypervisor/commands/gen-config.go
+++ cmd/hypervisor/commands/gen-config.go
@@ -10,19 +10,26 @@ import (
 	"github.com/SkycoinProject/skywire-mainnet/pkg/util/pathutil"
 )
 
+// nolint:gochecknoglobals
 var (
 	output        string
 	replace       bool
 	configLocType = pathutil.WorkingDirLoc
 )
 
+// nolint:gochecknoinits
 func init() {
+	outputUsage := "path of output config file. Uses default of 'type' flag if unspecified."
+	replaceUsage := "whether to allow rewrite of a file that already exists."
+	configLocTypeUsage := fmt.Sprintf("config generation mode. Valid values: %v", pathutil.AllConfigLocationTypes())
+
 	rootCmd.AddCommand(genConfigCmd)
-	genConfigCmd.Flags().StringVarP(&output, "output", "o", "", "path of output config file. Uses default of 'type' flag if unspecified.")
-	genConfigCmd.Flags().BoolVarP(&replace, "replace", "r", false, "whether to allow rewrite of a file that already exists.")
-	genConfigCmd.Flags().VarP(&configLocType, "type", "m", fmt.Sprintf("config generation mode. Valid values: %v", pathutil.AllConfigLocationTypes()))
+	genConfigCmd.Flags().StringVarP(&output, "output", "o", "", outputUsage)
+	genConfigCmd.Flags().BoolVarP(&replace, "replace", "r", false, replaceUsage)
+	genConfigCmd.Flags().VarP(&configLocType, "type", "m", configLocTypeUsage)
 }
 
+// nolint:gochecknoglobals
 var genConfigCmd = &cobra.Command{
 	Use:   "gen-config",
 	Short: "generates a configuration file",
