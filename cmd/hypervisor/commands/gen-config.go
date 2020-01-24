0d05650ee8ae6adadcde6ec3c6d5a68b6a29ee7a
Kifen <kifenraph@gmail.com>
2020-01-24 12:29:48 +0000

    fix merge conflicts
    Former-commit-id: b0a89f86c44883afedecdc19486a4b799454246c
    Former-commit-id: 09e7608faab709cd25422615ac18843195af0c41

--- cmd/hypervisor/commands/gen-config.go
+++ cmd/hypervisor/commands/gen-config.go
@@ -20,11 +20,18 @@ var (
 
 // nolint:gochecknoinits
 func init() {
+	outputUsage := "path of output config file. Uses default of 'type' flag if unspecified."
+	replaceUsage := "whether to allow rewrite of a file that already exists."
+	configLocTypeUsage := fmt.Sprintf("config generation mode. Valid values: %v", pathutil.AllConfigLocationTypes())
+
 	rootCmd.AddCommand(genConfigCmd)
 	genConfigCmd.Flags().StringVarP(&output, "output", "o", "", "path of output config file. Uses default of 'type' flag if unspecified.")
 	genConfigCmd.Flags().BoolVarP(&replace, "replace", "r", false, "whether to allow rewrite of a file that already exists.")
 	genConfigCmd.Flags().VarP(&configLocType, "type", "m", fmt.Sprintf("config generation mode. Valid values: %v", pathutil.AllConfigLocationTypes()))
 	genConfigCmd.Flags().BoolVarP(&testenv, "testing-environment", "t", false, "whether to use production or test deployment service.")
+	genConfigCmd.Flags().StringVarP(&output, "output", "o", "", outputUsage)
+	genConfigCmd.Flags().BoolVarP(&replace, "replace", "r", false, replaceUsage)
+	genConfigCmd.Flags().VarP(&configLocType, "type", "m", configLocTypeUsage)
 }
 
 // nolint:gochecknoglobals
