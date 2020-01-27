94e957125c9b7456c47f9a09142be3a7bd2dc9e4
Kifen <kifenraph@gmail.com>
2020-01-27 12:44:20 +0000

    rename nodeA to visorA

--- integration/messaging/env-vars.sh
+++ integration/messaging/env-vars.sh
@@ -1,14 +1,14 @@
 # This script needs to be `source`d from bash-compatible shell
 # E.g. `source ./integration/generic/env-vars.sh` or `. ./integration/messaging/env-vars.sh`
-export PK_A=$(jq -r ".node.static_public_key" ./integration/messaging/nodeA.json)
-export RPC_A=$(jq -r ".interfaces.rpc" ./integration/messaging/nodeA.json)
+export PK_A=$(jq -r ".node.static_public_key" ./integration/messaging/visorA.json)
+export RPC_A=$(jq -r ".interfaces.rpc" ./integration/messaging/visorA.json)
 export PK_B=$(jq -r ".node.static_public_key" ./integration/intermediary-nodeB.json)
 export RPC_B=$(jq -r ".interfaces.rpc" ./integration/intermediary-nodeB.json)
-export PK_C=$(jq -r ".node.static_public_key" ./integration/messaging/nodeC.json)
-export RPC_C=$(jq -r ".interfaces.rpc" ./integration/messaging/nodeC.json)
+export PK_C=$(jq -r ".node.static_public_key" ./integration/messaging/visorC.json)
+export RPC_C=$(jq -r ".interfaces.rpc" ./integration/messaging/visorC.json)
 
 export CHAT_A=http://localhost:8000/message
-export CHAT_C=http://localhost$(jq -r '.apps [] |select(.app=="skychat")| .args[1] ' ./integration/messaging/nodeC.json)/message
+export CHAT_C=http://localhost$(jq -r '.apps [] |select(.app=="skychat")| .args[1] ' ./integration/messaging/visorC.json)/message
 
 export MSGD=https://dmsg.discovery.skywire.skycoin.com
 export TRD=https://transport.discovery.skywire.skycoin.com
@@ -18,9 +18,9 @@ alias CLI_A='./skywire-cli --rpc $RPC_A'
 alias CLI_B='./skywire-cli --rpc $RPC_B'
 alias CLI_C='./skywire-cli --rpc $RPC_C'
 
-alias RUN_A='./skywire-visor ./integration/messaging/nodeA.json --tag NodeA'
+alias RUN_A='./skywire-visor ./integration/messaging/visorA.json --tag NodeA'
 alias RUN_B='./skywire-visor ./integration/messaging/intermediary-nodeB.json --tag NodeB'
-alias RUN_C='./skywire-visor ./integration/messaging/nodeC.json --tag NodeC'
+alias RUN_C='./skywire-visor ./integration/messaging/visorC.json --tag NodeC'
 
 echo PK_A: $PK_A
 echo PK_B: $PK_B
