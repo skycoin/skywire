e44323dd076ba0b12ee33bc999879a50ac15cfca
Kifen <kifenraph@gmail.com>
2020-01-27 12:44:20 +0000

    rename nodeA to visorA
    Former-commit-id: 9b7e99ef68ba1c6923728354c628f2b0906c5155

--- integration/proxy/env-vars.sh
+++ integration/proxy/env-vars.sh
@@ -1,11 +1,11 @@
 # This script needs to be `source`d from bash-compatible shell
 # E.g. `source ./integration/proxy/env-vars.sh` or `. ./integration/proxy/env-vars.sh`
-export PK_A=$(jq -r ".node.static_public_key" ./integration/proxy/nodeA.json)
-export RPC_A=$(jq -r ".interfaces.rpc" ./integration/proxy/nodeA.json)
+export PK_A=$(jq -r ".node.static_public_key" ./integration/proxy/visorA.json)
+export RPC_A=$(jq -r ".interfaces.rpc" ./integration/proxy/visorA.json)
 export PK_B=$(jq -r ".node.static_public_key" ./integration/intermediary-nodeB.json)
 export RPC_B=$(jq -r ".interfaces.rpc" ./integration/intermediary-nodeB.json)
-export PK_C=$(jq -r ".node.static_public_key" ./integration/proxy/nodeC.json)
-export RPC_C=$(jq -r ".interfaces.rpc" ./integration/proxy/nodeC.json)
+export PK_C=$(jq -r ".node.static_public_key" ./integration/proxy/visorC.json)
+export RPC_C=$(jq -r ".interfaces.rpc" ./integration/proxy/visorC.json)
 
 alias CLI_A='./skywire-cli --rpc $RPC_A'
 alias CLI_B='./skywire-cli --rpc $RPC_B'
@@ -15,9 +15,9 @@ export MSGD=https://dmsg.discovery.skywire.skycoin.com
 export TRD=https://transport.discovery.skywire.skycoin.com
 export RF=https://routefinder.skywire.skycoin.com
 
-alias RUN_A='go run ./cmd/skywire-visor ./integration/messaging/nodeA.json --tag NodeA'
+alias RUN_A='go run ./cmd/skywire-visor ./integration/messaging/visorA.json --tag NodeA'
 alias RUN_B='go run ./cmd/skywire-visor ./integration/intermediary-nodeB.json --tag NodeB'
-alias RUN_C='go run ./cmd/skywire-visor ./integration/messaging/nodeC.json --tag NodeC'
+alias RUN_C='go run ./cmd/skywire-visor ./integration/messaging/visorC.json --tag NodeC'
 
 echo PK_A: $PK_A
 echo PK_B: $PK_B
