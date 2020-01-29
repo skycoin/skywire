d8db2d717d30e78db8c674f996f0ad542a5219cd
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2020-01-29 23:14:52 +0400

    Print build information on start of all cmd's

--- cmd/setup-node/setup-node.go
+++ cmd/setup-node/setup-node.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/SkycoinProject/skywire-mainnet/cmd/setup-node/commands"
+import (
+	"log"
+
+	"github.com/SkycoinProject/skywire-mainnet/cmd/setup-node/commands"
+	"github.com/SkycoinProject/skywire-mainnet/pkg/buildinfo"
+)
 
 func main() {
+	if _, err := buildinfo.WriteTo(log.Writer()); err != nil {
+		log.Printf("Failed to output build info: %v", err)
+	}
+
 	commands.Execute()
 }
--- cmd/skywire-visor/skywire-visor.go
+++ cmd/skywire-visor/skywire-visor.go
@@ -4,9 +4,16 @@ skywire visor
 package main
 
 import (
+	"log"
+
 	"github.com/SkycoinProject/skywire-mainnet/cmd/skywire-visor/commands"
+	"github.com/SkycoinProject/skywire-mainnet/pkg/buildinfo"
 )
 
 func main() {
+	if _, err := buildinfo.WriteTo(log.Writer()); err != nil {
+		log.Printf("Failed to output build info: %v", err)
+	}
+
 	commands.Execute()
 }
