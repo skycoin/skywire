2a02558f1f87aabe3b7d53c155c3c2230530b727
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2020-01-29 23:14:52 +0400

    Print build information on start of all cmd's
    Former-commit-id: 8b2a114d9ba7cacc72745697ca003bac50af6fe4

--- cmd/skywire-cli/skywire-cli.go
+++ cmd/skywire-cli/skywire-cli.go
@@ -4,9 +4,16 @@ CLI for skywire visor
 package main
 
 import (
+	"log"
+
 	"github.com/SkycoinProject/skywire-mainnet/cmd/skywire-cli/commands"
+	"github.com/SkycoinProject/skywire-mainnet/pkg/buildinfo"
 )
 
 func main() {
+	if _, err := buildinfo.WriteTo(log.Writer()); err != nil {
+		log.Printf("Failed to output build info: %v", err)
+	}
+
 	commands.Execute()
 }
