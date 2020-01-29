677e96bd4c52d5405f3d3b7d644e373a0a847cff
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2020-01-29 23:14:52 +0400

    Print build information on start of all cmd's
    Former-commit-id: 8b2a114d9ba7cacc72745697ca003bac50af6fe4
    Former-commit-id: 2a02558f1f87aabe3b7d53c155c3c2230530b727

--- cmd/hypervisor/hypervisor.go
+++ cmd/hypervisor/hypervisor.go
@@ -3,8 +3,17 @@ skywire hypervisor
 */
 package main
 
-import "github.com/SkycoinProject/skywire-mainnet/cmd/hypervisor/commands"
+import (
+	"log"
+
+	"github.com/SkycoinProject/skywire-mainnet/cmd/hypervisor/commands"
+	"github.com/SkycoinProject/skywire-mainnet/pkg/buildinfo"
+)
 
 func main() {
+	if _, err := buildinfo.WriteTo(log.Writer()); err != nil {
+		log.Printf("Failed to output build info: %v", err)
+	}
+
 	commands.Execute()
 }
