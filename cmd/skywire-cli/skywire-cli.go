f20728a2967e68945a0197ff11c50f44871308de
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2020-01-29 23:53:36 +0400

    Remove unnecessary version output in skywire-cli
    Former-commit-id: 232e59a5dd048c0f3b3e4005d57d4b9f649b2fcd

--- cmd/skywire-cli/skywire-cli.go
+++ cmd/skywire-cli/skywire-cli.go
@@ -4,16 +4,9 @@ CLI for skywire visor
 package main
 
 import (
-	"log"
-
 	"github.com/SkycoinProject/skywire-mainnet/cmd/skywire-cli/commands"
-	"github.com/SkycoinProject/skywire-mainnet/pkg/buildinfo"
 )
 
 func main() {
-	if _, err := buildinfo.Get().WriteTo(log.Writer()); err != nil {
-		log.Printf("Failed to output build info: %v", err)
-	}
-
 	commands.Execute()
 }
