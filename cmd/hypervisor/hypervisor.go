fff1fed700cb7619de0c149d1c28ba8a6dcced8a
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2020-01-29 23:41:24 +0400

    Fix output of build info on start
    Former-commit-id: 717b13531358ee8280d5be9a7871357d0538aca0
    Former-commit-id: e09a28c12a5aa870624ede4a8c50cc20968ef5d0

--- cmd/hypervisor/hypervisor.go
+++ cmd/hypervisor/hypervisor.go
@@ -4,16 +4,9 @@ skywire hypervisor
 package main
 
 import (
-	"log"
-
 	"github.com/SkycoinProject/skywire-mainnet/cmd/hypervisor/commands"
-	"github.com/SkycoinProject/skywire-mainnet/pkg/buildinfo"
 )
 
 func main() {
-	if _, err := buildinfo.Get().WriteTo(log.Writer()); err != nil {
-		log.Printf("Failed to output build info: %v", err)
-	}
-
 	commands.Execute()
 }
