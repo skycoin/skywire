fef401203a42af93b0b7d060581718568fccc7e3
Sir Darkrengarius <darkren92@gmail.com>
2020-02-05 14:38:40 +0300

    Ensure thread safety within the app proc
    Former-commit-id: 77469e7fb451ba892a5902daca328f4ee0c591af
    Former-commit-id: d6992d177d42596f170f037b7ecb447fced55aa7

--- pkg/app/appserver/proc.go
+++ pkg/app/appserver/proc.go
@@ -1,26 +1,36 @@
 package appserver
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"sync"
+	"sync/atomic"
 
 	"github.com/SkycoinProject/skycoin/src/util/logging"
 
 	"github.com/SkycoinProject/skywire-mainnet/pkg/app/appcommon"
 )
 
+var (
+	errProcAlreadyRunning = errors.New("process already running")
+	errProcNotStarted     = errors.New("process is not started")
+)
+
 // Proc is a wrapper for a skywire app. Encapsulates
 // the running process itself and the RPC server for
 // app/visor communication.
 type Proc struct {
-	key    appcommon.Key
-	config appcommon.Config
-	log    *logging.Logger
-	cmd    *exec.Cmd
-	doneC  chan struct{}
+	key       appcommon.Key
+	config    appcommon.Config
+	log       *logging.Logger
+	cmd       *exec.Cmd
+	isRunning int32
+	waitMx    sync.Mutex
+	waitErr   error
 }
 
 // NewProc constructs `Proc`.
@@ -53,32 +63,63 @@ func NewProc(log *logging.Logger, c appcommon.Config, args []string, stdout, std
 		config: c,
 		log:    log,
 		cmd:    cmd,
-		doneC:  make(chan struct{}),
 	}, nil
 }
 
 // Start starts the application.
 func (p *Proc) Start() error {
-	return p.cmd.Start()
+	if !atomic.CompareAndSwapInt32(&p.isRunning, 0, 1) {
+		return errProcAlreadyRunning
+	}
+
+	if err := p.cmd.Start(); err != nil {
+		return err
+	}
+
+	// acquire lock immediately
+	p.waitMx.Lock()
+	go func() {
+		defer p.waitMx.Unlock()
+		p.waitErr = p.cmd.Wait()
+	}()
+
+	return nil
 }
 
 // Stop stops the application.
 func (p *Proc) Stop() error {
+	if atomic.LoadInt32(&p.isRunning) != 1 {
+		return errProcNotStarted
+	}
+
 	err := p.cmd.Process.Signal(os.Interrupt)
 	if err != nil {
 		return err
 	}
 
-	<-p.doneC
+	// the lock will be acquired as soon as the cmd finishes its work
+	p.waitMx.Lock()
+	defer p.waitMx.Unlock()
 
 	return nil
 }
 
 // Wait waits for the application cmd to exit.
 func (p *Proc) Wait() error {
-	err := p.cmd.Wait()
-	close(p.doneC)
-	return err
+	if atomic.LoadInt32(&p.isRunning) != 1 {
+		return errProcNotStarted
+	}
+
+	// the lock will be acquired as soon as the cmd finishes its work
+	p.waitMx.Lock()
+	defer p.waitMx.Unlock()
+
+	return p.waitErr
+}
+
+// IsRunning checks whether application cmd is running.
+func (p *Proc) IsRunning() bool {
+	return atomic.LoadInt32(&p.isRunning) == 1
 }
 
 // getBinaryPath formats binary path using app dir, name and version.
