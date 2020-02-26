c63edc10588707b109e3831c061fb9829e2a225d
志宇 <evanlinjin@pm.me>
2020-02-26 23:47:36 +0800

    Changes to integrate dmsgpty

--- vendor/github.com/SkycoinProject/dmsg/netutil/retrier.go
+++ vendor/github.com/SkycoinProject/dmsg/netutil/retrier.go
@@ -13,113 +13,83 @@ var (
 	ErrMaximumRetriesReached = errors.New("maximum retries attempted without success")
 )
 
+// Default values for retrier.
+const (
+	DefaultBackoff    = 100 * time.Millisecond
+	DefaultMaxBackoff = time.Minute * 5
+	DefaultTries      = 0
+	DefaultFactor     = 2
+)
+
 // RetryFunc is a function used as argument of (*Retrier).Do(), which will retry on error unless it is whitelisted
 type RetryFunc func() error
 
 // Retrier holds a configuration for how retries should be performed
 type Retrier struct {
-	expBackoff   time.Duration // multiplied on every retry by a expFactor
-	expFactor    uint32        // multiplier for the backoff duration that is applied on every retry
-	times        uint32        // number of times that the given function is going to be retried until success, if 0 it will be retried forever until success
-	errWhitelist map[error]struct{}
-	log          logrus.FieldLogger
+	initBO time.Duration      // initial backoff duration
+	maxBO  time.Duration      // maximum backoff duration
+	factor int                // multiplier for the backoff duration that is applied on every retry
+	times  int                // number of times that the given function is going to be retried until success, if 0 it will be retried forever until success
+	errWl  map[error]struct{} // list of errors which will always trigger retirer to return
+	log    logrus.FieldLogger
 }
 
 // NewRetrier returns a retrier that is ready to call Do() method
-func NewRetrier(log logrus.FieldLogger, backOff time.Duration, times, factor uint32) *Retrier {
+func NewRetrier(log logrus.FieldLogger, initBackoff, maxBackoff time.Duration, times, factor int) *Retrier {
 	return &Retrier{
-		expBackoff:   backOff,
-		times:        times,
-		expFactor:    factor,
-		errWhitelist: make(map[error]struct{}),
-		log:          log,
+		initBO: initBackoff,
+		maxBO:  maxBackoff,
+		times:  times,
+		factor: factor,
+		errWl:  make(map[error]struct{}),
+		log:    log,
 	}
 }
 
-// Default values for retrier.
-const (
-	DefaultBackOff = 100 * time.Millisecond
-	DefaultTries   = 0
-	DefaultFactor  = 2
-)
-
 // NewDefaultRetrier creates a retrier with default values.
 func NewDefaultRetrier(log logrus.FieldLogger) *Retrier {
-	return NewRetrier(log, DefaultBackOff, DefaultTries, DefaultFactor)
+	return NewRetrier(log, DefaultBackoff, DefaultMaxBackoff, DefaultTries, DefaultFactor)
 }
 
 // WithErrWhitelist sets a list of errors into the retrier, if the RetryFunc provided to Do() fails with one of them it will return inmediatelly with such error. Calling
 // this function is not thread-safe, and is advised to only use it when initializing the Retrier
 func (r *Retrier) WithErrWhitelist(errors ...error) *Retrier {
-	m := make(map[error]struct{})
 	for _, err := range errors {
-		m[err] = struct{}{}
+		r.errWl[err] = struct{}{}
 	}
-
-	r.errWhitelist = m
 	return r
 }
 
-// Do takes a RetryFunc and attempts to execute it, if it fails with an error it will be retried a maximum of given times with an expBackoff, until it returns
-// nil or an error that is whitelisted
+// Do takes a RetryFunc and attempts to execute it.
+// If it fails with an error it will be retried a maximum of given times with an initBO
+// until it returns nil or an error that is whitelisted
 func (r *Retrier) Do(ctx context.Context, f RetryFunc) error {
-	if r.times == 0 {
-		return r.retryUntilSuccess(ctx, f)
-	}
+	bo := r.initBO
 
-	return r.retryNTimes(f)
-}
-
-func (r *Retrier) retryNTimes(f RetryFunc) error {
-	currentBackoff := r.expBackoff
-
-	for i := uint32(0); i < r.times; i++ {
-		err := f()
-		if err != nil {
-			if r.isWhitelisted(err) {
-				return err
-			}
-
-			r.log.WithError(err).Warn()
-			currentBackoff *= time.Duration(r.expFactor)
-			time.Sleep(currentBackoff)
-			continue
-		}
-
-		return nil
-	}
-
-	return ErrMaximumRetriesReached
-}
-
-func (r *Retrier) retryUntilSuccess(ctx context.Context, f RetryFunc) error {
-	currentBackoff := r.expBackoff
-
-	for {
+	for i := 0; r.times == 0 || i < r.times; i++ {
 		if err := f(); err != nil {
-			if r.isWhitelisted(err) {
+			if _, ok := r.errWl[err]; ok {
 				return err
 			}
-
-			r.log.WithError(err).Warn()
-			currentBackoff *= time.Duration(r.expFactor)
-
-			//time.Sleep(currentBackoff)
-			ticker := time.NewTicker(currentBackoff)
+			if newBO := bo * time.Duration(r.factor); r.maxBO == 0 || newBO <= r.maxBO {
+				bo = newBO
+			}
+			if r.log != nil {
+				r.log.
+					WithError(err).
+					WithField("current_backoff", bo).
+					Warn("Retrier: retrying...")
+			}
+			t := time.NewTimer(bo)
 			select {
-			case <-ticker.C:
+			case <-t.C:
 				continue
-
 			case <-ctx.Done():
-				ticker.Stop()
+				t.Stop()
 				return ctx.Err()
 			}
 		}
 		return nil
 	}
-}
-
-func (r *Retrier) isWhitelisted(err error) bool {
-	_, ok := r.errWhitelist[err]
-	return ok
+	return ErrMaximumRetriesReached
 }
