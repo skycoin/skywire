6202fe5288a7d11c5f173e29d4b0683da0acec62
Sir Darkrengarius <darkren92@gmail.com>
2020-03-19 13:11:06 +0300

    Replace unix pipes with TCP sockets

--- pkg/app/appcommon/config.go
+++ pkg/app/appcommon/config.go
@@ -2,9 +2,10 @@ package appcommon
 
 // Config defines configuration parameters for `Proc`.
 type Config struct {
-	Name         string `json:"name"`
-	SockFilePath string `json:"sock_file_path"`
-	VisorPK      string `json:"visor_pk"`
-	BinaryDir    string `json:"binary_dir"`
-	WorkDir      string `json:"work_dir"`
+	Name       string `json:"name"`
+	ServerHost string `json:"server_host"`
+	ServerPort uint   `json:"server_port"`
+	VisorPK    string `json:"visor_pk"`
+	BinaryDir  string `json:"binary_dir"`
+	WorkDir    string `json:"work_dir"`
 }
