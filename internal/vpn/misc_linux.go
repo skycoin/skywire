a3e29a65692cb43576900fb5c8cc84462db194c7
Sir Darkrengarius <darkren92@gmail.com>
2020-04-09 16:15:35 +0300

    Add VPN server draft
    Former-commit-id: 742462f4409211a6e4d25c769428684c4a2427b8
    Former-commit-id: c43ebe45aef7f793a75338bae764d36a0db7cfd0

--- internal/vpn/misc_linux.go
+++ internal/vpn/misc_linux.go
@@ -2,6 +2,47 @@
 
 package vpn
 
-const (
-	GatewayForIfcCMDFmt = "route -n | grep %s | awk '$1 == \"0.0.0.0\" {print $2}'"
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
 )
+
+const (
+	gatewayForIfcCMDFmt         = "route -n | grep %s | awk '$1 == \"0.0.0.0\" {print $2}'"
+	setIPv4ForwardingCMDFmt     = "sysctl -w net.ipv4.ip_forward=%s"
+	setIPv6ForwardingCMDFmt     = "sysctl -w net.ipv6.conf.all.forwarding=%s"
+	getIPv4ForwardingCMD        = "sysctl net.ipv4.ip_forward"
+	getIPv6ForwardingCMD        = "sysctl net.ipv6.conf.all.forwarding"
+	enableIPMasqueradingCMDFmt  = "iptables -t nat -A POSTROUTING -o %s -j MASQUERADE"
+	disableIPMasqueradingCMDFmt = "iptables -t nat -D POSTROUTING -o %s -j MASQUERADE"
+)
+
+func EnableIPMasquerading(ifcName string) error {
+	cmd := fmt.Sprintf(enableIPMasqueradingCMDFmt, ifcName)
+	if err := exec.Command("bash", "-c", cmd).Wait(); err != nil {
+		return fmt.Errorf("error running command %s: %w", cmd, err)
+	}
+
+	return nil
+}
+
+func DisableIPMasquerading(ifcName string) error {
+	cmd := fmt.Sprintf(disableIPMasqueradingCMDFmt, ifcName)
+	if err := exec.Command("bash", "-c", cmd).Wait(); err != nil {
+		return fmt.Errorf("error running command %s: %w", cmd, err)
+	}
+
+	return nil
+}
+
+func parseIPForwardingOutput(output []byte) (string, error) {
+	output = bytes.TrimRight(output, "\n")
+
+	outTokens := bytes.Split(output, []byte{'='})
+	if len(outTokens) != 2 {
+		return "", fmt.Errorf("invalid output: %s", output)
+	}
+
+	return string(bytes.Trim(outTokens[1], " ")), nil
+}
