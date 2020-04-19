ec36242989b170fcc963e7c80ae955cc1c768243
Sir Darkrengarius <darkren92@gmail.com>
2020-04-19 19:55:11 +0300

    Make IP generator loop over private IP ranges
    Former-commit-id: 7c5ac7bb3eec86eb0a6cad9d4bd44e135f80b66c

--- internal/vpn/tun_ip_generator.go
+++ internal/vpn/tun_ip_generator.go
@@ -2,22 +2,84 @@ package vpn
 
 import (
 	"errors"
-	"math"
 	"net"
 	"sync"
 )
 
-type TUNIPGenerator struct {
-	mx        sync.Mutex
-	currentIP uint8
-	reserved  [math.MaxUint8]bool
-	step      uint8
+type IPIncrementer struct {
+	mx                sync.Mutex
+	octets            [4]uint8
+	octetLowerBorders [4]uint8
+	octetBorders      [4]uint8
+	step              uint8
+	reserved          map[[4]uint8]struct{}
 }
 
-func NewTUNIPGenerator(step uint8) *TUNIPGenerator {
+func NewIPIncrementer(octetLowerBorders, octetBorders [4]uint8, step uint8) *IPIncrementer {
+	return &IPIncrementer{
+		mx:                sync.Mutex{},
+		octets:            octetLowerBorders,
+		octetLowerBorders: octetLowerBorders,
+		octetBorders:      octetBorders,
+		step:              step,
+		reserved:          make(map[[4]uint8]struct{}),
+	}
+}
+
+func (inc *IPIncrementer) Next() (ip, gateway net.IP, err error) {
+	inc.mx.Lock()
+	defer inc.mx.Unlock()
+
+	var ipArr [4]uint8
+
+	for i := 3; i >= 0; i-- {
+		for k := 0; k < i; k++ {
+			ipArr[k] = inc.octets[k]
+		}
+		for k := i + 1; k < 4; k++ {
+			ipArr[k] = inc.octets[k]
+		}
+
+		for j := inc.octets[i] + inc.step; j != inc.octets[i]; j += inc.step {
+			if j >= inc.octetBorders[i] {
+				j = inc.octetLowerBorders[i]
+				continue
+			}
+
+			if i == 3 && j == 0 {
+				// TODO: fix to skip only the network address
+				continue
+			}
+
+			ipArr[i] = j
+			if _, ok := inc.reserved[ipArr]; !ok {
+				inc.octets[i] = j
+				inc.reserved[ipArr] = struct{}{}
+
+				// TODO: fix possible miscalculations
+				return net.IPv4(inc.octets[0], inc.octets[1], inc.octets[2], inc.octets[3]), net.IPv4(inc.octets[0], inc.octets[1], inc.octets[2], inc.octets[3]-1), nil
+			}
+		}
+
+		inc.octets[i] = inc.octetLowerBorders[i]
+	}
+
+	return nil, nil, errors.New("no free IPs left")
+}
+
+type TUNIPGenerator struct {
+	mx           sync.Mutex
+	currentRange uint8
+	ranges       []*IPIncrementer
+}
+
+func NewTUNIPGenerator() *TUNIPGenerator {
 	return &TUNIPGenerator{
-		step:      step,
-		currentIP: 1,
+		ranges: []*IPIncrementer{
+			NewIPIncrementer([4]uint8{192, 168, 0, 0}, [4]uint8{192, 168, 255, 255}, 4),
+			NewIPIncrementer([4]uint8{172, 16, 0, 0}, [4]uint8{172, 31, 255, 255}, 4),
+			NewIPIncrementer([4]uint8{10, 0, 0, 0}, [4]uint8{10, 255, 255, 255}, 4),
+		},
 	}
 }
 
@@ -25,17 +87,14 @@ func (g *TUNIPGenerator) Next() (ip, gateway net.IP, err error) {
 	g.mx.Lock()
 	defer g.mx.Unlock()
 
-	for i := g.currentIP + g.step; i != g.currentIP; i += g.step {
-		if i == 0 {
-			// skip 192.168.255.0
+	for i := g.currentRange; i < 4; i++ {
+		ip, gateway, err := g.ranges[i].Next()
+		if err != nil {
+			g.currentRange++
 			continue
 		}
 
-		if !g.reserved[i] {
-			g.currentIP = i
-			g.reserved[i] = true
-			return net.IPv4(192, 168, 255, g.currentIP+1), net.IPv4(192, 168, 255, g.currentIP), nil
-		}
+		return ip, gateway, nil
 	}
 
 	return nil, nil, errors.New("no free IPs left")
