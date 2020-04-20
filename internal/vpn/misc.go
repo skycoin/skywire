ffd024ad517d3292596e800af62a7b3672b8fd2a
Sir Darkrengarius <darkren92@gmail.com>
2020-04-20 15:54:19 +0300

    Add func to get pre-reserved private IPs
    Former-commit-id: a0bea5bef0be9fd9b8093fb86b601ab986c79e37

--- internal/vpn/misc.go
+++ internal/vpn/misc.go
@@ -141,6 +141,48 @@ func DefaultNetworkIfc() (string, error) {
 	return "", errors.New("no internet connection")
 }
 
+func GetIPsToReserve() ([]net.IP, error) {
+	var toReserve []net.IP
+
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return nil, err
+		}
+		for _, addr := range addrs {
+			fmt.Printf("Scanning addr: %s\n", addr)
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				continue // not an ipv4 address
+			}
+
+			toReserve = append(toReserve, ip)
+		}
+	}
+
+	return toReserve, nil
+}
+
 func GetIPv4ForwardingValue() (string, error) {
 	return getIPForwardingValue(getIPv4ForwardingCMD)
 }
