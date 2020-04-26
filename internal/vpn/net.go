9c10b2d31876af9da3601f22c046ee211e118eb4
Sir Darkrengarius <darkren92@gmail.com>
2020-04-26 20:31:22 +0300

    Try to simplify handshake with `ioutil.ReadAll`
    Former-commit-id: 38e04ce40cc741cfc9dcea6d692bb87b220bad46
    Former-commit-id: c2fd2ac49dc4d8290b84bd0f572950a8185d8abf

--- internal/vpn/net.go
+++ internal/vpn/net.go
@@ -3,6 +3,7 @@ package vpn
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net"
 )
 
@@ -27,21 +28,9 @@ func WriteJSON(conn net.Conn, data interface{}) error {
 
 // ReadJSON reads portion of data from the `conn` and unmarshals it into `data`.
 func ReadJSON(conn net.Conn, data interface{}) error {
-	const bufSize = 1024
-
-	var dataBytes []byte
-	buf := make([]byte, bufSize)
-	for {
-		n, err := conn.Read(buf)
-		if err != nil {
-			return fmt.Errorf("error reading data: %w", err)
-		}
-
-		dataBytes = append(dataBytes, buf[:n]...)
-
-		if n < 1024 {
-			break
-		}
+	dataBytes, err := ioutil.ReadAll(conn)
+	if err != nil {
+		return fmt.Errorf("error reading data: %w", err)
 	}
 
 	if err := json.Unmarshal(dataBytes, data); err != nil {
