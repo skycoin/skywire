9ebf21f9179d603ce12f765ea2cbf525a949fc7e
Sir Darkrengarius <darkren92@gmail.com>
2020-04-30 14:35:22 +0300

    Add all cmds called by path relative to PATH
    Former-commit-id: c0c62af6cef3838813f2fbf89fb28398f07f0bd1
    Former-commit-id: 6b3f36297db58c4a78030fc45e70449bdc91b1cd

--- internal/vpn/os_linux.go
+++ internal/vpn/os_linux.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	gatewayForIfcCMDFmt         = "/sbin/route -n | /bin/grep %s | /usr/bin/awk '$1 == \"0.0.0.0\" {print $2}'"
+	gatewayForIfcCMDFmt         = "route -n | grep %s | awk '$1 == \"0.0.0.0\" {print $2}'"
 	setIPv4ForwardingCMDFmt     = "sysctl -w net.ipv4.ip_forward=%s"
 	setIPv6ForwardingCMDFmt     = "sysctl -w net.ipv6.conf.all.forwarding=%s"
 	getIPv4ForwardingCMD        = "sysctl net.ipv4.ip_forward"
@@ -21,7 +21,7 @@ const (
 // EnableIPMasquerading enables IP masquerading for the interface with name `ifcName`.
 func EnableIPMasquerading(ifcName string) error {
 	cmd := fmt.Sprintf(enableIPMasqueradingCMDFmt, ifcName)
-	if err := exec.Command("/bin/sh", "-c", cmd).Run(); err != nil {
+	if err := exec.Command("sh", "-c", cmd).Run(); err != nil {
 		return fmt.Errorf("error running command %s: %w", cmd, err)
 	}
 
@@ -31,7 +31,7 @@ func EnableIPMasquerading(ifcName string) error {
 // DisableIPMasquerading disables IP masquerading for the interface with name `ifcName`.
 func DisableIPMasquerading(ifcName string) error {
 	cmd := fmt.Sprintf(disableIPMasqueradingCMDFmt, ifcName)
-	if err := exec.Command("/bin/sh", "-c", cmd).Run(); err != nil {
+	if err := exec.Command("sh", "-c", cmd).Run(); err != nil {
 		return fmt.Errorf("error running command %s: %w", cmd, err)
 	}
 
@@ -44,7 +44,7 @@ func AddRoute(ip, gateway, netmask string) error {
 		netmask = "255.255.255.255"
 	}
 
-	return run("/sbin/route", "add", "-net", ip, "netmask", netmask, "gw", gateway)
+	return run("route", "add", "-net", ip, "netmask", netmask, "gw", gateway)
 }
 
 // DeleteRoute removes route to `ip` with `netmask` through the `gateway` from the OS routing table.
@@ -53,7 +53,7 @@ func DeleteRoute(ip, gateway, netmask string) error {
 		netmask = "255.255.255.255"
 	}
 
-	return run("/sbin/route", "delete", "-net", ip, "netmask", netmask, "gw", gateway)
+	return run("route", "delete", "-net", ip, "netmask", netmask, "gw", gateway)
 }
 
 func parseIPForwardingOutput(output []byte) (string, error) {
