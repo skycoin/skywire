bf3c6510cfe3472075ceadd835211e00f4296b10
Sir Darkrengarius <darkren92@gmail.com>
2020-04-30 15:33:16 +0300

    FUlly add passcode

---
+++ internal/vpn/client_config.go
@@ -0,0 +1,6 @@
+package vpn
+
+// ClientConfig is a configuration for VPN client.
+type ClientConfig struct {
+	Passcode string
+}
--- internal/vpn/client_hello.go
+++ internal/vpn/client_hello.go
@@ -5,4 +5,5 @@ import "net"
 // ClientHello is a message sent by client during the Client/Server handshake.
 type ClientHello struct {
 	UnavailablePrivateIPs []net.IP `json:"unavailable_private_ips"`
+	Passcode              string   `json:"passcode"`
 }
