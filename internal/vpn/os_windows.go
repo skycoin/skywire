6b5e0aaf2ae6273efd1e19c1de5c0a9105fe41fe
Sir Darkrengarius <darkren92@gmail.com>
2020-05-06 16:59:42 +0300

    Finish TUN setup for Windows
    Former-commit-id: 9ec65d28f1d175d73870f76b522cf03222f318cb

--- internal/vpn/os_windows.go
+++ internal/vpn/os_windows.go
@@ -13,6 +13,8 @@ import (
 
 const (
 	defaultNetworkGatewayCMD = "route PRINT"
+	tunSetupCMDFmt           = "netsh interface ip set address \"%s\" static %s %s %s"
+	tunMTUSetupCMDFmt        = "netsh interface ipv4 set subinterface \"%s\" mtu=%d"
 )
 
 var redundantWhitespacesCleanupRegex = regexp.MustCompile(`[\s\p{Zs}]{2,}`)
@@ -50,4 +52,24 @@ func DefaultNetworkGateway() (net.IP, error) {
 	}
 
 	return nil, errors.New("couldn't find default network gateway")
+}
+
+// SetupTUN sets the allocated TUN interface up, setting its IP, gateway, netmask and MTU.
+func SetupTUN(ifcName, ipCIDR, gateway string, mtu int) error {
+	ip, netmask, err := parseCIDR(ipCIDR)
+	if err != nil {
+		return fmt.Errorf("error parsing IP CIDR: %w", err)
+	}
+
+	setupCmd := fmt.Sprintf(tunSetupCMDFmt, ifcName, ip, netmask, gateway)
+	if err := run("cmd", "/C", setupCmd); err != nil {
+		return fmt.Errorf("error running command %s: %w", setupCmd, err)
+	}
+
+	mtuSetupCmd := fmt.Sprintf(tunMTUSetupCMDFmt, ifcName, mtu)
+	if err := run("cmd", "/C", mtuSetupCmd); err != nil {
+		return fmt.Errorf("error running command %s: %w", mtuSetupCmd, err)
+	}
+
+	return nil
 }
