7d232fe68b91fd7c091e27d67b84cf65ce1e9ebf
darkrengarius <darkren92@gmail.com>
2020-05-12 12:47:38 +0300

    Revert testing changes
    Former-commit-id: 4d500476c65fd7491a6dd8c64a0bd9cd9fa0d6b3

--- internal/vpn/const.go
+++ internal/vpn/const.go
@@ -3,7 +3,7 @@ package vpn
 const (
 	// TUNNetmaskCIDR is a general netmask used for all TUN interfaces in CIDR format (only suffix).
 	// TODO: change back
-	TUNNetmaskCIDR = "/30"
+	TUNNetmaskCIDR = "/29"
 	// TUNMTU is MTU value used for all TUN interfaces.
 	TUNMTU = 1500
 )
