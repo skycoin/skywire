cd5f82a7e614068aa316f8278721169faa9434ca
Sir Darkrengarius <darkren92@gmail.com>
2020-05-13 13:39:56 +0300

    Make linter happy
    Former-commit-id: 03d1f5fa9244d0bef7ba3352c25552995e3ad220

--- pkg/syslog/syslog_windows.go
+++ pkg/syslog/syslog_windows.go
@@ -8,6 +8,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// SetupHook sets up syslog hook to the daemon on `addr`.
 func SetupHook(addr, tag string) (logrus.Hook, error) {
 	return nil, errors.New("syslog is not available for this OS")
 }
