1c043f0a7e90025ace3e416d322f1185d38fbeba
志宇 <mail@evanlinjin.me>
2020-05-18 16:21:08 +1200

    Fixed tests.
    Former-commit-id: 197c9c5eb8523fab2a10a4246685439dc332e27d
    Former-commit-id: 3409bc6659babdf4a6d5801c16e003ea19868ef4

--- pkg/visor/visorconfig/v0.go
+++ pkg/visor/visorconfig/v0.go
@@ -1,7 +1,8 @@
 package visorconfig
 
 // V0Name is the version string before proper versioning is implemented.
-const V0Name = ""
+const V0Name = "v0.0.0"
 
+// V0 is visor config v0.0.0
 type V0 struct {
 }
