6ebdae78a554fea9a0a92373d416a0e8db67e0c6
志宇 <mail@evanlinjin.me>
2020-05-18 16:21:08 +1200

    Fixed tests.

--- ci_scripts/install-golangci-lint.sh
+++ ci_scripts/install-golangci-lint.sh
@@ -10,6 +10,9 @@ if [[ "$VERSION" != "latest" ]]; then
   VERSION="v$VERSION"
 fi
 
+[[ -z "${GOBIN}" ]] export GOBIN=$HOME/go/bin
+echo "GOBIN=${GOBIN}"
+
 # In alpine linux (as it does not come with curl by default)
 wget -O - -q https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b "${GOBIN}" "${VERSION}"
 
