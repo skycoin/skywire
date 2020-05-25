100ef1e74e13f71fedd6f6964d11af740e2183b8
Sir Darkrengarius <darkren92@gmail.com>
2020-05-25 19:40:03 +0300

    Make linter happy, add VPN disc client to the factory
    Former-commit-id: 8bf5d5d25a29390b52145e4447543a804f59b561
    Former-commit-id: c9ec245b678888bf9f8bdfe67506bc971bc820e0

--- pkg/app/appdisc/factory.go
+++ pkg/app/appdisc/factory.go
@@ -36,7 +36,6 @@ func (f *Factory) setDefaults() {
 
 // Updater obtains an updater based on the app name and configuration.
 func (f *Factory) Updater(conf appcommon.ProcConfig) (Updater, bool) {
-
 	// Always return empty updater if keys are not set.
 	if f.setDefaults(); f.PK.Null() || f.SK.Null() {
 		return &emptyUpdater{}, false
@@ -44,24 +43,31 @@ func (f *Factory) Updater(conf appcommon.ProcConfig) (Updater, bool) {
 
 	log := f.Log.WithField("appName", conf.AppName)
 
+	// Do not update in proxy discovery if passcode-protected.
+	if containsFlag(conf.ProcArgs, "passcode") {
+		return &emptyUpdater{}, false
+	}
+
 	switch conf.AppName {
-	case "skysocks":
-
-		// Do not update in proxy discovery if passcode-protected.
-		if containsFlag(conf.ProcArgs, "passcode") {
-			return &emptyUpdater{}, false
-		}
-
+	case skyenv.SkysocksName:
 		return &proxyUpdater{
 			client: servicedisc.NewClient(log, servicedisc.Config{
 				PK:       f.PK,
 				SK:       f.SK,
 				Port:     uint16(conf.RoutingPort),
 				DiscAddr: f.ProxyDisc,
-			}),
+			}, servicedisc.ServiceTypeProxy),
 			interval: f.UpdateInterval,
 		}, true
-
+	case skyenv.VPNServerName:
+		return &proxyUpdater{
+			client: servicedisc.NewClient(log, servicedisc.Config{
+				PK:       f.PK,
+				SK:       f.SK,
+				Port:     uint16(conf.RoutingPort),
+				DiscAddr: f.ProxyDisc,
+			}, servicedisc.ServiceTypeVPN),
+		}, true
 	default:
 		return &emptyUpdater{}, false
 	}
