3a7b88d4b094eb81ed584f7c894a70234d4d7023
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2020-06-22 17:30:03 +0300

    Filter outgoing KCP connection
    Former-commit-id: c0b24616e3fd2af226ad8af23b8bc80b3fe37fb1
    Former-commit-id: 51f29f2a1cec02fce965d1c65559236c8c72a0db

--- internal/packetfilter/address-filter.go
+++ internal/packetfilter/address-filter.go
@@ -6,15 +6,15 @@ import (
 	"github.com/SkycoinProject/skycoin/src/util/logging"
 )
 
-var log = logging.MustGetLogger("packetfilter")
-
 type AddressFilter struct {
+	log   *logging.Logger
 	addr  net.Addr
-	match bool
+	match bool // TODO: remove
 }
 
 func NewAddressFilter(addr net.Addr, match bool) *AddressFilter {
 	return &AddressFilter{
+		log:   logging.MustGetLogger("address-filter"),
 		addr:  addr,
 		match: match,
 	}
@@ -23,12 +23,12 @@ func NewAddressFilter(addr net.Addr, match bool) *AddressFilter {
 func (f *AddressFilter) ClaimIncoming(in []byte, addr net.Addr) bool {
 	if f.match {
 		ret := addr.String() == f.addr.String()
-		log.Infof("[AddressFilter] f.match = %v, addr = %q, f.addr = %q, ret = %v", f.match, addr.String(), f.addr.String(), ret)
+		f.log.Infof("[AddressFilter] f.match = %v, addr = %q, f.addr = %q, ret = %v", f.match, addr.String(), f.addr.String(), ret)
 		return ret
 	}
 
 	ret := addr.String() != f.addr.String()
-	log.Infof("[AddressFilter] f.match = %v, addr = %q, f.addr = %q, ret = %v", f.match, addr.String(), f.addr.String(), ret)
+	f.log.Infof("[AddressFilter] f.match = %v, addr = %q, f.addr = %q, ret = %v", f.match, addr.String(), f.addr.String(), ret)
 	return ret
 }
 
