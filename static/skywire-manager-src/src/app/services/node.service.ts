fde6fe4041b0c813c523cef25a5f3928a74a391e
Senyoret1 <34079003+Senyoret1@users.noreply.github.com>
2020-07-11 11:16:05 -0400

    Improve the UI data refreshing and navigation
    Former-commit-id: ed920c456944855885ad0ac089073d76f65f1f0a

--- static/skywire-manager-src/src/app/services/node.service.ts
+++ static/skywire-manager-src/src/app/services/node.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { flatMap, map, mergeMap } from 'rxjs/operators';
+import { Observable, Subscription, BehaviorSubject, of } from 'rxjs';
+import { flatMap, map, mergeMap, delay, tap } from 'rxjs/operators';
 import BigNumber from 'bignumber.js';
 
 import { StorageService } from './storage.service';
@@ -8,6 +8,29 @@ import { Node, Transport, Route, HealthInfo } from '../app.datatypes';
 import { ApiService } from './api.service';
 import { TransportService } from './transport.service';
 import { RouteService } from './route.service';
+import { processServiceError } from '../utils/errors';
+import { OperationError } from '../utils/operation-error';
+
+/**
+ * Response returned by the node and node list observables.
+ */
+export interface BackendData {
+  /**
+   * Node or node list, if the last operation for getting the data did not end in an error.
+   */
+  data: Node[] | Node;
+  /**
+   * Error found while trying to get the data. If this property has a value, the data
+   * property will be null.
+   */
+  error: OperationError;
+  /**
+   * Unix time in which the data returned in the data property was obtained. If
+   * OperationError has a value, this property will still have a valid value if valid
+   * data was previously found.
+   */
+  momentOfLastCorrectUpdate: number;
+}
 
 /**
  * Allows to work with the nodes.
@@ -16,17 +39,241 @@ import { RouteService } from './route.service';
   providedIn: 'root'
 })
 export class NodeService {
+
+  // Delays the service waits before requesting data.
+  private initialErrorRetryDelay = 3000;
+  private dataRefreshDelay: number;
+
   constructor(
     private apiService: ApiService,
     private storageService: StorageService,
     private transportService: TransportService,
     private routeService: RouteService,
-  ) {}
+  ) {
+    // Get the data refresing time set by the user.
+    this.storageService.getRefreshTimeObservable().subscribe(val => {
+      this.dataRefreshDelay = val * 1000;
+
+      // If the service is currently automatically refreshing the data, restart the process.
+      if (this.nodeListRefreshSubscription) {
+        this.forceNodeListRefresh();
+      }
+      if (this.specificNodeRefreshSubscription) {
+        this.forceSpecificNodeRefresh();
+      }
+    });
+  }
+
+  // Vars related to the node list.
+  private nodeListSubject = new BehaviorSubject<BackendData>(null);
+  private updatingNodeListSubject = new BehaviorSubject<boolean>(false);
+  /**
+   * Subscription for getting the node list. If it has a value, indicates that the
+   * service is automatically refreshing the node list.
+   */
+  private nodeListRefreshSubscription: Subscription;
+
+  // Vars related to the specific node.
+  private specificNodeSubject = new BehaviorSubject<BackendData>(null);
+  private updatingSpecificNodeSubject = new BehaviorSubject<boolean>(false);
+  /**
+   * Public key of the specific node this service must retrieve.
+   */
+  private specificNodeKey = '';
+  /**
+   * Subscription for getting the specific node. If it has a value, indicates that the
+   * service is automatically refreshing the node info.
+   */
+  private specificNodeRefreshSubscription: Subscription;
 
   /**
-   * Get the list of the nodes connected to the hypervisor.
+   * Allows to get the node list. The list is periodically updated. It may emit null.
    */
-  getNodes(): Observable<Node[]> {
+  get nodeList(): Observable<BackendData> { return this.nodeListSubject.asObservable(); }
+  /**
+   * Allows to know if the service is currently updating the node list.
+   */
+  get updatingNodeList(): Observable<boolean> { return this.updatingNodeListSubject.asObservable(); }
+  /**
+   * Allows to get the specific node. The info is periodically updated. It may emit null.
+   */
+  get specificNode(): Observable<BackendData> { return this.specificNodeSubject.asObservable(); }
+  /**
+   * Allows to know if the service is currently updating the specific node.
+   */
+  get updatingSpecificNode(): Observable<boolean> { return this.updatingSpecificNodeSubject.asObservable(); }
+
+  /**
+   * Makes the service start updating the node list. You must call this function before
+   * using the nodeList observable.
+   */
+  startRequestingNodeList() {
+    // Get for how many ms the saved data is still valid.
+    const momentOfLastCorrectUpdate = this.nodeListSubject.value ? this.nodeListSubject.value.momentOfLastCorrectUpdate : 0;
+    const remainingTime = this.calculateRemainingTime(momentOfLastCorrectUpdate);
+
+    if (remainingTime === 0) {
+      // Get the data from the backend.
+      this.nodeListSubject.next(null);
+      this.startDataSubscription(0, true);
+    } else {
+      // Use the data obtained the last time and schedule an update after the appropriate time.
+      this.startDataSubscription(remainingTime, true);
+    }
+  }
+
+  /**
+   * Makes the service start updating a specific node. You must call this function before
+   * using the specificNode observable.
+   * @param publicKey Public key of the specific node to consult.
+   */
+  startRequestingSpecificNode(publicKey: string) {
+    // Get for how many ms the saved data is still valid.
+    const momentOfLastCorrectUpdate = this.specificNodeSubject.value ? this.specificNodeSubject.value.momentOfLastCorrectUpdate : 0;
+    const remainingTime = this.calculateRemainingTime(momentOfLastCorrectUpdate);
+
+    if (this.specificNodeKey !== publicKey || remainingTime === 0) {
+      // Get the data from the backend.
+      this.specificNodeKey = publicKey;
+      this.specificNodeSubject.next(null);
+      this.startDataSubscription(0, false);
+    } else {
+      // Use the data obtained the last time and schedule an update after the appropriate time.
+      this.startDataSubscription(remainingTime, false);
+    }
+  }
+
+  /**
+   * Calculates for how many ms the saved data is still valid before an update should be made.
+   * @param momentOfLastCorrectUpdate Moment in which the data was saved.
+   */
+  private calculateRemainingTime(momentOfLastCorrectUpdate: number): number {
+    if (momentOfLastCorrectUpdate < 1) {
+      return 0;
+    }
+
+    let refreshDelay = this.dataRefreshDelay - (Date.now() - momentOfLastCorrectUpdate);
+    if (refreshDelay < 0) {
+      refreshDelay = 0;
+    }
+
+    return refreshDelay;
+  }
+
+  /**
+   * Makes the service stop updating the node list.
+   */
+  stopRequestingNodeList() {
+    if (this.nodeListRefreshSubscription) {
+      this.nodeListRefreshSubscription.unsubscribe();
+      this.nodeListRefreshSubscription = null;
+    }
+  }
+
+  /**
+   * Makes the service stop updating the specific node.
+   */
+  stopRequestingSpecificNode() {
+    if (this.specificNodeRefreshSubscription) {
+      this.specificNodeRefreshSubscription.unsubscribe();
+      this.specificNodeRefreshSubscription = null;
+    }
+  }
+
+  /**
+   * Starts periodically updating the node list or the specific node.
+   * @param delayMs Delay before loading the data.
+   * @param gettingNodeList True for getting the node list and false for getting the specific node.
+   */
+  private startDataSubscription(delayMs: number, gettingNodeList: boolean) {
+    let updatingSubject: BehaviorSubject<boolean>;
+    let dataSubject: BehaviorSubject<BackendData>;
+    let operation: Observable<any>;
+
+    if (gettingNodeList) {
+      updatingSubject = this.updatingNodeListSubject;
+      dataSubject = this.nodeListSubject;
+      operation = this.getNodes();
+
+      if (this.nodeListRefreshSubscription) {
+        this.nodeListRefreshSubscription.unsubscribe();
+      }
+    } else {
+      updatingSubject = this.updatingSpecificNodeSubject;
+      dataSubject = this.specificNodeSubject;
+      operation = this.getNode(this.specificNodeKey);
+
+      if (this.specificNodeRefreshSubscription) {
+        this.specificNodeRefreshSubscription.unsubscribe();
+      }
+    }
+
+    const subscription = of(1).pipe(
+      // Wait the requested delay.
+      delay(delayMs),
+      // Additional steps for making sure the UI shows the animation (important in case of quick errors).
+      tap(() => updatingSubject.next(true)),
+      delay(120),
+      // Load the data.
+      flatMap(() => operation))
+    .subscribe(result => {
+      updatingSubject.next(false);
+
+      const newData: BackendData = {
+        data: result,
+        error: null,
+        momentOfLastCorrectUpdate: Date.now(),
+      };
+
+      dataSubject.next(newData);
+
+      // Schedule the next update.
+      this.startDataSubscription(this.dataRefreshDelay, gettingNodeList);
+    }, err => {
+      updatingSubject.next(false);
+
+      err = processServiceError(err);
+      const newData: BackendData = {
+        data: null,
+        error: err,
+        momentOfLastCorrectUpdate: dataSubject.value ? dataSubject.value.momentOfLastCorrectUpdate : -1,
+      };
+
+      // Schedule the next update.
+      if (dataSubject.value && dataSubject.value.momentOfLastCorrectUpdate !== -1) {
+        this.startDataSubscription(this.dataRefreshDelay, gettingNodeList);
+      } else {
+        this.startDataSubscription(this.initialErrorRetryDelay, gettingNodeList);
+      }
+
+      dataSubject.next(newData);
+    });
+
+    if (gettingNodeList) {
+      this.nodeListRefreshSubscription = subscription;
+    } else {
+      this.specificNodeRefreshSubscription = subscription;
+    }
+  }
+
+  /**
+   * Makes the service immediately refresh the node list.
+   */
+  forceNodeListRefresh() {
+    this.startDataSubscription(0, true);
+  }
+
+  /**
+   * Makes the service immediately refresh the specific node.
+   */
+  forceSpecificNodeRefresh() {
+    this.startDataSubscription(0, false);
+  }
+
+  /**
+   * Gets the list of the nodes connected to the hypervisor.
+   */
+  private getNodes(): Observable<Node[]> {
     let nodes: Node[];
 
     return this.apiService.get('visors').pipe(mergeMap((result: Node[]) => {
@@ -110,7 +357,7 @@ export class NodeService {
   /**
    * Gets the details of a specific node.
    */
-  getNode(nodeKey: string): Observable<Node> {
+  private getNode(nodeKey: string): Observable<Node> {
     let currentNode: Node;
 
     // Get the basic node data.
