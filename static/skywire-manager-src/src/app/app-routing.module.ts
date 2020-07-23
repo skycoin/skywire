71a611f424cba3925ceee7b1b4d12e65d9ed6af6
Senyoret1 <34079003+Senyoret1@users.noreply.github.com>
2020-07-23 17:32:41 -0400

    Move some folders with manager code related to the apps
    Former-commit-id: b255a5d9aef742926a2f082c9f3988d1ade2dfd8

--- static/skywire-manager-src/src/app/app-routing.module.ts
+++ static/skywire-manager-src/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ import { AppsComponent } from './components/pages/node/apps/apps.component';
 import { SidenavComponent } from './components/layout/sidenav/sidenav.component';
 import { AllTransportsComponent } from './components/pages/node/routing/all-transports/all-transports.component';
 import { AllRoutesComponent } from './components/pages/node/routing/all-routes/all-routes.component';
-import { AllAppsComponent } from './components/pages/node/apps/node-apps/all-apps/all-apps.component';
+import { AllAppsComponent } from './components/pages/node/apps/all-apps/all-apps.component';
 import { NodeInfoComponent } from './components/pages/node/node-info/node-info.component';
 
 const routes: Routes = [
