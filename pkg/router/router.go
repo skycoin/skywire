4f04ded583cbc93e7e79750edde19368cff9b439
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2020-07-27 19:31:33 +0300

    Fix dmsg bug

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -154,6 +154,8 @@ func New(n *snet.Network, config *Config) (Router, error) {
 		return nil, err
 	}
 
+	config.Logger.Warnf("Router listening dmsg at port %v", skyenv.DmsgAwaitSetupPort)
+
 	trustedVisors := make(map[cipher.PubKey]struct{})
 	for _, node := range config.SetupNodes {
 		trustedVisors[node] = struct{}{}
--- pkg/setup/setupclient/client.go
+++ pkg/setup/setupclient/client.go
@@ -10,6 +10,7 @@ import (
 	"github.com/SkycoinProject/skycoin/src/util/logging"
 
 	"github.com/SkycoinProject/skywire-mainnet/pkg/routing"
+	"github.com/SkycoinProject/skywire-mainnet/pkg/skyenv"
 	"github.com/SkycoinProject/skywire-mainnet/pkg/snet"
 )
 
@@ -46,7 +47,7 @@ func NewClient(ctx context.Context, log *logging.Logger, n *snet.Network, setupN
 
 func (c *Client) dial(ctx context.Context) (*snet.Conn, error) {
 	for _, sPK := range c.setupNodes {
-		conn, err := c.n.Dial(ctx, dmsg.Type, sPK, snet.SetupPort)
+		conn, err := c.n.Dial(ctx, dmsg.Type, sPK, skyenv.DmsgSetupPort)
 		if err != nil {
 			c.log.WithError(err).Warnf("failed to dial to setup node: setupPK(%s)", sPK)
 			continue
