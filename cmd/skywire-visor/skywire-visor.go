2acc3e46aa02adcfadcd1cdd2e50153a49afb638
Sir Darkrengarius <darkren92@gmail.com>
2020-08-03 15:07:01 +0300

    Rever skywire-visor cmd changes

--- cmd/skywire-visor/skywire-visor.go
+++ cmd/skywire-visor/skywire-visor.go
@@ -4,19 +4,9 @@ skywire visor
 package main
 
 import (
-	"fmt"
-	"net"
-
 	"github.com/SkycoinProject/skywire-mainnet/cmd/skywire-visor/commands"
 )
 
 func main() {
-	ips, err := net.LookupIP("address.resolver.skywire.cc")
-	if err != nil {
-		panic(err)
-	}
-	for _, ip := range ips {
-		fmt.Printf("IP: %v\n", ip.String())
-	}
 	commands.Execute()
 }
