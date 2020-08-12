10183af204331e46de873c6fee5e3c17add0997b
Sir Darkrengarius <darkren92@gmail.com>
2020-08-12 19:23:54 +0300

    Add logs
    Former-commit-id: 215279a8c122f19193dc23f9b4d82b4189853a5d

--- cmd/apps/vpn-client/vpn-client.go
+++ cmd/apps/vpn-client/vpn-client.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"net"
 	"os"
+	"os/exec"
 	"os/signal"
 	"syscall"
 	"time"
@@ -43,6 +44,12 @@ var (
 )
 
 func main() {
+	cmd := exec.Command("ip", "r", "add", "172.104.38.122", "via", "192.168.1.1")
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+	return
+
 	flag.Parse()
 
 	if *serverPKStr == "" {
