1ecfaf3b9317c7f98e73d71294d24806ce400282
Sir Darkrengarius <darkren92@gmail.com>
2020-08-12 22:35:04 +0300

    Change VPN client exit on missing PK

--- cmd/apps/vpn-client/vpn-client.go
+++ cmd/apps/vpn-client/vpn-client.go
@@ -48,7 +48,9 @@ func main() {
 	if *serverPKStr == "" {
 		// TODO: fix args passage for Windows
 		//*serverPKStr = "03e9019b3caa021dbee1c23e6295c6034ab4623aec50802fcfdd19764568e2958d"
-		log.Fatalln("VPN server pub key is missing")
+		log.Errorln("VPN server pub key is missing")
+		//log.Fatalln("VPN server pub key is missing")
+		return
 	}
 
 	serverPK := cipher.PubKey{}
