da900eab9a12829e70aa890769f0d0143fde18dc
Sir Darkrengarius <darkren92@gmail.com>
2020-09-01 20:00:31 +0300

    Fix route group test
    Former-commit-id: 6f100990dc3d222d853a0c2bfbacdc740faebea0

--- pkg/router/route_group_test.go
+++ pkg/router/route_group_test.go
@@ -577,6 +577,9 @@ func pushPackets(ctx context.Context, from *transport.Manager, to *RouteGroup) {
 			if !safeSend(ctx, to, payload) {
 				return
 			}
+		case routing.HandshakePacket:
+			// error won't happen with the handshake packet
+			_ = to.handlePacket(packet) //nolint:errcheck
 		default:
 			panic(fmt.Sprintf("wrong packet type %v", packet.Type()))
 		}
