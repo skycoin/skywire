974057e69fd1a9a1fd0afb394a13324497235681
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2020-09-08 01:38:44 +0300

    WIP on issue investigation

--- pkg/util/updater/updater.go
+++ pkg/util/updater/updater.go
@@ -495,6 +495,7 @@ func (u *Updater) downloadFile(url, filename string) (path string, err error) {
 		return "", err
 	}
 
+	// TODO(nkryuchkov): use syscall.Dup
 	out := io.MultiWriter(f, u.progressBar(resp.ContentLength, filename))
 
 	if _, err := io.Copy(out, resp.Body); err != nil {
