2f5066495061e4a0baf4703a950ef8a26dbf2527
Moses Narrow <moe_narrow@use.startmail.com>
2020-11-02 20:34:18 -0600

    add files via upload

--- pkg/visor/hypervisorconfig/config.go
+++ pkg/visor/hypervisorconfig/config.go
@@ -81,7 +81,7 @@ func GenerateWorkDirConfig(testenv bool) Config {
 // GenerateHomeConfig generates a config with default values and uses db from user's home folder.
 func GenerateHomeConfig(testenv bool) Config {
 	c := MakeConfig(testenv)
-	c.DBPath = filepath.Join(pathutil.HomeDir(), ".skycoin/hypervisor/users.db")
+	c.DBPath = filepath.Join(pathutil.HomeDir(), skyenv.DefaultHypervisorDB)
 	return c
 }
 
