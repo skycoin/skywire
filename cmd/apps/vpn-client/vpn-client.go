0dcf44c82c7f10149cef43592bbbe8cb8d868c4f
Sir Darkrengarius <darkren92@gmail.com>
2020-11-19 21:32:56 +0300

    Add killswitch flag, automatic reconnection
    Former-commit-id: 9713c7f088c91579cf4f2d1919d251456dac0e5e

--- cmd/apps/vpn-client/vpn-client.go
+++ cmd/apps/vpn-client/vpn-client.go
@@ -1,40 +1,18 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"net"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
-	"github.com/sirupsen/logrus"
 	"github.com/skycoin/dmsg/cipher"
-	"github.com/skycoin/dmsg/netutil"
 
 	"github.com/skycoin/skywire/internal/vpn"
 	"github.com/skycoin/skywire/pkg/app"
 	"github.com/skycoin/skywire/pkg/app/appevent"
-	"github.com/skycoin/skywire/pkg/app/appnet"
-	"github.com/skycoin/skywire/pkg/routing"
-	"github.com/skycoin/skywire/pkg/skyenv"
-)
-
-const (
-	netType = appnet.TypeSkynet
-	vpnPort = routing.Port(skyenv.VPNServerPort)
-)
-
-const (
-	serverDialInitBO = 1 * time.Second
-	serverDialMaxBO  = 10 * time.Second
-)
-
-var (
-	log = logrus.New()
-	r   = netutil.NewRetrier(log, serverDialInitBO, serverDialMaxBO, 0, 1)
 )
 
 var (
@@ -42,6 +20,7 @@ var (
 	localPKStr  = flag.String("pk", "", "Local PubKey")
 	localSKStr  = flag.String("sk", "", "Local SecKey")
 	passcode    = flag.String("passcode", "", "Passcode to authenticate connection")
+	killswitch  = flag.Bool("killswitch", false, "If set, Internet won't be restored during reconnection attempts")
 )
 
 func main() {
@@ -113,37 +92,15 @@ func main() {
 
 	fmt.Printf("Connecting to VPN server %s\n", serverPK.String())
 
-	appConn, err := dialServer(appClient, serverPK)
-	if err != nil {
-		fmt.Println("Error connecting to VPN server")
-		os.Exit(1)
-	}
-	defer func() {
-		if err := appConn.Close(); err != nil {
-			fmt.Printf("Error closing connection to the VPN server: %v\n", err)
-		}
-	}()
-
-	fmt.Printf("Dialed %s\n", appConn.RemoteAddr())
-
 	vpnClientCfg := vpn.ClientConfig{
-		Passcode: *passcode,
+		Passcode:   *passcode,
+		Killswitch: *killswitch,
+		ServerPK:   serverPK,
 	}
-	vpnClient, err := vpn.NewClient(vpnClientCfg, log, appConn)
+	vpnClient, err := vpn.NewClient(vpnClientCfg, appClient)
 	if err != nil {
 		fmt.Printf("Error creating VPN client: %v\n", err)
 	}
-
-	osSigs := make(chan os.Signal, 2)
-	sigs := []os.Signal{syscall.SIGTERM, syscall.SIGINT}
-	for _, sig := range sigs {
-		signal.Notify(osSigs, sig)
-	}
-
-	go func() {
-		<-osSigs
-		vpnClient.Close()
-	}()
 
 	var directRoutesDone bool
 	for !directRoutesDone {
@@ -173,25 +130,16 @@ func main() {
 		}
 	}()
 
-	if err := vpnClient.Serve(); err != nil {
-		fmt.Printf("Error serving VPN: %v\n", err)
-	}
-}
-
-func dialServer(appCl *app.Client, pk cipher.PubKey) (net.Conn, error) {
-	var conn net.Conn
-	err := r.Do(context.Background(), func() error {
-		var err error
-		conn, err = appCl.Dial(appnet.Addr{
-			Net:    netType,
-			PubKey: pk,
-			Port:   vpnPort,
-		})
-		return err
-	})
-	if err != nil {
-		return nil, err
+	osSigs := make(chan os.Signal, 2)
+	sigs := []os.Signal{syscall.SIGTERM, syscall.SIGINT}
+	for _, sig := range sigs {
+		signal.Notify(osSigs, sig)
 	}
 
-	return conn, nil
+	go func() {
+		<-osSigs
+		vpnClient.Close()
+	}()
+
+	vpnClient.Serve()
 }
