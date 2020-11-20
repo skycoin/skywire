24678b9d388f8909a30082ca4e5fd82f992b245d
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2020-11-20 23:42:30 +0300

    Revert accidentally deleted cases for some packets
    Former-commit-id: d4165d221f6d576f63e6363c9ae3269cc48a5a8c

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -3,6 +3,7 @@ package router
 
 import (
 	"context"
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"io"
@@ -759,7 +760,17 @@ func (r *router) forwardPacket(ctx context.Context, packet routing.Packet, rule 
 		if err != nil {
 			return err
 		}
-
+	case routing.HandshakePacket:
+		b := int(packet[routing.PacketPayloadOffset])
+		supportEncryptionVal := true
+		if b == 0 {
+			supportEncryptionVal = false
+		}
+		p = routing.MakeHandshakePacket(rule.NextRouteID(), supportEncryptionVal)
+	case routing.NetworkProbePacket:
+		timestamp := int64(binary.BigEndian.Uint64(packet[routing.PacketPayloadOffset:]))
+		throughput := int64(binary.BigEndian.Uint64(packet[routing.PacketPayloadOffset+8:]))
+		p = routing.MakeNetworkProbePacket(rule.NextRouteID(), timestamp, throughput)
 	case routing.KeepAlivePacket:
 		p = routing.MakeKeepAlivePacket(rule.NextRouteID())
 	case routing.ClosePacket:
