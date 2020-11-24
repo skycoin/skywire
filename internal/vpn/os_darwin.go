549be3f6642d6a8d29e4f060cf2b7f34692dd727
Sir Darkrengarius <darkren92@gmail.com>
2020-11-24 16:40:08 +0300

    Refactor, change route command to change TUN route
    Former-commit-id: 80e80121f332da84c82906ea83750c69cf7be5d4

--- internal/vpn/os_darwin.go
+++ internal/vpn/os_darwin.go
@@ -17,22 +17,27 @@ func SetupTUN(ifcName, ipCIDR, gateway string, mtu int) error {
 	return run("ifconfig", ifcName, ip, gateway, "mtu", strconv.Itoa(mtu), "netmask", netmask, "up")
 }
 
+// ChangeRoute changes current route to `ipCIDR` to go through the `gateway`
+// in the OS routing table.
+func ChangeRoute(ipCIDR, gateway string) error {
+	return modifyRoutingTable("change", ipCIDR, gateway)
+}
+
 // AddRoute adds route to `ipCIDR` through the `gateway` to the OS routing table.
 func AddRoute(ipCIDR, gateway string) error {
-	ip, netmask, err := parseCIDR(ipCIDR)
-	if err != nil {
-		return fmt.Errorf("error parsing IP CIDR: %w", err)
-	}
-
-	return run("route", "add", "-net", ip, gateway, netmask)
+	return modifyRoutingTable("add", ipCIDR, gateway)
 }
 
 // DeleteRoute removes route to `ipCIDR` through the `gateway` from the OS routing table.
 func DeleteRoute(ipCIDR, gateway string) error {
+	return modifyRoutingTable("delete", ipCIDR, gateway)
+}
+
+func modifyRoutingTable(action, ipCIDR, gateway string) error {
 	ip, netmask, err := parseCIDR(ipCIDR)
 	if err != nil {
 		return fmt.Errorf("error parsing IP CIDR: %w", err)
 	}
 
-	return run("route", "delete", "-net", ip, gateway, netmask)
+	return run("route", action, "-net", ip, gateway, netmask)
 }
