ce2c5a0bcdc0d400a83ebb86d09b2cd7c7457f3f
Sir Darkrengarius <darkren92@gmail.com>
2020-11-26 15:18:14 +0300

    Add ErrorWithStderr, filter out duplicate route errors for Linux

--- internal/vpn/errors.go
+++ internal/vpn/errors.go
@@ -1,7 +1,25 @@
 package vpn
 
-import "errors"
+import (
+	"errors"
+)
 
 var (
 	errCouldFindDefaultNetworkGateway = errors.New("could not find default network gateway")
 )
+
+type ErrorWithStderr struct {
+	Err    error
+	Stderr []byte
+}
+
+func NewErrorWithStderr(err error, stderr []byte) *ErrorWithStderr {
+	return &ErrorWithStderr{
+		Err:    err,
+		Stderr: stderr,
+	}
+}
+
+func (e *ErrorWithStderr) Error() string {
+	return e.Err.Error() + ": " + string(e.Stderr)
+}
