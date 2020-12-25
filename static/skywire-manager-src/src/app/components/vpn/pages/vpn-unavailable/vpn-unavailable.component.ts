b8da599a84988944ddc0da6d76f2adcce65304c5
Senyoret1 <34079003+Senyoret1@users.noreply.github.com>
2020-12-25 17:35:17 -0400

    Improvements for the Skywire VPN desktop client
    Former-commit-id: 33989893271180d15e870c65246c59770d130072

--- static/skywire-manager-src/src/app/components/vpn/pages/vpn-unavailable/vpn-unavailable.component.ts
+++ static/skywire-manager-src/src/app/components/vpn/pages/vpn-unavailable/vpn-unavailable.component.ts
@@ -8,7 +8,7 @@ import { Subscription } from 'rxjs/internal/Subscription';
   styleUrls: ['./vpn-unavailable.component.scss'],
 })
 export class VpnUnavailableComponent {
-  problem = null;
+  private problem = null;
 
   private navigationsSubscription: Subscription;
 
@@ -20,5 +20,29 @@ export class VpnUnavailableComponent {
       this.problem = queryParams.get('problem');
       setTimeout(() => this.navigationsSubscription.unsubscribe());
     });
+  }
+
+  getTitle(): string {
+    if (this.problem === 'pk') {
+      return 'vpn.error-page.text-pk';
+    } else if (this.problem === 'storage') {
+      return 'vpn.error-page.text-storage';
+    } else if (this.problem === 'pkChange') {
+      return 'vpn.error-page.text-pk-change';
+    }
+
+    return 'vpn.error-page.text';
+  }
+
+  getInfo(): string {
+    if (this.problem === 'pk') {
+      return 'vpn.error-page.more-info-pk';
+    } else if (this.problem === 'storage') {
+      return 'vpn.error-page.more-info-storage';
+    } else if (this.problem === 'pkChange') {
+      return 'vpn.error-page.more-info-pk-change';
+    }
+
+    return 'vpn.error-page.more-info';
   }
 }
