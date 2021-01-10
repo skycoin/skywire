b9f3a955cd8398d36edf53486b934a4dd7efd4de
Sir Darkrengarius <darkren92@gmail.com>
2021-01-10 18:31:57 +0300

    Add draft uninstall function

--- cmd/skywire-desktop/gui.go
+++ cmd/skywire-desktop/gui.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"syscall"
 	"time"
+
+	"github.com/skycoin/skywire/pkg/util/osutil"
 
 	"github.com/getlantern/systray"
 )
@@ -9,6 +12,8 @@ import (
 var (
 	mStartStopVisor *systray.MenuItem
 	mOpenHypervisor *systray.MenuItem
+	mUninstall      *systray.MenuItem
+	mQuit           *systray.MenuItem
 )
 
 const (
@@ -26,6 +31,7 @@ func onGUIReady() {
 
 	initStartStopVisorBtn(visorRunning)
 	initOpenHypervisorBtn(visorRunning)
+	initUninstallBtn()
 	initQuitBtn()
 
 	go handleUserInteraction()
@@ -82,33 +88,90 @@ func initOpenHypervisorBtn(visorRunning bool) {
 	}
 }
 
+func initUninstallBtn() {
+	mUninstall = systray.AddMenuItem("Uninstall", "")
+}
+
 func initQuitBtn() {
-	mQuit := systray.AddMenuItem("Quit", "")
-	go func() {
-		<-mQuit.ClickedCh
-		systray.Quit()
-	}()
+	mQuit = systray.AddMenuItem("Quit", "")
 }
 
 func handleUserInteraction() {
 	for {
 		select {
 		case <-mOpenHypervisor.ClickedCh:
-			if err := openHypervisor(); err != nil {
-				log.WithError(err).Errorln("Failed to open hypervisor")
-			}
+			handleOpenHypervisor()
 		case <-mStartStopVisor.ClickedCh:
-			visorRunning, err := isVisorRunning()
-			if err != nil {
-				log.WithError(err).Errorln("Failed to get visor state")
-				continue
-			}
-
-			if visorRunning {
-				stopVisor()
-			} else {
-				startVisor()
-			}
+			handleStartStopVisor()
+		case <-mUninstall.ClickedCh:
+			handleUninstall()
+		case <-mQuit.ClickedCh:
+			systray.Quit()
 		}
 	}
+}
+
+func handleOpenHypervisor() {
+	if err := openHypervisor(); err != nil {
+		log.WithError(err).Errorln("Failed to open hypervisor")
+	}
+}
+
+func handleStartStopVisor() {
+	visorRunning, err := isVisorRunning()
+	if err != nil {
+		log.WithError(err).Errorln("Failed to get visor state")
+		return
+	}
+
+	if visorRunning {
+		stopVisor()
+	} else {
+		startVisor()
+	}
+}
+
+func handleUninstall() {
+	mStartStopVisor.Disable()
+	mOpenHypervisor.Disable()
+	mUninstall.Disable()
+	mQuit.Disable()
+
+	/*if err := uninstall(); err != nil {
+		mUninstall.Enable()
+		log.WithError(err).Errorln("failed to uninstall skywire apps")
+		return
+	}*/
+
+	suid := syscall.Getuid()
+
+	if err := syscall.Setuid(0); err != nil {
+		mUninstall.Enable()
+		log.WithError(err).Errorln("failed to setuid 0")
+	}
+
+	cmd := "installer -pkg /Users/darkrengarius/go/src/github.com/SkycoinPro/skywire-services/scripts/mac_installer/remover.pkg -target /"
+	if err := osutil.Run("/bin/bash", "-c", cmd); err != nil {
+		mUninstall.Enable()
+		log.WithError(err).Errorln("failed to remove systray app")
+		if err := syscall.Setuid(suid); err != nil {
+			log.WithError(err).Errorln("Failed to revert uid")
+		}
+		return
+	}
+
+	if err := syscall.Setuid(suid); err != nil {
+		log.WithError(err).Errorln("Failed to revert uid")
+	}
+
+	return
+
+	cmd = "rm -rf /Applications/Skywire.app"
+	if err := osutil.Run("/bin/bash", "-c", cmd); err != nil {
+		mUninstall.Enable()
+		log.WithError(err).Errorln("failed to remove systray app")
+		return
+	}
+
+	systray.Quit()
 }
