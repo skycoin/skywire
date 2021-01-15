9b64765f700fc07aa786683cdeb8fa1cfe3aedda
Sir Darkrengarius <darkren92@gmail.com>
2021-01-15 19:54:19 +0300

    Add debug log

--- cmd/skywire-visor/commands/root.go
+++ cmd/skywire-visor/commands/root.go
@@ -98,7 +98,7 @@ func Execute() {
 func runVisor(args []string) {
 	log := initLogger(tag, syslogAddr)
 
-	log.Infoln("UPDATED VERSION")
+	log.Infoln("UPDATED VERSION 2")
 
 	if discordWebhookURL := discord.GetWebhookURLFromEnv(); discordWebhookURL != "" {
 		// Workaround for Discord logger hook. Actually, it's Info.
