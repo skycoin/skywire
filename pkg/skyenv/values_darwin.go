7d4cff910f8e664dfdd478dc8f36cbc6593e1fc3
Sir Darkrengarius <darkren92@gmail.com>
2021-01-29 18:13:33 +0300

    Make linter happy
    Former-commit-id: ecdc91a43b9b6b66f5c58fdd286480045af240f0

--- pkg/skyenv/values_darwin.go
+++ pkg/skyenv/values_darwin.go
@@ -8,6 +8,7 @@ const (
 	packageSkywirePath = "/Skywire"
 )
 
+// PackageSkywirePath gets Skywire installation folder.
 func PackageSkywirePath() string {
 	return os.Getenv("HOME") + packageSkywirePath
 }
