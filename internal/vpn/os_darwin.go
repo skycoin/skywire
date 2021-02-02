f160039f7735e15940b69a88f626da3781cc6ffd
Sir Darkrengarius <darkren92@gmail.com>
2021-02-02 23:19:54 +0300

    Add func to fetch default network interface name for OSX
    Former-commit-id: 9ad4bdc233dc1f8b70232770a3ee408a633ecc03

--- internal/vpn/os_darwin.go
+++ internal/vpn/os_darwin.go
@@ -3,11 +3,30 @@
 package vpn
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
+	"os/exec"
 	"strconv"
 	"strings"
 )
+
+const (
+	defaultNetworkInterfaceCMD = "netstat -rn | sed -n '/Internet/,/Internet6/p' | grep default | awk '{print $4}'"
+)
+
+// DefaultNetworkInterface fetches default network interface name.
+func DefaultNetworkInterface() (string, error) {
+	outputBytes, err := exec.Command("sh", "-c", defaultNetworkInterfaceCMD).Output()
+	if err != nil {
+		return "", fmt.Errorf("error running command %s: %w", defaultNetworkInterfaceCMD, err)
+	}
+
+	// just in case
+	outputBytes = bytes.TrimRight(outputBytes, "\n")
+
+	return string(outputBytes), nil
+}
 
 // SetupTUN sets the allocated TUN interface up, setting its IP, gateway, netmask and MTU.
 func SetupTUN(ifcName, ipCIDR, gateway string, mtu int) error {
