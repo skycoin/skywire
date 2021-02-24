bd63d39bfa1d0a3e739828aa484001e3d6a1b8b0
Never M <nevermn@protonmail.com>
2021-02-24 13:48:06 +0200

    Add transport setup service logic
    Former-commit-id: d533c4f8d3c1c71f1e8952d09114db7e76924e5e

--- pkg/skyenv/values.go
+++ pkg/skyenv/values.go
@@ -37,11 +37,13 @@ const (
 // Dmsg port constants.
 // TODO(evanlinjin): Define these properly. These are currently random.
 const (
-	DmsgCtrlPort       uint16 = 7   // Listening port for dmsgctrl protocol (similar to TCP Echo Protocol).
-	DmsgSetupPort      uint16 = 36  // Listening port of a setup node.
-	DmsgAwaitSetupPort uint16 = 136 // Listening port of a visor for setup operations.
-	DmsgTransportPort  uint16 = 45  // Listening port of a visor for incoming transports.
-	DmsgHypervisorPort uint16 = 46  // Listening port of a visor for incoming hypervisor connections.
+	DmsgCtrlPort                  uint16 = 7   // Listening port for dmsgctrl protocol (similar to TCP Echo Protocol).
+	DmsgSetupPort                 uint16 = 36  // Listening port of a setup node.
+	DmsgAwaitSetupPort            uint16 = 136 // Listening port of a visor for setup operations.
+	DmsgTransportPort             uint16 = 45  // Listening port of a visor for incoming transports.
+	DmsgHypervisorPort            uint16 = 46  // Listening port of a visor for incoming hypervisor connections.
+	DmsgTransportSetupServicePort uint16 = 47
+	DmsgTransportSetupVisorPort   uint16 = 48
 )
 
 // Default dmsgpty constants.
