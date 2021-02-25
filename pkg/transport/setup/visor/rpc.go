5daee94f2d4dfea34cde67311e41ed28aff14a7c
Never M <nevermn@protonmail.com>
2021-02-25 15:26:51 +0200

    Implement AddTransport RPC over dmsg for transport setup service
    Former-commit-id: e9a763f700ecf0c209740fa63a847633a246a1db

--- pkg/transport/setup/visor/rpc.go
+++ pkg/transport/setup/visor/rpc.go
@@ -1,5 +1,13 @@
 package visor
 
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/skycoin/dmsg/cipher"
+	"github.com/skycoin/skywire/pkg/transport"
+)
+
 type TestGateway struct{}
 
 type TestResult struct {
@@ -12,5 +20,36 @@ type TestRequest struct {
 
 func (r *TestGateway) TestCall(req TestRequest, res *TestResult) error {
 	res.Text = "visor response: " + req.Text
+	return nil
+}
+
+type RPCGateway struct {
+	tm *transport.Manager
+}
+
+type TransportRequest struct {
+	RemotePK cipher.PubKey
+	Type     string
+}
+
+type TransportResponse struct {
+	ID     uuid.UUID
+	Local  cipher.PubKey
+	Remote cipher.PubKey
+	Type   string
+	IsUp   bool
+}
+
+func (gw *RPCGateway) AddTransport(req TransportRequest, res *TransportResponse) error {
+	ctx := context.Background()
+	mt, err := gw.tm.SaveTransport(ctx, req.RemotePK, req.Type)
+	if err != nil {
+		return err
+	}
+	res.ID = mt.Entry.ID
+	res.Local = gw.tm.Local()
+	res.Remote = mt.Remote()
+	res.Type = mt.Type()
+	res.IsUp = mt.IsUp()
 	return nil
 }
