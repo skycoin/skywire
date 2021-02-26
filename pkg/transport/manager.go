0d0a67a51866a2a8ab9bbcf0c7af87080eada009
Sir Darkrengarius <darkren92@gmail.com>
2021-02-27 02:15:30 +0300

    Fix deadlock in managed tp's `updateStatus`

--- pkg/transport/manager.go
+++ pkg/transport/manager.go
@@ -436,13 +436,17 @@ func (tm *Manager) Transport(id uuid.UUID) *ManagedTransport {
 
 // WalkTransports ranges through all transports.
 func (tm *Manager) WalkTransports(walk func(tp *ManagedTransport) bool) {
+	tm.Logger.Infoln("ACQUIRING LOCK")
 	tm.mx.RLock()
-	for _, tp := range tm.tps {
+	tm.Logger.Infoln("ACQUIRED LOCK")
+	for id, tp := range tm.tps {
+		tm.Logger.Infof("WALKING TP %s", id.String())
 		if ok := walk(tp); !ok {
 			break
 		}
 	}
 	tm.mx.RUnlock()
+	tm.Logger.Infoln("UNLOCKED")
 }
 
 // Local returns Manager.config.PubKey
