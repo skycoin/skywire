c4663a10c5cedd72257e4280a8bb348e9ba43fb8
Sir Darkrengarius <darkren92@gmail.com>
2021-02-28 20:33:12 +0300

    Add handshake timeout
    Former-commit-id: e1db412058928665ac8aded9af1e2c66cb1d1481

--- internal/vpn/net.go
+++ internal/vpn/net.go
@@ -5,12 +5,30 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"time"
 
 	"github.com/skycoin/dmsg/cipher"
 	"github.com/skycoin/dmsg/noise"
 
 	"github.com/skycoin/skywire/pkg/app/appnet"
 )
+
+// WriteJSONWithTimeout marshals `data` and sends it over the `conn` with the specified write `timeout`.
+func WriteJSONWithTimeout(conn net.Conn, data interface{}, timeout time.Duration) error {
+	if err := conn.SetWriteDeadline(time.Now().Add(timeout)); err != nil {
+		return fmt.Errorf("failed to set write deadline: %w", err)
+	}
+
+	if err := WriteJSON(conn, data); err != nil {
+		return err
+	}
+
+	if err := conn.SetWriteDeadline(time.Time{}); err != nil {
+		return fmt.Errorf("failed to remove write deadline: %w", err)
+	}
+
+	return nil
+}
 
 // WriteJSON marshals `data` and sends it over the `conn`.
 func WriteJSON(conn net.Conn, data interface{}) error {
@@ -31,6 +49,24 @@ func WriteJSON(conn net.Conn, data interface{}) error {
 	return nil
 }
 
+// ReadJSONWithTimeout reads portion of data from the `conn` and unmarshals it into `data` with the
+// specified read `timeout`.
+func ReadJSONWithTimeout(conn net.Conn, data interface{}, timeout time.Duration) error {
+	if err := conn.SetReadDeadline(time.Now().Add(timeout)); err != nil {
+		return fmt.Errorf("failed to set read deadline: %w", err)
+	}
+
+	if err := ReadJSON(conn, data); err != nil {
+		return err
+	}
+
+	if err := conn.SetReadDeadline(time.Time{}); err != nil {
+		return fmt.Errorf("failed to remove read deadline: %w", err)
+	}
+
+	return nil
+}
+
 // ReadJSON reads portion of data from the `conn` and unmarshals it into `data`.
 func ReadJSON(conn net.Conn, data interface{}) error {
 	const bufSize = 1024
@@ -40,6 +76,7 @@ func ReadJSON(conn net.Conn, data interface{}) error {
 	for {
 		n, err := conn.Read(buf)
 		if err != nil {
+			fmt.Printf("FAILED TO READ JSON: %v\n", err)
 			return fmt.Errorf("error reading data: %w", err)
 		}
 
