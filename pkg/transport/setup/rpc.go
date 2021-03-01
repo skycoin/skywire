cfa163fb105cb3a8f9015e499c2a43b1fb82c4d6
Never M <nevermn@protonmail.com>
2021-03-01 17:59:04 +0200

    Improve transport setup logging
    Former-commit-id: 82c6b3e60d45b4525085b5462e0a0c2636a04248

--- pkg/transport/setup/rpc.go
+++ pkg/transport/setup/rpc.go
@@ -6,13 +6,15 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/skycoin/dmsg/cipher"
+	"github.com/skycoin/skycoin/src/util/logging"
 
 	"github.com/skycoin/skywire/pkg/transport"
 )
 
 // TransportGateway that exposes methods to be used via RPC
 type TransportGateway struct {
-	tm *transport.Manager
+	tm  *transport.Manager
+	log *logging.Logger
 }
 
 // TransportRequest to perform an action over RPC
@@ -43,9 +45,11 @@ type BoolResponse struct {
 // AddTransport specified by request
 func (gw *TransportGateway) AddTransport(req TransportRequest, res *TransportResponse) error {
 	ctx := context.Background()
+	gw.log.WithField("PK", req.RemotePK).WithField("type", req.Type).Info("Adding transport")
 	// todo: pass transport type "skycoin"
 	mt, err := gw.tm.SaveTransport(ctx, req.RemotePK, req.Type)
 	if err != nil {
+		gw.log.WithError(err).Error("Cannot save transport")
 		return err
 	}
 	res.ID = mt.Entry.ID
@@ -61,6 +65,7 @@ var ErrNotFound = errors.New("transport not found")
 
 // RemoveTransport removes all transports that match given request
 func (gw *TransportGateway) RemoveTransport(req UUIDRequest, res *BoolResponse) error {
+	gw.log.WithField("ID", req.ID).Info("Removing transport")
 	gw.tm.DeleteTransport(req.ID)
 	res.Result = true
 	return nil
