1c3e8e224429fe3248ebcc9e211ef2edcd572106
Sir Darkrengarius <darkren92@gmail.com>
2021-03-17 01:37:23 +0300

    Remove error on windows for local IP

--- pkg/util/netutil/netutil_windows.go
+++ pkg/util/netutil/netutil_windows.go
@@ -2,13 +2,7 @@
 
 package netutil
 
-import "errors"
-
-var (
-	errMethodNotSupported = errors.New("this methods is not supported for this OS")
-)
-
 // DefaultNetworkInterface fetches default network interface name.
 func DefaultNetworkInterface() (string, error) {
-	return "", errMethodNotSupported
+	return "", nil
 }
