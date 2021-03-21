7fb27134601cf16ff181ae14517493da15eff302
Senyoret1 <34079003+Senyoret1@users.noreply.github.com>
2021-03-21 19:03:53 -0400

    Show correct units in the VPN client

--- static/skywire-manager-src/src/app/pipes/auto-scale.pipe.ts
+++ static/skywire-manager-src/src/app/pipes/auto-scale.pipe.ts
@@ -21,6 +21,10 @@ class AutoScalePipeParams {
    * If true, the numeric value will have at most 1 decimal.
    */
   limitDecimals: boolean;
+  /**
+   * If the data must be shown in bits (true) or bytes (false).
+   */
+  useBits: boolean;
 }
 
 /**
@@ -30,17 +34,43 @@ class AutoScalePipeParams {
   name: 'autoScale'
 })
 export class AutoScalePipe implements PipeTransform {
+  private static readonly accumulatedMeasurements = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  private static readonly measurementsPerSec = ['B/s', 'KB/s', 'MB/s', 'GB/s', 'TB/s', 'PB/s', 'EB/s', 'ZB/s', 'YB/s'];
+  private static readonly accumulatedMeasurementsInBits = ['b', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb', 'Eb', 'Zb', 'Yb'];
+  private static readonly measurementsPerSecInBits = ['b/s', 'Kb/s', 'Mb/s', 'Gb/s', 'Tb/s', 'Pb/s', 'Eb/s', 'Zb/s', 'Yb/s'];
 
   transform(value: any, params: AutoScalePipeParams): any {
-    const accumulatedMeasurements = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-    const measurementsPerSec = ['B/s', 'KB/s', 'MB/s', 'GB/s', 'TB/s', 'PB/s', 'EB/s', 'ZB/s', 'YB/s'];
+    let useBytes = true;
+
     /**
-     * Units to use, as per requested.
+     * Load the requested unit labels.
      */
-    const measurements = !params || !params.showPerSecond ? accumulatedMeasurements : measurementsPerSec;
+    let measurements: String[];
+    if (!params) {
+      measurements = AutoScalePipe.accumulatedMeasurements;
+    } else {
+      if (params.showPerSecond) {
+        if (params.useBits) {
+          measurements = AutoScalePipe.measurementsPerSecInBits;
+          useBytes = false;
+        } else {
+          measurements = AutoScalePipe.measurementsPerSec;
+        }
+      } else {
+        if (params.useBits) {
+          measurements = AutoScalePipe.accumulatedMeasurementsInBits;
+          useBytes = false;
+        } else {
+          measurements = AutoScalePipe.accumulatedMeasurements;
+        }
+      }
+    }
 
     // Calculate the number and the unit.
     let val = new BigNumber(value);
+    if (!useBytes) {
+      val = val.multipliedBy(8);
+    }
     let measurement = measurements[0];
     let currentIndex = 0;
     while (val.dividedBy(1024).isGreaterThan(1)) {
