8e8ea38333b066f2a04ecc4d248e6783e05d77eb
Never M <nevermn@protonmail.com>
2021-04-01 17:28:06 +0300

    Add async module init todos

--- pkg/visor/init_windows.go
+++ pkg/visor/init_windows.go
@@ -5,6 +5,7 @@ package visor
 func initDmsgpty(ctx context.Context, log *logging.Logger) error {
 	conf := v.conf.Dmsgpty
 
+	// todo: remove? why telling user to configure smth they cannot use
 	if conf == nil {
 		log.Info("'dmsgpty' is not configured, skipping.")
 		return nil
