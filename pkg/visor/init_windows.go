569c1fd13bf73747a030a210f6ad219e2e24c778
Never M <nevermn@protonmail.com>
2021-04-06 12:34:57 +0300

    Remove windows dmsg_pty init config message
    Former-commit-id: d7abce472ffc48da8cde4ab4478da0ec7bd6089b

--- pkg/visor/init_windows.go
+++ pkg/visor/init_windows.go
@@ -3,14 +3,6 @@
 package visor
 
 func initDmsgpty(ctx context.Context, log *logging.Logger) error {
-	conf := v.conf.Dmsgpty
-
-	// todo: remove? why telling user to configure smth they cannot use
-	if conf == nil {
-		log.Info("'dmsgpty' is not configured, skipping.")
-		return nil
-	}
-
 	log.Error("dmsgpty is not supported on windows.")
 	return nil
 }
