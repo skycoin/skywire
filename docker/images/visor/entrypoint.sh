fa46abfd5ba222abc83278ea49d010b61c7f17d6
Alexander Adhyatma <alexadhyatma@mailbox.org>
2021-05-03 08:30:11 +0700

    added README for instruction on running dockerized skywire
    Former-commit-id: b4871f24ae6975224dbbb3ade66070987756147f

--- docker/images/visor/entrypoint.sh
+++ docker/images/visor/entrypoint.sh
@@ -1,15 +1,33 @@
 #!/bin/sh
 
+test -d /opt/config || {
+  echo "no docker volume mounted, exiting..."
+  exit 1
+}
+
+if [ "$#" -ne 1 ]; then
+  test -f /opt/config/skywire-config.json || {
+    echo "no config found, generating one...." &&
+      /bin/skywire-cli visor gen-config -o /opt/config/skywire-config.json -r --is-hypervisor &&
+      #      sed -i 's/localhost//g' /opt/config/skywire-config.json &&
+      echo "config generated." &&
+      exit 0
+  }
+fi
+
 cmd="$(echo "$1" | tr -d '[:space:]')"
+shift 1
+
+echo "$@"
 
 case "$cmd" in
 skywire-visor)
-  ./"$cmd" -- "$@"
+  ./"$cmd" -c /opt/config/skywire-config.json "$@"
   ;;
 skywire-cli)
-  /bin/skywire-cli -- "$@"
+  /bin/skywire-cli "$@"
   ;;
-*)
-  /bin/apps/"$cmd" -- "$@"
+skychat | skysocks | skysocks-client)
+  /apps/"$cmd" "$@"
   ;;
 esac
