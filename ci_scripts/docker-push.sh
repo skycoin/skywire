bcaed16ce69d40c6da089bf6ee7750c7d35792be
Alexander Adhyatma <alexadhyatma@mailbox.org>
2021-05-04 13:32:37 +0700

    added ldflags buildinfo to docker image build argument
    Former-commit-id: 7d15f43ca3f56d6800ebde52d7fc7344c3ebad76

--- ci_scripts/docker-push.sh
+++ ci_scripts/docker-push.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+function extra_ldflags() {
+  local build_dir="github.com/skycoin/dmsg/buildinfo"
+  local version="$(git describe)"
+  local commit="$(git rev-parse HEAD)"
+  local build_date="$(date -u "+%Y-%m-%d%T%H:%M:%SZ")"
+  echo "-X ${build_dir}.version=${version} -X ${build_dir}.commit=${commit} -X ${build_dir}.date=${build_date}"
+}
+
 function print_usage() {
   echo "Use: $0 [-t <docker_image_tag_name>] [-p | -b]"
   echo "use -p for push (it builds and push the image)"
@@ -14,13 +22,14 @@ fi
 function docker_build() {
   docker image build \
     --tag=skycoin/skywire:"$tag" \
+    --build-arg BUILDINFO_LDFLAGS="$(extra_ldflags)" \
     -f ./docker/images/visor/Dockerfile .
 }
 
 function docker_push() {
-    docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-    docker tag skycoin/skywire:"$tag" skycoin/skywire:"$tag"
-    docker image push skycoin/skywire:"$tag"
+  docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+  docker tag skycoin/skywire:"$tag" skycoin/skywire:"$tag"
+  docker image push skycoin/skywire:"$tag"
 }
 
 while getopts ":t:pb" o; do
@@ -29,6 +38,8 @@ while getopts ":t:pb" o; do
     tag="$(echo "${OPTARG}" | tr -d '[:space:]')"
     if [[ $tag == "develop" ]]; then
       tag="test"
+    elif [[ $tag == "master" ]]; then
+      tag="latest"
     fi
     ;;
   p)
