e81538686818c514e9b2db672aed133150d0459d
Alexander Adhyatma <alexadhyatma@mailbox.org>
2021-06-10 15:26:46 +0700

    TODO: pass SIGINT / TERM

--- docker/images/visor/entrypoint.sh
+++ docker/images/visor/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+## PID of skywire-visor
+pid=0
+
 default_config_path=/opt/skywire/config.json
 
 gen_default_config() {
@@ -9,6 +12,15 @@ gen_default_config() {
   echo "config generated"
 }
 
+sigint_handler() {
+  if [ $pid -ne 0 ]; then
+    kill -SIGINT "$pid"
+    wait "$pid"
+  fi
+}
+
+trap 'kill ${!}; sigint_handler' INT TERM
+
 cmd="$(echo "$1" | tr -d '[:space:]')"
 shift 1
 
@@ -16,15 +28,18 @@ case "$cmd" in
 skywire-visor)
   case "$1" in
   -c)
-    /release/"$cmd" "$@"
+    /release/"$cmd" "$@" &
+    pid="$!"
     ;;
   *)
     gen_default_config
     /release/"$cmd" -c "$default_config_path" "$@"
+    pid="$!"
     ;;
   esac
   ;;
 skywire-cli)
   /release/"$cmd" "$@"
+  pid="$!"
   ;;
 esac
