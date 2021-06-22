30dbf408a3fc124bf399aef3044d1a9ce4428675
Alexander Adhyatma <alexadhyatma@mailbox.org>
2021-06-22 12:05:20 +0700

    added hypervisor dmsgpty for windows

--- pkg/visor/hypervisor.go
+++ pkg/visor/hypervisor.go
@@ -22,6 +22,7 @@ import (
 	"github.com/skycoin/dmsg"
 	"github.com/skycoin/dmsg/buildinfo"
 	"github.com/skycoin/dmsg/cipher"
+	"github.com/skycoin/dmsg/dmsgpty"
 	"github.com/skycoin/dmsg/httputil"
 	"github.com/skycoin/skycoin/src/util/logging"
 	"nhooyr.io/websocket"
@@ -1520,4 +1521,23 @@ func (hv *Hypervisor) serveDmsg(ctx context.Context, log *logging.Logger) {
 	}()
 	log.WithField("addr", dmsg.Addr{PK: hv.c.PK, Port: hv.c.DmsgPort}).
 		Info("Serving RPC client over dmsg.")
+}
+
+// dmsgPtyUI servers as a wrapper for `*dmsgpty.UI`. this way source file with
+// `*dmsgpty.UI` will be included for Unix systems and excluded for Windows.
+type dmsgPtyUI struct {
+	PtyUI *dmsgpty.UI
+}
+
+func setupDmsgPtyUI(dmsgC *dmsg.Client, visorPK cipher.PubKey) *dmsgPtyUI {
+	ptyDialer := dmsgpty.DmsgUIDialer(dmsgC, dmsg.Addr{PK: visorPK, Port: skyenv.DmsgPtyPort})
+	return &dmsgPtyUI{
+		PtyUI: dmsgpty.NewUI(ptyDialer, dmsgpty.DefaultUIConfig()),
+	}
+}
+
+func (hv *Hypervisor) getPty() http.HandlerFunc {
+	return hv.withCtx(hv.visorCtx, func(w http.ResponseWriter, r *http.Request, ctx *httpCtx) {
+		ctx.PtyUI.PtyUI.Handler()(w, r)
+	})
 }
