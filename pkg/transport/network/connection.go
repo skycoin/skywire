8c3cc71f36c7ca27ebdea9b10b45db307247e0cc
Never M <nevermn@protonmail.com>
2021-06-23 09:50:44 +0300

    Outline new snet structure inside transport package
    Former-commit-id: 42c71fe2322ae744d1aab88b515c608efaf8b5a1

---
+++ pkg/transport/network/connection.go
@@ -0,0 +1,11 @@
+package network
+
+// Conn represents a skywire network connection. It wraps net.Conn
+// with other skywire-specific data
+type Conn struct {
+}
+
+// Listener represents a skywire network listener. It wraps net.Listener
+// with other skywire-specific data
+type Listener struct {
+}
---
+++ pkg/transport/network/sudph.go
@@ -0,0 +1,4 @@
+package network
+
+type sudphClient struct {
+}
