0e4712ac6a39eddae05afdc218effb8b81129c2f
Never M <nevermn@protonmail.com>
2021-06-29 15:48:34 +0300

    Get rid of transport close callback

--- pkg/app/appevent/utils.go
+++ pkg/app/appevent/utils.go
@@ -9,6 +9,15 @@ func (eb *Broadcaster) SendTCPDial(ctx context.Context, remoteNet, remoteAddr st
 	eb.sendEvent(ctx, event)
 }
 
+// SendTPClose sends transport close event
+func (eb *Broadcaster) SendTPClose(ctx context.Context, netType, addr string) {
+	data := TCPCloseData{RemoteNet: string(netType), RemoteAddr: addr}
+	event := NewEvent(TCPClose, data)
+	if err := eb.Broadcast(context.Background(), event); err != nil {
+		eb.log.WithError(err).Errorln("Failed to broadcast TCPClose event")
+	}
+}
+
 func (eb *Broadcaster) sendEvent(_ context.Context, event *Event) {
 	err := eb.Broadcast(context.Background(), event) //nolint:errcheck
 	if err != nil {
