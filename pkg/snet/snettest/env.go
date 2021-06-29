677233416061ced8cb20080677f12e515c6619a8
Never M <nevermn@protonmail.com>
2021-06-29 13:05:48 +0300

    Remove address resolver from snet

--- pkg/snet/snettest/env.go
+++ pkg/snet/snettest/env.go
@@ -4,22 +4,13 @@ package snettest
 // it doesn't test sending data, but it probably should
 // for now (while refactoring snet) dmsg parts are cut out
 import (
-	"strconv"
 	"testing"
 
 	"github.com/skycoin/dmsg"
 	"github.com/skycoin/dmsg/cipher"
 	"github.com/skycoin/dmsg/disc"
-	"github.com/skycoin/skycoin/src/util/logging"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"golang.org/x/net/nettest"
 
 	"github.com/skycoin/skywire/pkg/snet"
-	"github.com/skycoin/skywire/pkg/snet/arclient"
-	"github.com/skycoin/skywire/pkg/snet/directtp"
-	"github.com/skycoin/skywire/pkg/snet/directtp/pktable"
-	"github.com/skycoin/skywire/pkg/snet/directtp/tptypes"
 )
 
 // KeyPair holds a public/private key pair.
@@ -55,136 +46,5 @@ type Env struct {
 // NewEnv creates a `network.Network` test environment.
 // `nPairs` is the public/private key pairs of all the `network.Network`s to be created.
 func NewEnv(t *testing.T, keys []KeyPair, networks []string) *Env {
-	// Prepare `dmsg`.
-	dmsgD := disc.NewMock(0)
-	dmsgS, dmsgSErr := createDmsgSrv(t, dmsgD)
-
-	const baseSTCPPort = 7033
-
-	tableEntries := make(map[cipher.PubKey]string)
-	for i, pair := range keys {
-		tableEntries[pair.PK] = "127.0.0.1:" + strconv.Itoa(baseSTCPPort+i)
-	}
-
-	table := pktable.NewTable(tableEntries)
-
-	var hasStcp, hasStcpr, hasSudph bool
-
-	for _, network := range networks {
-		switch network {
-		case tptypes.STCP:
-			hasStcp = true
-		case tptypes.STCPR:
-			hasStcpr = true
-		case tptypes.SUDPH:
-			hasSudph = true
-		}
-	}
-
-	// Prepare `snets`.
-	ns := make([]*snet.Network, len(keys))
-
-	const stcpBasePort = 7033
-
-	for i, pairs := range keys {
-		networkConfigs := snet.NetworkConfigs{
-			STCP: &snet.STCPConfig{
-				LocalAddr: "127.0.0.1:" + strconv.Itoa(stcpBasePort+i),
-			},
-		}
-
-		clients := snet.NetworkClients{
-			Direct: make(map[string]directtp.Client),
-		}
-
-		addressResolver := new(arclient.MockAPIClient)
-
-		if hasStcp {
-			conf := directtp.Config{
-				Type:      tptypes.STCP,
-				PK:        pairs.PK,
-				SK:        pairs.SK,
-				Table:     table,
-				LocalAddr: networkConfigs.STCP.LocalAddr,
-			}
-
-			clients.Direct[tptypes.STCP] = directtp.NewClient(conf, logging.NewMasterLogger())
-		}
-
-		if hasStcpr {
-			conf := directtp.Config{
-				Type:            tptypes.STCPR,
-				PK:              pairs.PK,
-				SK:              pairs.SK,
-				AddressResolver: addressResolver,
-			}
-
-			clients.Direct[tptypes.STCPR] = directtp.NewClient(conf, logging.NewMasterLogger())
-		}
-
-		if hasSudph {
-			conf := directtp.Config{
-				Type:            tptypes.SUDPH,
-				PK:              pairs.PK,
-				SK:              pairs.SK,
-				AddressResolver: addressResolver,
-			}
-
-			clients.Direct[tptypes.SUDPH] = directtp.NewClient(conf, logging.NewMasterLogger())
-		}
-
-		snetConfig := snet.Config{
-			PubKey:         pairs.PK,
-			SecKey:         pairs.SK,
-			NetworkConfigs: networkConfigs,
-		}
-
-		// todo: dmsg is not set up here
-		n := snet.NewRaw(snetConfig, clients, nil, addressResolver)
-		require.NoError(t, n.Init())
-		ns[i] = n
-	}
-
-	// Prepare teardown closure.
-	teardown := func() {
-		for _, n := range ns {
-			assert.NoError(t, n.Close())
-		}
-		assert.NoError(t, dmsgS.Close())
-		for err := range dmsgSErr {
-			assert.NoError(t, err)
-		}
-	}
-
-	return &Env{
-		DmsgD:    dmsgD,
-		DmsgS:    dmsgS,
-		Keys:     keys,
-		Nets:     ns,
-		teardown: teardown,
-	}
-}
-
-// Teardown shutdowns the Env.
-func (e *Env) Teardown() { e.teardown() }
-
-func createDmsgSrv(t *testing.T, dc disc.APIClient) (srv *dmsg.Server, srvErr <-chan error) {
-	pk, sk, err := cipher.GenerateDeterministicKeyPair([]byte("s"))
-	require.NoError(t, err)
-
-	l, err := nettest.NewLocalListener("tcp")
-	require.NoError(t, err)
-
-	srv = dmsg.NewServer(pk, sk, dc, &dmsg.ServerConfig{MaxSessions: 100}, nil)
-
-	errCh := make(chan error, 1)
-
-	go func() {
-		errCh <- srv.Serve(l, "")
-		close(errCh)
-	}()
-
-	<-srv.Ready()
-
-	return srv, errCh
+	return nil
 }
