08eb9e752af413feec8e043fe8d94e948b87a753
ersonp <ersonpereiracr7@gmail.com>
2021-07-06 19:56:10 +0530

    Init sudph seperatly
    Former-commit-id: bb4a85a2619b48fd87f0e681d4b0ff7254cb536c

--- pkg/visor/visor.go
+++ pkg/visor/visor.go
@@ -125,6 +125,7 @@ func NewVisor(conf *visorconfig.V1, restartCtx *restart.Context) (*Visor, bool) 
 		mainModule = hv
 	}
 	mainModule.InitConcurrent(ctx)
+	sc.InitConcurrent(ctx)
 	if err := mainModule.Wait(ctx); err != nil {
 		log.Error(err)
 		return nil, false
