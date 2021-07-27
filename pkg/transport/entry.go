0349358ad0fa75721dd2f4b3b238aaf54df3b21a
Never M <nevermn@protonmail.com>
2021-07-27 12:46:18 +0300

    Remove transport status
    Former-commit-id: 8a60d7ea23f239ca8500eeef80cc2a4a95dd14f9

--- pkg/transport/entry.go
+++ pkg/transport/entry.go
@@ -176,18 +176,6 @@ func NewSignedEntry(entry *Entry, pk cipher.PubKey, secKey cipher.SecKey) (*Sign
 	return se, se.Sign(pk, secKey)
 }
 
-// Status represents the current state of a Transport from a Transport's single edge.
-// Each Transport will have two perspectives; one from each of it's edges.
-type Status struct {
-
-	// ID is the Transport ID that identifies the Transport that this status is regarding.
-	ID uuid.UUID `json:"t_id"`
-
-	// IsUp represents whether the Transport is up.
-	// A Transport that is down will fail to forward Packets.
-	IsUp bool `json:"is_up"`
-}
-
 // EntryWithStatus stores Entry and Statuses returned by both Edges.
 type EntryWithStatus struct {
 	Entry      *Entry  `json:"entry"`
