5517745b56311fcab437fc37e9141dab8f8ff169
Alexander Adhyatma <alexadhyatma@mailbox.org>
2021-08-10 19:05:01 +0700

    run elevated
    Former-commit-id: dc8afe2cd1b02bd973a8f2c653345b39c129b48f

--- pkg/util/osutil/run.go
+++ pkg/util/osutil/run.go
@@ -7,19 +7,46 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"runtime"
 	"strings"
 )
 
 // Run runs binary `bin` with `args`.
 func Run(bin string, args ...string) error {
-	return run(bin, os.Stdout, args...)
+	return run(bin, os.Stdout, false, args...)
+}
+
+// RunElevated runs binary with escalation (admin / root)
+func RunElevated(bin string, args ...string) error {
+	return run(bin, os.Stdout, true, args...)
+}
+
+// RunElevatedWithResultReader runs binary `bin` with `args` returning stdout contents as `io.Reader`
+func RunElevatedWithResultReader(bin string, args ...string) (io.Reader, error) {
+	stdout := bytes.NewBuffer(nil)
+
+	return stdout, run(bin, stdout, true, args...)
+}
+
+// RunElevatedWithResult runs binary `bin` with `args` returning stdout contents as bytes.
+func RunElevatedWithResult(bin string, args ...string) ([]byte, error) {
+	stdout, err := RunElevatedWithResultReader(bin, args...)
+	if err != nil {
+		return nil, err
+	}
+	stdoutBytes, err := ioutil.ReadAll(stdout)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read stdout: %w", err)
+	}
+
+	return stdoutBytes, nil
 }
 
 // RunWithResultReader runs binary `bin` with `args` returning stdout contents as `io.Reader`.
 func RunWithResultReader(bin string, args ...string) (io.Reader, error) {
 	stdout := bytes.NewBuffer(nil)
 
-	return stdout, run(bin, stdout, args...)
+	return stdout, run(bin, stdout, false, args...)
 }
 
 // RunWithResult runs binary `bin` with `args` returning stdout contents as bytes.
@@ -37,10 +64,28 @@ func RunWithResult(bin string, args ...string) ([]byte, error) {
 	return stdoutBytes, nil
 }
 
-func run(bin string, stdout io.Writer, args ...string) error {
-	fullCmd := bin + " " + strings.Join(args, " ")
+func run(bin string, stdout io.Writer, withEscalate bool, args ...string) error {
+	binArgs := []string{bin}
+	binArgs = append(binArgs, args...)
 
-	cmd := exec.Command(bin, args...) //nolint:gosec
+	var fullCmd string
+	var cmd *exec.Cmd
+	if withEscalate {
+		switch runtime.GOOS {
+		case "linux":
+			fullCmd = "pkexec" + bin + " " + strings.Join(args, " ")
+			cmd = exec.Command("pkexec", binArgs...) //nolint:gosec
+		case "windows":
+			fullCmd = "runas" + bin + " " + strings.Join(args, " ")
+			cmd = exec.Command("runas", binArgs...) //nolint:gosec
+		default:
+			fullCmd = "sudo" + bin + " " + strings.Join(args, " ")
+			cmd = exec.Command("sudo", binArgs...) //nolint:gosec
+		}
+	} else {
+		fullCmd = bin + " " + strings.Join(args, " ")
+		cmd = exec.Command(bin, args...)
+	}
 
 	stderrBuf := bytes.NewBuffer(nil)
 
