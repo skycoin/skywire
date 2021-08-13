35fad65df85d28364c2077c086051e668792b722
Alexander Adhyatma <alexadhyatma@mailbox.org>
2021-08-13 14:54:04 +0700

    os_linux_gui run with elevated permission

--- internal/vpn/os_linux_gui.go
+++ internal/vpn/os_linux_gui.go
@@ -1,4 +1,4 @@
-//+build linux,!systray
+//+build systray,linux
 
 package vpn
 
@@ -13,11 +13,11 @@ import (
 
 // SetupTUN sets the allocated TUN interface up, setting its IP, gateway, netmask and MTU.
 func SetupTUN(ifcName, ipCIDR, gateway string, mtu int) error {
-	if err := osutil.Run("ip", "a", "add", ipCIDR, "dev", ifcName); err != nil {
+	if err := osutil.RunElevated("ip", "a", "add", ipCIDR, "dev", ifcName); err != nil {
 		return fmt.Errorf("error assigning IP: %w", err)
 	}
 
-	if err := osutil.Run("ip", "link", "set", "dev", ifcName, "mtu", strconv.Itoa(mtu)); err != nil {
+	if err := osutil.RunElevated("ip", "link", "set", "dev", ifcName, "mtu", strconv.Itoa(mtu)); err != nil {
 		return fmt.Errorf("error setting MTU: %w", err)
 	}
 
@@ -26,7 +26,7 @@ func SetupTUN(ifcName, ipCIDR, gateway string, mtu int) error {
 		return fmt.Errorf("error parsing IP CIDR: %w", err)
 	}
 
-	if err := osutil.Run("ip", "link", "set", ifcName, "up"); err != nil {
+	if err := osutil.RunElevated("ip", "link", "set", ifcName, "up"); err != nil {
 		return fmt.Errorf("error setting interface up: %w", err)
 	}
 
@@ -40,12 +40,12 @@ func SetupTUN(ifcName, ipCIDR, gateway string, mtu int) error {
 // ChangeRoute changes current route to `ip` to go through the `gateway`
 // in the OS routing table.
 func ChangeRoute(ip, gateway string) error {
-	return osutil.Run("ip", "r", "change", ip, "via", gateway)
+	return osutil.RunElevated("ip", "r", "change", ip, "via", gateway)
 }
 
 // AddRoute adds route to `ip` with `netmask` through the `gateway` to the OS routing table.
 func AddRoute(ip, gateway string) error {
-	err := osutil.Run("ip", "r", "add", ip, "via", gateway)
+	err := osutil.RunElevated("ip", "r", "add", ip, "via", gateway)
 
 	var e *osutil.ErrorWithStderr
 	if errors.As(err, &e) {
@@ -59,5 +59,5 @@ func AddRoute(ip, gateway string) error {
 
 // DeleteRoute removes route to `ip` with `netmask` through the `gateway` from the OS routing table.
 func DeleteRoute(ip, gateway string) error {
-	return osutil.Run("ip", "r", "del", ip, "via", gateway)
+	return osutil.RunElevated("ip", "r", "del", ip, "via", gateway)
 }
