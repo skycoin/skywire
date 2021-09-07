db8d9de4c3ba71d2a1e2a0a2b698d3509eb8fd13
Alexander Adhyatma <alexadhyatma@mailbox.org>
2021-09-08 03:56:49 +0700

    fixes restart test and hypervisor test windows

--- pkg/visor/hypervisor_test.go
+++ pkg/visor/hypervisor_test.go
@@ -36,7 +36,7 @@ func TestNewNode(t *testing.T) {
 	confDir, err := ioutil.TempDir(os.TempDir(), "SWHV")
 	require.NoError(t, err)
 
-	config.DBPath = filepath.Join(confDir, "users.db")
+	config.DBPath = filepath.Join(confDir, "users_test.db")
 
 	t.Run("no_access_without_login", func(t *testing.T) {
 		testNodeNoAccessWithoutLogin(t, config)
