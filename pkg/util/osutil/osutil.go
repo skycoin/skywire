48d6bdcfe40d008fcfab368d16aea2ead0a0a273
Alexander Adhyatma <alexadhyatma@mailbox.org>
2021-09-08 00:01:25 +0700

    fixes formatting, config, and socket unlink

--- pkg/util/osutil/osutil.go
+++ pkg/util/osutil/osutil.go
@@ -1,7 +1,6 @@
 package osutil
 
 import (
-	"errors"
 	"syscall"
 )
 
@@ -9,8 +8,7 @@ import (
 func UnlinkSocketFiles(socketFiles ...string) error {
 	for _, f := range socketFiles {
 		if err := syscall.Unlink(f); err != nil {
-			// todo: check for specific unix error and use errors.Is instead of string contains
-			if !errors.Is(err, syscall.ERROR_FILE_NOT_FOUND) && !errors.Is(err, syscall.ERROR_PATH_NOT_FOUND) {
+			if pathErrCheck(err) {
 				return err
 			}
 		}
---
+++ pkg/util/osutil/path_check_unix.go
@@ -0,0 +1,12 @@
+//+build !windows
+
+package osutil
+
+import (
+	"errors"
+	"io/fs"
+)
+
+func pathErrCheck(err error) bool {
+	return !errors.Is(err, fs.ErrNotExist)
+}
