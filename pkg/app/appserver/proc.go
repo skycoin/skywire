d5044711491c43fe6136843e2d7cbabea8dbd3d0
ersonp <ersonpereiracr7@gmail.com>
2021-09-08 13:28:59 +0530

    Chane appdisc to updatedisc

--- pkg/app/appserver/proc.go
+++ pkg/app/appserver/proc.go
@@ -16,8 +16,8 @@ import (
 	"github.com/skycoin/skycoin/src/util/logging"
 
 	"github.com/skycoin/skywire/pkg/app/appcommon"
-	"github.com/skycoin/skywire/pkg/app/appdisc"
 	"github.com/skycoin/skywire/pkg/app/appnet"
+	"github.com/skycoin/skywire/pkg/app/updatedisc"
 )
 
 var (
@@ -29,7 +29,7 @@ var (
 // communication.
 // TODO(evanlinjin): In the future, we will implement the ability to run multiple instances (procs) of a single app.
 type Proc struct {
-	disc appdisc.Updater // app discovery client
+	disc updatedisc.Updater // app discovery client
 	conf appcommon.ProcConfig
 	log  *logging.Logger
 
@@ -57,7 +57,7 @@ type Proc struct {
 }
 
 // NewProc constructs `Proc`.
-func NewProc(mLog *logging.MasterLogger, conf appcommon.ProcConfig, disc appdisc.Updater, m ProcManager,
+func NewProc(mLog *logging.MasterLogger, conf appcommon.ProcConfig, disc updatedisc.Updater, m ProcManager,
 	appName string) *Proc {
 	if mLog == nil {
 		mLog = logging.NewMasterLogger()
@@ -136,8 +136,8 @@ func (p *Proc) awaitConn() bool {
 	connDelta := p.rpcGW.cm.AddDeltaInformer()
 	go func() {
 		for n := range connDelta.Chan() {
-			if err := p.disc.ChangeValue(appdisc.ConnCountValue, []byte(strconv.Itoa(n))); err != nil {
-				p.log.WithError(err).WithField("value", appdisc.ConnCountValue).
+			if err := p.disc.ChangeValue(updatedisc.ConnCountValue, []byte(strconv.Itoa(n))); err != nil {
+				p.log.WithError(err).WithField("value", updatedisc.ConnCountValue).
 					Error("Failed to change app discovery value.")
 			}
 		}
@@ -146,8 +146,8 @@ func (p *Proc) awaitConn() bool {
 	lisDelta := p.rpcGW.lm.AddDeltaInformer()
 	go func() {
 		for n := range lisDelta.Chan() {
-			if err := p.disc.ChangeValue(appdisc.ListenerCountValue, []byte(strconv.Itoa(n))); err != nil {
-				p.log.WithError(err).WithField("value", appdisc.ListenerCountValue).
+			if err := p.disc.ChangeValue(updatedisc.ListenerCountValue, []byte(strconv.Itoa(n))); err != nil {
+				p.log.WithError(err).WithField("value", updatedisc.ListenerCountValue).
 					Error("Failed to change app discovery value.")
 			}
 		}
