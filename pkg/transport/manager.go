5b08448ab446c83f4e8b4ca6cc07f72bfde1ac72
ersonp <ersonpereiracr7@gmail.com>
2021-09-21 19:01:54 +0530

    Fix test

--- pkg/transport/manager.go
+++ pkg/transport/manager.go
@@ -555,7 +555,10 @@ func (tm *Manager) Close() {
 			tm.Logger.WithError(err).Warnf("Failed to close %s client", client.Type())
 		}
 	}
-	tm.arClient.Close()
+	err := tm.arClient.Close()
+	if err != nil {
+		tm.Logger.WithError(err).Warnf("Failed to close arClient")
+	}
 	tm.wg.Wait()
 	close(tm.readCh)
 }
