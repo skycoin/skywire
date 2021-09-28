e7d259701728f03f29c5988321a5446340bc3304
Alexander Adhyatma <alexadhyatma@mailbox.org>
2021-09-28 22:55:56 +0700

    changes default directory of default apps
    Former-commit-id: 50dc52e327fa6f67f1a30e2990cbb662efba2f50

--- pkg/skyenv/values_darwin.go
+++ pkg/skyenv/values_darwin.go
@@ -10,9 +10,15 @@ import (
 
 const (
 	packageSkywirePath = "/Library/Application Support/Skywire"
+	skywireApplicationPath = "/Applications/Skywire.app"
 )
 
 // PackageSkywirePath gets Skywire installation folder.
 func PackageSkywirePath() string {
 	return filepath.Join(os.Getenv("HOME"), packageSkywirePath)
+}
+
+// PackageAppBinPath gets the Skywire application directory folder.
+func appBinPath() string {
+	return filepath.Join(skywireApplicationPath, "Contents", "MacOS")
 }
