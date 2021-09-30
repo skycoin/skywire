b192e36fa671b474386a5bc7fc625ac1b22ef317
ersonp <ersonpereiracr7@gmail.com>
2021-09-30 16:45:42 +0530

    Add whitelisted errors
    We whitelist errors sent from server hello as it's pointless to retry.

--- internal/vpn/errors.go
+++ internal/vpn/errors.go
@@ -4,4 +4,8 @@ import "errors"
 
 var (
 	errCouldFindDefaultNetworkGateway = errors.New("could not find default network gateway")
+	errHandshakeStatusForbidden       = errors.New("password didn't match")
+	errHandshakeStatusInternalError   = errors.New("Internal server error")
+	errHandshakeNoFreeIPs             = errors.New("No free IPs left to serve")
+	errHandshakeStatusBadRequest      = errors.New("Request was malformed")
 )
