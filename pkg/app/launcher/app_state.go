80e6242f7b6070f4cd4f63eceb61462f62d8c79e
Alexander Adhyatma <alexadhyatma@mailbox.org>
2021-10-08 02:10:21 +0700

    formatting
    Former-commit-id: 5a3b1b70f033efcc06bbc5d8937f1e4130982041

--- pkg/app/launcher/app_state.go
+++ pkg/app/launcher/app_state.go
@@ -17,6 +17,6 @@ const (
 // AppState defines state parameters for a registered App.
 type AppState struct {
 	AppConfig
-	Status             AppStatus `json:"status"`
-	DetailedStatus     string    `json:"detailed_status"`
+	Status         AppStatus `json:"status"`
+	DetailedStatus string    `json:"detailed_status"`
 }
