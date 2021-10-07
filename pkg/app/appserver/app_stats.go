5a3b1b70f033efcc06bbc5d8937f1e4130982041
Alexander Adhyatma <alexadhyatma@mailbox.org>
2021-10-08 02:10:21 +0700

    formatting

--- pkg/app/appserver/app_stats.go
+++ pkg/app/appserver/app_stats.go
@@ -6,6 +6,6 @@ import (
 
 // AppStats contains app runtime statistics.
 type AppStats struct {
-	Connections        []ConnectionSummary `json:"connections"`
-	StartTime          *time.Time          `json:"start_time,omitempty"`
+	Connections []ConnectionSummary `json:"connections"`
+	StartTime   *time.Time          `json:"start_time,omitempty"`
 }
