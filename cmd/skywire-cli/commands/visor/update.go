f1ddc6e72a1d26c4a700ad9e7916dac82f6d9d9a
MohammadReza Palide <mrpalide@gmail.com>
2021-10-28 18:48:17 +0330

    fix a condition on check update | add BUILDTAG to skywire-cli on goreleaser
    Former-commit-id: e9a13f509bba87dbf344ee245d40340820f5698d

--- cmd/skywire-cli/commands/visor/update.go
+++ cmd/skywire-cli/commands/visor/update.go
@@ -28,39 +28,37 @@ var updateCmd = &cobra.Command{
 
 		// Get the latest version of skywire on for update
 		latestVersion, err := rpcClient().UpdateAvailable(channel)
-		if err != nil || latestVersion == nil {
+		if err != nil && latestVersion == nil {
 			logger.WithError(err).Fatal("Failed to get latest version of Skywire from servers.")
-		}
-
-		// Get the version of current Skywire on visor
-		visor, err := rpcClient().Summary()
-		currentVersion := visor.Overview.BuildInfo.Version
-		if err != nil {
-			logger.WithError(err).Fatal("Failed to get current version of Skywire.")
+			return
+		} else if err == nil && latestVersion == nil {
+			logger.Info("Great! Your visor has the latest version of Skywire.")
+			return
 		}
 
 		// Checking visor build tag. skywire-cli update command just work on skybian images.
+		visor, err := rpcClient().Summary()
+		if err != nil {
+			logger.WithError(err).Fatal("Failed to get build tag of visor.")
+			return
+		}
+
 		if visor.BuildTag != "skybian" {
 			logger.Warn("Update from skywire-cli just available for Skybian images. Use source code or package managers to update your Skywire.")
 			return
 		}
 
-		// Comparing current version and latest version of Skywire.
-		if currentVersion < latestVersion.String() {
-			logger.Infof("New version of Skywire available. %s", latestVersion.String())
-			updaterConfig := updater.UpdateConfig{
-				Version: latestVersion.String(),
-			}
-			logger.Info("Updating...")
+		// Updating
+		logger.Infof("New version of Skywire available. %s", latestVersion.String())
+		updaterConfig := updater.UpdateConfig{
+			Version: latestVersion.String(),
+		}
+		logger.Info("Updating...")
 
-			if _, err := rpcClient().Update(updaterConfig); err != nil {
-				logger.WithError(err).Warn("Failed to update visor.")
-				return
-			}
-			logger.Info("Update completed!")
-		} else {
-			logger.Info("Great! Your visor has the latest version of Skywire.")
+		if _, err := rpcClient().Update(updaterConfig); err != nil {
+			logger.WithError(err).Warn("Failed to update visor.")
 			return
 		}
+		logger.Info("Update completed!")
 	},
 }
