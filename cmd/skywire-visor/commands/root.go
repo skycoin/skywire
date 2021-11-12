d0e56e3ec926ec6015847d7a5d8f3a00a7b3571a
specter25 <ujjwalcoding012@gmail.com>
2021-11-12 21:04:48 +0530

    fixed lint errors
    Former-commit-id: 5774fc6781836721950882f86170d6eeb0d02f7e

--- cmd/skywire-visor/commands/root.go
+++ cmd/skywire-visor/commands/root.go
@@ -63,7 +63,7 @@ var rootCmd = &cobra.Command{
 }
 
 func init() {
-	rootCmd.AddCommand(CompletionCmd)
+	rootCmd.AddCommand(completionCmd)
 	rootCmd.Flags().StringVar(&tag, "tag", "skywire", "logging tag")
 	rootCmd.Flags().StringVar(&syslogAddr, "syslog", "", "syslog server address. E.g. localhost:514")
 	rootCmd.Flags().StringVarP(&pprofMode, "pprofmode", "p", "", "pprof profiling mode. Valid values: cpu, mem, mutex, block, trace, http")
