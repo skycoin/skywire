e75c71a45f07bdf6d92e8cc77fd580d8c8e867ca
MohammadReza Palide <mrpalide@gmail.com>
2021-11-15 14:18:46 +0330

    set services dmsg:// address to skywire-config.json file

--- pkg/dmsgc/dmsgc.go
+++ pkg/dmsgc/dmsgc.go
@@ -13,8 +13,10 @@ import (
 
 // DmsgConfig defines config for Dmsg network.
 type DmsgConfig struct {
-	Discovery     string `json:"discovery"`
-	SessionsCount int    `json:"sessions_count"`
+	Discovery     string   `json:"discovery"`
+	SessionsCount int      `json:"sessions_count"`
+	IsLocal       bool     `json:"is_local"`
+	Servers       []string `json:"servers"`
 }
 
 // New makes new dmsg client from configuration
