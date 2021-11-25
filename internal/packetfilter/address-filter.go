8e8dfe33585a194775d9220eb63dc0a5fa653dbd
ersonp <ersonpereiracr7@gmail.com>
2021-11-25 15:56:40 +0530

    Add master logger to NewAddressFilter
    This commit adds a new parameter mLog to NewAddressFilter() and uses that to get a PackageLogger for address-filter.

--- internal/packetfilter/address-filter.go
+++ internal/packetfilter/address-filter.go
@@ -13,9 +13,9 @@ type AddressFilter struct {
 }
 
 // NewAddressFilter returns a new AddressFilter.
-func NewAddressFilter(addr net.Addr) *AddressFilter {
+func NewAddressFilter(addr net.Addr, mLog *logging.MasterLogger) *AddressFilter {
 	return &AddressFilter{
-		log:  logging.MustGetLogger("address-filter"),
+		log:  mLog.PackageLogger("address-filter"),
 		addr: addr,
 	}
 }
