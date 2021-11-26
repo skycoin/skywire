e41603e030b8e4246ece5b2f0f1cb45c90ddd7aa
ersonp <ersonpereiracr7@gmail.com>
2021-11-26 17:28:12 +0530

    Minor fixes
    Former-commit-id: a45d73e4c279567bb25b7b60bf6738e9170ee7a0

--- cmd/skywire-cli/commands/mdisc/root.go
+++ cmd/skywire-cli/commands/mdisc/root.go
@@ -42,7 +42,7 @@ var entryCmd = &cobra.Command{
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 		defer cancel()
 		pk := internal.ParsePK("visor-public-key", args[0])
-		entry, err := disc.NewHTTP(mdAddr, http.Client{}, nil).Entry(ctx, pk)
+		entry, err := disc.NewHTTP(mdAddr, &http.Client{}, nil).Entry(ctx, pk)
 		internal.Catch(err)
 		fmt.Println(entry)
 	},
@@ -54,7 +54,7 @@ var availableServersCmd = &cobra.Command{
 	Run: func(_ *cobra.Command, _ []string) {
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 		defer cancel()
-		entries, err := disc.NewHTTP(mdAddr, http.Client{}, nil).AvailableServers(ctx)
+		entries, err := disc.NewHTTP(mdAddr, &http.Client{}, nil).AvailableServers(ctx)
 		internal.Catch(err)
 		printAvailableServers(entries)
 	},
