39f22b88d75ce2c65529ac475ae7afb820f52aaf
MohammadReza Palide <mrpalide@gmail.com>
2022-01-20 08:12:37 +0330

    remove host-keeper from config

--- pkg/visor/visorconfig/parse.go
+++ pkg/visor/visorconfig/parse.go
@@ -186,6 +186,5 @@ func updateUrls(conf *V1) *V1 {
 	if conf.Launcher.ServiceDisc == skyenv.OldDefaultServiceDiscAddr {
 		conf.Launcher.ServiceDisc = skyenv.DefaultServiceDiscAddr
 	}
-	conf.HostKeeper = skyenv.DefaultHostKeeperAddr
 	return conf
 }
