404c0ce6b026d300caed678ea631f391d29b7202
MohammadReza Palide <mrpalide@gmail.com>
2022-01-25 22:27:57 +0330

    remove useless function GetAvailPublicVPNServers
    Former-commit-id: 1c489d4614b3d7dc35568af8c5e94d44c331b13d

--- internal/gui/gui_test.go
+++ internal/gui/gui_test.go
@@ -12,20 +12,6 @@ import (
 	"github.com/skycoin/skywire/pkg/visor/visorconfig"
 )
 
-func TestGetAvailPublicVPNServers(t *testing.T) {
-	pk, sk := cipher.GenerateKeyPair()
-	common := &visorconfig.Common{
-		Version: "v1.1.0",
-		SK:      sk,
-		PK:      pk,
-	}
-	config := visorconfig.MakeBaseConfig(common)
-	servers := GetAvailPublicVPNServers(config)
-	require.NotEqual(t, nil, servers)
-	require.NotEqual(t, []string{}, servers)
-	t.Logf("Servers: %v", servers)
-}
-
 func TestReadEmbeddedIcon(t *testing.T) {
 	b, err := ReadSysTrayIcon()
 	require.NoError(t, err)
