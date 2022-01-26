b38158c35eb1978b44d34376779efcffe2c1b4a1
MohammadReza Palide <mrpalide@gmail.com>
2022-01-26 11:21:51 +0330

    make skywire.bat run as administrator
    Former-commit-id: 9b7957aa4c1c82c55dcd19f47807c5669a1087ed

--- scripts/win_installer/skywire.bat
+++ scripts/win_installer/skywire.bat
@@ -1,4 +1,6 @@
 @Echo Off
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+cd /d "%~dp0"
 start "" http://127.0.0.1:8000
 if exist "%HOMEPATH%\skywire-config.json" (
 skywire-visor.exe -c "%HOMEPATH%\skywire-config.json"
