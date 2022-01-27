027f93c7bba3b87c746f08eaa5b7d7e94aebdafa
MohammadReza Palide <mrpalide@gmail.com>
2022-01-27 17:51:15 +0330

    make skywire.bat clear
    Former-commit-id: 6e4534dcc72f7ced74e78226d45e556aa57989c1

--- scripts/win_installer/skywire.bat
+++ scripts/win_installer/skywire.bat
@@ -1,10 +1,12 @@
 @Echo Off
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 cd /d "%~dp0"
-start "" http://127.0.0.1:8000
-if exist "%HOMEPATH%\skywire-config.json" (
-skywire-visor.exe -c "%HOMEPATH%\skywire-config.json"
-) else (
-skywire-cli config gen -biro "%HOMEPATH%\skywire-config.json" --os windows --disable-apps skychat,skysocks,skysocks-client,vpn-servergi
-skywire-visor.exe -c "%HOMEPATH%\skywire-config.json"
+if not exist "apps\" (
+    mkdir apps
+    move vpn-client.exe apps
 )
+if not exist "%HOMEPATH%\skywire-config.json" (
+skywire-cli config gen -biro "%HOMEPATH%\skywire-config.json" --os windows --disable-apps skychat,skysocks,skysocks-client,vpn-server
+)
+start "" http://127.0.0.1:8000
+skywire-visor.exe -c "%HOMEPATH%\skywire-config.json"
