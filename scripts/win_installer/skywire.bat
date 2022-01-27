048320fdcb08fe2a767cf603d1ce0be18ae05ae2
MohammadReza Palide <mrpalide@gmail.com>
2022-01-27 15:34:48 +0330

    set -b flag
    Former-commit-id: 5649397803098ef38e80e75feb51f2a9e8745c91

--- scripts/win_installer/skywire.bat
+++ scripts/win_installer/skywire.bat
@@ -5,6 +5,6 @@ start "" http://127.0.0.1:8000
 if exist "%HOMEPATH%\skywire-config.json" (
 skywire-visor.exe -c "%HOMEPATH%\skywire-config.json"
 ) else (
-skywire-cli config gen -iro "%HOMEPATH%\skywire-config.json" --os windows --disable-apps skychat,skysocks,skysocks-client,vpn-servergi
+skywire-cli config gen -biro "%HOMEPATH%\skywire-config.json" --os windows --disable-apps skychat,skysocks,skysocks-client,vpn-servergi
 skywire-visor.exe -c "%HOMEPATH%\skywire-config.json"
 )
