210334bde5bf76d7460f7bf6257ede3bb1e32e33
MohammadReza Palide <mrpalide@gmail.com>
2022-02-07 07:36:18 +0330

    add wintun.dll
    Former-commit-id: 92ff1d468ec28f2077a1c5752fc331d1d7eaa7f8

--- scripts/win_installer/skywire.bat
+++ scripts/win_installer/skywire.bat
@@ -1,9 +1,14 @@
 @Echo Off
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 cd /d "%~dp0"
-if not exist "apps\" (
-    mkdir apps
-    move vpn-client.exe apps
+if exist vpn-client.exe (
+    if not exist "apps\" (
+        mkdir apps
+    )
+    move /Y vpn-client.exe apps
+)
+if exist "wintun.dll" (
+    move /Y wintun.dll "C:\Windows\System32"
 )
 if not exist "%HOMEPATH%\skywire-config.json" (
 skywire-cli config gen -biro "%HOMEPATH%\skywire-config.json" --os windows --disable-apps skychat,skysocks,skysocks-client,vpn-server
