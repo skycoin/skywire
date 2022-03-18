a8e30782e7bbe952fdd8cfd58ae2facf8dda01c7
Moses Narrow <moe_narrow@use.startmail.com>
2022-03-18 13:49:03 -0500

    eliminate writes when only reads are needed
    Former-commit-id: 671d283130a649219f335f9c4d61ddfa91b4a99b

--- cmd/skywire-cli/commands/visor/vvpn/vpn.go
+++ cmd/skywire-cli/commands/visor/vvpn/vpn.go
@@ -6,23 +6,45 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/toqueteos/webbrowser"
+
+	"github.com/skycoin/skywire/pkg/visor/visorconfig"
 )
+
+var path string
+var pkg bool
 
 func init() {
 	RootCmd.AddCommand(vpnUICmd)
+	vpnUICmd.Flags().StringVarP(&path, "input", "i", "", "read from specified config file")
+	vpnUICmd.Flags().BoolVarP(&pkg, "pkg", "p", false, "read from /opt/skywire/skywire.json")
+
 	RootCmd.AddCommand(vpnURLCmd)
+	vpnURLCmd.Flags().StringVarP(&path, "input", "i", "", "read from specified config file")
+	vpnURLCmd.Flags().BoolVarP(&pkg, "pkg", "p", false, "read from /opt/skywire/skywire.json")
 }
 
 var vpnUICmd = &cobra.Command{
 	Use:   "ui",
 	Short: "Open VPN UI in default browser",
 	Run: func(_ *cobra.Command, _ []string) {
+		var url string
+		if pkg == true {
+			path = visorconfig.Pkgpath
+		}
+		if path != "" {
+			conf, err := visorconfig.ReadConfig(path)
+			if err != nil {
+				log.Fatal("Failed:", err)
+			}
+			url = fmt.Sprintf("http://127.0.0.1:8000/#/vpn/%s/", conf.PK.Hex())
+		} else {
 		client := rpcClient()
 		overview, err := client.Overview()
 		if err != nil {
 			log.Fatal("Failed to connect:", err)
 		}
-		url := fmt.Sprintf("http://127.0.0.1:8000/#/vpn/%s/", overview.PubKey.Hex())
+		url = fmt.Sprintf("http://127.0.0.1:8000/#/vpn/%s/", overview.PubKey.Hex())
+	}
 		if err := webbrowser.Open(url); err != nil {
 			log.Fatal("Failed to open VPN UI in browser:", err)
 		}
@@ -33,12 +55,24 @@ var vpnURLCmd = &cobra.Command{
 	Use:   "url",
 	Short: "Show VPN UI URL",
 	Run: func(_ *cobra.Command, _ []string) {
+		var url string
+		if pkg == true {
+			path = visorconfig.Pkgpath
+		}
+		if path != "" {
+			conf, err := visorconfig.ReadConfig(path)
+			if err != nil {
+				log.Fatal("Failed:", err)
+			}
+			url = fmt.Sprintf("http://127.0.0.1:8000/#/vpn/%s/", conf.PK.Hex())
+		} else {
 		client := rpcClient()
 		overview, err := client.Overview()
 		if err != nil {
 			logger.Fatal("Failed to connect:", err)
 		}
-		url := fmt.Sprintf("http://127.0.0.1:8000/#/vpn/%s/", overview.PubKey.Hex())
+		url = fmt.Sprintf("http://127.0.0.1:8000/#/vpn/%s/", overview.PubKey.Hex())
+	}
 		fmt.Println(url)
 	},
 }
