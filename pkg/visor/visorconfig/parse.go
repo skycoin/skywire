c03c5fe690d11ffc0b11a2a947389772b7dec700
ersonp <ersonpereiracr7@gmail.com>
2022-04-07 12:01:03 +0530

    Fix Parse
    This commit fixes the Parse file by removing the funcs parseV1, ensureAppDisc and the strut ParseOptions because they are not used anymore and we do not write the config to file on startup.
    
    Parse_test.go is deleted because it is not needed since we do not write to the config with Parse.
    
    Parse now only reads from the io reader provided to ght the conf in V1 and compares the version of the config to the version of the visor and sends back the result in the new variable compat.

--- pkg/visor/visorconfig/parse.go
+++ pkg/visor/visorconfig/parse.go
@@ -1,14 +1,13 @@
 package visorconfig
 
 import (
-	"bytes"
-	"encoding/json"
 	"errors"
-	"fmt"
+	"io"
+	"regexp"
+	"strings"
 
 	"github.com/skycoin/skycoin/src/util/logging"
 
-	utilenv "github.com/skycoin/skywire-utilities/pkg/skyenv"
 	"github.com/skycoin/skywire/pkg/skyenv"
 )
 
@@ -18,46 +17,22 @@ var (
 )
 
 // Parse parses the visor config from a given reader.
-// If the config file is not the most recent version, it is upgraded and written back to 'path'.
-func Parse(log *logging.MasterLogger, raw []byte, options *ParseOptions) (*V1, error) {
+// The config version is checked against the visor's version and if not the same we send back the
+// error as well as compat(compatibility) as false.
+func Parse(log *logging.Logger, r io.Reader) (conf *V1, compat bool, err error) {
 
-	cc, err := NewCommon(log, nil)
+	conf, err = Reader(r)
 	if err != nil {
-		return nil, err
+		return nil, compat, err
 	}
+	log.WithField("config version: ", conf.Version).Info()
 
-	if err := json.Unmarshal(raw, cc); err != nil {
-		return nil, fmt.Errorf("failed to obtain config version: %w", err)
+	// we check if the version of the visor and config are the same
+	if (conf.Version != "unknown") && (skyenv.BuildInfo.Version != "unknown") {
+		compat, err = regexp.MatchString(strings.Split(skyenv.BuildInfo.Version, "-")[0], conf.Version)
+		if err != nil {
+			return nil, compat, err
+		}
 	}
-	return parseV1(cc, raw, options)
-}
-
-func parseV1(cc *Common, raw []byte, options *ParseOptions) (*V1, error) {
-	conf := MakeBaseConfig(cc, options.TestEnv, options.DmsgHTTP, options.Services)
-	dec := json.NewDecoder(bytes.NewReader(raw))
-	if err := dec.Decode(&conf); err != nil {
-		return nil, err
-	}
-
-	if err := conf.ensureKeys(); err != nil {
-		return nil, fmt.Errorf("%v: %w", ErrInvalidSK, err)
-	}
-	conf = ensureAppDisc(conf)
-	conf.Version = skyenv.Version()
-	return conf, conf.flush(conf, options.Path)
-}
-
-func ensureAppDisc(conf *V1) *V1 {
-	if conf.Launcher.ServiceDisc == "" {
-		conf.Launcher.ServiceDisc = utilenv.ServiceDiscAddr
-	}
-	return conf
-}
-
-// ParseOptions is passed to Parse
-type ParseOptions struct {
-	Path     string
-	TestEnv  bool
-	DmsgHTTP bool
-	Services *Services
+	return conf, compat, nil
 }
