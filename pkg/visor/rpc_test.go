e9bd4465dcb4d5f31cc52dd71edd79df4d13c52b
ersonp <ersonpereiracr7@gmail.com>
2022-04-07 22:35:02 +0530

    Fix visor config common
    This commit fixes the struct common in the package visorconfig.
    
    We reuse the field path in the common struct to flush the config instead of using it from the params of the method flush which was also removed.
    
    We are going back to the way it was done before as it is much more cleaner to do it that way and we avoid introducing any variables.
    
    
    Former-commit-id: ebbb74f2cfec80976cfeea4abccfd24f82b2eb62

--- pkg/visor/rpc_test.go
+++ pkg/visor/rpc_test.go
@@ -16,7 +16,7 @@ import (
 )
 
 func baseConfig(t *testing.T) *visorconfig.V1 {
-	cc, err := visorconfig.NewCommon(nil, nil)
+	cc, err := visorconfig.NewCommon(nil, visorconfig.StdinName, nil)
 	var services *visorconfig.Services
 	require.NoError(t, err)
 	return visorconfig.MakeBaseConfig(cc, false, true, services)
--- pkg/visor/visorconfig/parse.go
+++ pkg/visor/visorconfig/parse.go
@@ -18,9 +18,9 @@ var (
 // Parse parses the visor config from a given reader.
 // The config version is checked against the visor's version and if not the same we send back the
 // error as well as compat(compatibility) as false.
-func Parse(log *logging.Logger, r io.Reader) (conf *V1, compat bool, err error) {
+func Parse(log *logging.Logger, r io.Reader, confPath string) (conf *V1, compat bool, err error) {
 
-	conf, err = Reader(r)
+	conf, err = Reader(r, confPath)
 	if err != nil {
 		return nil, compat, err
 	}
