3caf053b0e125111c8ecfd2369d093df35249e14
ersonp <ersonpereiracr7@gmail.com>
2022-04-07 23:53:12 +0530

    Fix skywire-visor root command
    This commit uses local vars skywire, workDir and root instead of global vars skyenv.Skywire, skyenv.Wd, skyenv.Root since they we used within the same package. We also delete them from skyenv.
    
    We also use local var confPath instead of global var skyenv.ConfigName.

--- pkg/skyenv/values_linux.go
+++ pkg/skyenv/values_linux.go
@@ -3,23 +3,25 @@
 
 package skyenv
 
-//OS detection at runtime
-const OS = "linux"
+const (
+	//OS detection at runtime
+	OS = "linux"
 
-// SkywirePath is the path to the installation folder for the linux packages.
-var SkywirePath = "/opt/skywire"
+	// SkywirePath is the path to the installation folder for the linux packages.
+	SkywirePath = "/opt/skywire"
 
-// DmsghttpPath is the path to dmsghttp-config.json in the packages
-var DmsghttpPath = "/opt/skywire/dmsghttp-config.json"
+	// DmsghttpPath is the path to dmsghttp-config.json in the packages
+	DmsghttpPath = "/opt/skywire/dmsghttp-config.json"
 
-//The following files are created by the autoconfig script in the linux packages
-//also referenced in the skywire systemd service
+	//The following files are created by the autoconfig script in the linux packages
+	//also referenced in the skywire systemd service
 
-// Skywirejson is the Hypervisor config
-var Skywirejson = "skywire.json"
+	// Skywirejson is the Hypervisor config
+	Skywirejson = "skywire.json"
 
-// Skywirevisorjson is the visor config
-var Skywirevisorjson = "skywire-visor.json"
+	// Skywirevisorjson is the visor config
+	Skywirevisorjson = "skywire-visor.json"
+)
 
 // PackageConfig contains installation paths (for linux)
 func PackageConfig() PkgConfig {
