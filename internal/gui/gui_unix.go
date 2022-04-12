3cd623253b209acfc7360d18987e6b50d6fcfa98
Moses Narrow <moe_narrow@use.startmail.com>
2022-04-12 14:12:41 -0500

    disable browser-launching systray buttons as root ; disable apps that are not found at path specified in config during config gen with small exceptions

--- internal/gui/gui_unix.go
+++ internal/gui/gui_unix.go
@@ -6,19 +6,20 @@ package gui
 import (
 	"os/user"
 
-	 "github.com/skycoin/skywire/pkg/util/osutil"
+	"github.com/skycoin/skywire/pkg/util/osutil"
 )
+
 func platformExecUninstall() error {
 	return osutil.Run("/bin/bash", "-c", deinstallerPath)
 }
 
 func checkRoot() bool {
-thisUser, err := user.Current()
-if err != nil {
-	panic(err)
-}
-if thisUser.Username == "root" {
-	return true
-}
-return false
+	thisUser, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	if thisUser.Username == "root" {
+		return true
+	}
+	return false
 }
