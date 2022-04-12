0562dbd15e8db268532b194fcfed9013b0bb276d
Moses Narrow <moe_narrow@use.startmail.com>
2022-04-11 22:03:01 -0500

    require root user to run VPN client & server
    Former-commit-id: 29bf63902ef6677a515a03f5ba5eb6504d659463

--- cmd/apps/vpn-client/vpn-client.go
+++ cmd/apps/vpn-client/vpn-client.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"net"
 	"os"
 	"os/signal"
+	"os/user"
 	"runtime"
 	"syscall"
 
+	cc "github.com/ivanpirog/coloredcobra"
 	ipc "github.com/james-barrow/golang-ipc"
+	"github.com/spf13/cobra"
 
 	"github.com/skycoin/skywire-utilities/pkg/cipher"
 	"github.com/skycoin/skywire/internal/vpn"
@@ -19,142 +21,193 @@ import (
 )
 
 var (
-	serverPKStr = flag.String("srv", "", "PubKey of the server to connect to")
-	localPKStr  = flag.String("pk", "", "Local PubKey")
-	localSKStr  = flag.String("sk", "", "Local SecKey")
-	passcode    = flag.String("passcode", "", "Passcode to authenticate connection")
-	killswitch  = flag.Bool("killswitch", false, "If set, the Internet won't be restored during reconnection attempts")
+	serverPKStr string
+	localPKStr  string
+	localSKStr  string
+	passcode    string
+	killswitch  bool
 )
 
-func main() {
-	flag.Parse()
-
-	if *serverPKStr == "" {
-		// TODO(darkrengarius): fix args passage for Windows
-		//*serverPKStr = "03e9019b3caa021dbee1c23e6295c6034ab4623aec50802fcfdd19764568e2958d"
-		fmt.Println("VPN server pub key is missing")
-		os.Exit(1)
-	}
-
-	serverPK := cipher.PubKey{}
-	if err := serverPK.UnmarshalText([]byte(*serverPKStr)); err != nil {
-		fmt.Printf("Invalid VPN server pub key: %v\n", err)
-		os.Exit(1)
-	}
-
-	localPK := cipher.PubKey{}
-	if *localPKStr != "" {
-		if err := localPK.UnmarshalText([]byte(*localPKStr)); err != nil {
-			fmt.Printf("Invalid local PK: %v\n", err)
-			os.Exit(1)
-		}
-	}
-
-	localSK := cipher.SecKey{}
-	if *localSKStr != "" {
-		if err := localSK.UnmarshalText([]byte(*localSKStr)); err != nil {
-			fmt.Printf("Invalid local SK: %v\n", err)
-			os.Exit(1)
-		}
-	}
-
-	var directIPsCh, nonDirectIPsCh = make(chan net.IP, 100), make(chan net.IP, 100)
-	defer close(directIPsCh)
-	defer close(nonDirectIPsCh)
-
-	eventSub := appevent.NewSubscriber()
-
-	parseIP := func(addr string) net.IP {
-		ip, ok, err := vpn.ParseIP(addr)
-		if err != nil {
-			fmt.Printf("Failed to parse IP %s: %v\n", addr, err)
-			return nil
-		}
-		if !ok {
-			fmt.Printf("Failed to parse IP %s\n", addr)
-			return nil
-		}
-
-		return ip
-	}
-
-	eventSub.OnTCPDial(func(data appevent.TCPDialData) {
-		if ip := parseIP(data.RemoteAddr); ip != nil {
-			directIPsCh <- ip
-		}
-	})
-
-	eventSub.OnTCPClose(func(data appevent.TCPCloseData) {
-		if ip := parseIP(data.RemoteAddr); ip != nil {
-			nonDirectIPsCh <- ip
-		}
-	})
-
-	appClient := app.NewClient(eventSub)
-	defer appClient.Close()
-
-	fmt.Printf("Connecting to VPN server %s\n", serverPK.String())
-
-	vpnClientCfg := vpn.ClientConfig{
-		Passcode:   *passcode,
-		Killswitch: *killswitch,
-		ServerPK:   serverPK,
-	}
-
-	vpnClient, err := vpn.NewClient(vpnClientCfg, appClient)
+func init() {
+	thisUser, err := user.Current()
 	if err != nil {
-		fmt.Printf("Error creating VPN client: %v\n", err)
+		panic(err)
+	}
+	if (thisUser.Username != "root") && ((skyenv.OS == "linux") || (skyenv.OS == "mac")) {
+		fmt.Println("vpn client must be run as root")
+		os.Exit(1)
 	}
 
-	var directRoutesDone bool
-	for !directRoutesDone {
-		select {
-		case ip := <-directIPsCh:
-			if err := vpnClient.AddDirectRoute(ip); err != nil {
-				fmt.Printf("Failed to setup direct route to %s: %v\n", ip.String(), err)
-			}
-		default:
-			directRoutesDone = true
-		}
-	}
+	rootCmd.Flags().SortFlags = false
 
-	go func() {
-		for ip := range directIPsCh {
-			if err := vpnClient.AddDirectRoute(ip); err != nil {
-				fmt.Printf("Failed to setup direct route to %s: %v\n", ip.String(), err)
+	rootCmd.Flags().StringVarP(&serverPKStr, "srv", "q", "", "PubKey of the server to connect to")
+	rootCmd.Flags().StringVarP(&localPKStr, "pk", "p", "", "Local PubKey")
+	rootCmd.Flags().StringVarP(&localSKStr, "sk", "s", "", "Local SecKey")
+	rootCmd.Flags().StringVarP(&passcode, "passcode", "r", "", "Passcode to authenticate connection")
+	rootCmd.Flags().BoolVarP(&killswitch, "killswitch", "k", false, "If set, the Internet won't be restored during reconnection attempts")
+
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "vpn-client",
+	Short: "Skywire VPN Client",
+	Long: `
+	┬  ┬┌─┐┌┐┌   ┌─┐┬  ┬┌─┐┌┐┌┌┬┐
+	└┐┌┘├─┘│││───│  │  │├┤ │││ │
+	 └┘ ┴  ┘└┘   └─┘┴─┘┴└─┘┘└┘ ┴ `,
+	Run: func(_ *cobra.Command, _ []string) {
+		if serverPKStr == "" {
+			// TODO(darkrengarius): fix args passage for Windows
+			//*serverPKStr = "03e9019b3caa021dbee1c23e6295c6034ab4623aec50802fcfdd19764568e2958d"
+			fmt.Println("VPN server pub key is missing")
+			os.Exit(1)
+		}
+
+		serverPK := cipher.PubKey{}
+		if err := serverPK.UnmarshalText([]byte(serverPKStr)); err != nil {
+			fmt.Printf("Invalid VPN server pub key: %v\n", err)
+			os.Exit(1)
+		}
+
+		localPK := cipher.PubKey{}
+		if localPKStr != "" {
+			if err := localPK.UnmarshalText([]byte(localPKStr)); err != nil {
+				fmt.Printf("Invalid local PK: %v\n", err)
+				os.Exit(1)
 			}
 		}
-	}()
 
-	go func() {
-		for ip := range nonDirectIPsCh {
-			if err := vpnClient.RemoveDirectRoute(ip); err != nil {
-				fmt.Printf("Failed to remove direct route to %s: %v\n", ip.String(), err)
+		localSK := cipher.SecKey{}
+		if localSKStr != "" {
+			if err := localSK.UnmarshalText([]byte(localSKStr)); err != nil {
+				fmt.Printf("Invalid local SK: %v\n", err)
+				os.Exit(1)
 			}
 		}
-	}()
 
-	if runtime.GOOS != "windows" {
-		osSigs := make(chan os.Signal, 2)
-		sigs := []os.Signal{syscall.SIGTERM, syscall.SIGINT}
-		for _, sig := range sigs {
-			signal.Notify(osSigs, sig)
+		var directIPsCh, nonDirectIPsCh = make(chan net.IP, 100), make(chan net.IP, 100)
+		defer close(directIPsCh)
+		defer close(nonDirectIPsCh)
+
+		eventSub := appevent.NewSubscriber()
+
+		parseIP := func(addr string) net.IP {
+			ip, ok, err := vpn.ParseIP(addr)
+			if err != nil {
+				fmt.Printf("Failed to parse IP %s: %v\n", addr, err)
+				return nil
+			}
+			if !ok {
+				fmt.Printf("Failed to parse IP %s\n", addr)
+				return nil
+			}
+
+			return ip
+		}
+
+		eventSub.OnTCPDial(func(data appevent.TCPDialData) {
+			if ip := parseIP(data.RemoteAddr); ip != nil {
+				directIPsCh <- ip
+			}
+		})
+
+		eventSub.OnTCPClose(func(data appevent.TCPCloseData) {
+			if ip := parseIP(data.RemoteAddr); ip != nil {
+				nonDirectIPsCh <- ip
+			}
+		})
+
+		appClient := app.NewClient(eventSub)
+		defer appClient.Close()
+
+		fmt.Printf("Connecting to VPN server %s\n", serverPK.String())
+
+		vpnClientCfg := vpn.ClientConfig{
+			Passcode:   passcode,
+			Killswitch: killswitch,
+			ServerPK:   serverPK,
+		}
+
+		vpnClient, err := vpn.NewClient(vpnClientCfg, appClient)
+		if err != nil {
+			fmt.Printf("Error creating VPN client: %v\n", err)
+		}
+
+		var directRoutesDone bool
+		for !directRoutesDone {
+			select {
+			case ip := <-directIPsCh:
+				if err := vpnClient.AddDirectRoute(ip); err != nil {
+					fmt.Printf("Failed to setup direct route to %s: %v\n", ip.String(), err)
+				}
+			default:
+				directRoutesDone = true
+			}
 		}
 
 		go func() {
-			<-osSigs
-			vpnClient.Close()
+			for ip := range directIPsCh {
+				if err := vpnClient.AddDirectRoute(ip); err != nil {
+					fmt.Printf("Failed to setup direct route to %s: %v\n", ip.String(), err)
+				}
+			}
 		}()
-	} else {
-		ipcClient, err := ipc.StartClient(skyenv.VPNClientName, nil)
-		if err != nil {
-			fmt.Printf("Error creating ipc server for VPN client: %v\n", err)
-			os.Exit(1)
-		}
-		go vpnClient.ListenIPC(ipcClient)
-	}
 
-	if err := vpnClient.Serve(); err != nil {
-		fmt.Printf("Failed to serve VPN: %v\n", err)
+		go func() {
+			for ip := range nonDirectIPsCh {
+				if err := vpnClient.RemoveDirectRoute(ip); err != nil {
+					fmt.Printf("Failed to remove direct route to %s: %v\n", ip.String(), err)
+				}
+			}
+		}()
+
+		if runtime.GOOS != "windows" {
+			osSigs := make(chan os.Signal, 2)
+			sigs := []os.Signal{syscall.SIGTERM, syscall.SIGINT}
+			for _, sig := range sigs {
+				signal.Notify(osSigs, sig)
+			}
+
+			go func() {
+				<-osSigs
+				vpnClient.Close()
+			}()
+		} else {
+			ipcClient, err := ipc.StartClient(skyenv.VPNClientName, nil)
+			if err != nil {
+				fmt.Printf("Error creating ipc server for VPN client: %v\n", err)
+				os.Exit(1)
+			}
+			go vpnClient.ListenIPC(ipcClient)
+		}
+
+		if err := vpnClient.Serve(); err != nil {
+			fmt.Printf("Failed to serve VPN: %v\n", err)
+		}
+
+	},
+}
+
+// Execute executes root CLI command.
+func Execute() {
+	cc.Init(&cc.Config{
+		RootCmd:         rootCmd,
+		Headings:        cc.HiBlue + cc.Bold,
+		Commands:        cc.HiBlue + cc.Bold,
+		CmdShortDescr:   cc.HiBlue,
+		Example:         cc.HiBlue + cc.Italic,
+		ExecName:        cc.HiBlue + cc.Bold,
+		Flags:           cc.HiBlue + cc.Bold,
+		FlagsDescr:      cc.HiBlue,
+		NoExtraNewlines: true,
+		NoBottomNewline: true,
+	})
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
 	}
+}
+
+func main() {
+	Execute()
 }
