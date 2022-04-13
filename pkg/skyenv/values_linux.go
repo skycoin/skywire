6b5fab81b85b1af4356bf071c399a76cac62f47b
Moses Narrow <moe_narrow@use.startmail.com>
2022-04-13 13:20:35 -0500

    implement -u --user flag for skywire-cli config gen & skywire-visor

--- pkg/skyenv/values_linux.go
+++ pkg/skyenv/values_linux.go
@@ -6,7 +6,6 @@ package skyenv
 const (
 	//OS detection at runtime
 	OS = "linux"
-
 	// SkywirePath is the path to the installation folder for the linux packages.
 	SkywirePath = "/opt/skywire"
 )
@@ -16,7 +15,17 @@ func PackageConfig() PkgConfig {
 	var pkgconfig PkgConfig
 	pkgconfig.Launcher.BinPath = "/opt/skywire/apps"
 	pkgconfig.LocalPath = "/opt/skywire/local"
-	pkgconfig.Hypervisor.DbPath = "/opt/skywire/users.db" //permissions errors if the process is not run as root.
+	pkgconfig.Hypervisor.DbPath = "/opt/skywire/users.db"
 	pkgconfig.Hypervisor.EnableAuth = true
 	return pkgconfig
+}
+
+// UserConfig contains installation paths (for linux)
+func UserConfig() PkgConfig {
+	var usrconfig PkgConfig
+	usrconfig.Launcher.BinPath = "/opt/skywire/apps"
+	usrconfig.LocalPath = HomePath() + "/.skywire/local"
+	usrconfig.Hypervisor.DbPath = HomePath() + "/.skywire/users.db" //permissions errors if the process is not run as root.
+	usrconfig.Hypervisor.EnableAuth = true
+	return usrconfig
 }
