8c45e47312caeff8a9d901913c3659581eaf9069
MohammadReza Palide <mrpalide@gmail.com>
2022-04-21 06:10:40 +0430

    fix jdknives review

--- internal/vpn/server_config.go
+++ internal/vpn/server_config.go
@@ -2,7 +2,7 @@ package vpn
 
 // ServerConfig is a configuration for VPN server.
 type ServerConfig struct {
-	Passcode        string
-	Secure          bool
-	NetworkInteface string
+	Passcode         string
+	Secure           bool
+	NetworkInterface string
 }
