db6e20c0eb82276a03e357ef85ab4c3f376f7475
MohammadReza Palide <mrpalide@gmail.com>
2022-06-10 12:33:07 +0430

    create fantasy output

--- scripts/win_installer/script.ps1
+++ scripts/win_installer/script.ps1
@@ -1,14 +1,22 @@
-Write-Output ".:: Create MSI Installer Package ::.  "
 
-Write-Output "    1. Preparing Stage..."
-if (Test-Path ".\archive.zip") { Remove-Item ".\archive.zip" -Recurse -Force}
-if (Test-Path ".\archive") { Remove-Item ".\archive" -Recurse -Force}
-if (Test-Path ".\scripts\win_installer\build") { Remove-Item ".\scripts\win_installer\build" -Recurse -Force}
-if (Test-Path ".\scripts\win_installer\wintun.zip") { Remove-Item ".\scripts\win_installer\wintun.zip" -Recurse -Force}
-if (Test-Path ".\scripts\win_installer\wintun") { Remove-Item ".\scripts\win_installer\wintun" -Recurse -Force}
-if (Test-Path ".\scripts\win_installer\skywire.msi") { Remove-Item ".\scripts\win_installer\skywire.msi" -Recurse -Force}
+$ErrorActionPreference = "Stop"
+function CleanStage
+{
+    if (Test-Path ".\archive.zip") { Remove-Item ".\archive.zip" -Recurse -Force}
+    if (Test-Path ".\archive") { Remove-Item ".\archive" -Recurse -Force}
+    if (Test-Path ".\scripts\win_installer\build") { Remove-Item ".\scripts\win_installer\build" -Recurse -Force}
+    if (Test-Path ".\scripts\win_installer\wintun.zip") { Remove-Item ".\scripts\win_installer\wintun.zip" -Recurse -Force}
+    if (Test-Path ".\scripts\win_installer\wintun") { Remove-Item ".\scripts\win_installer\wintun" -Recurse -Force}
+    if (Test-Path ".\scripts\win_installer\skywire.msi") { Remove-Item ".\scripts\win_installer\skywire.msi" -Recurse -Force}
+}
+Write-Output "`n##########################################################"
+Write-Output "#                                                        #"
+Write-Output "#        .:::: Create MSI Installer Package ::::.        #"
+Write-Output "#                                                        #"
+Write-Output "#       1. Preparing Stage...                            #"
+CleanStage
 
-Write-Output "    2. Fetching Windows Archive from GitHub..."
+Write-Output "#       2. Fetching Archive from GitHub...               #"
 $version = $args[0]
 if ($version -eq "latest") {
     $url = 'https://github.com/skycoin/skywire/releases/latest'
@@ -18,17 +26,17 @@ if ($version -eq "latest") {
     $version = $realTagUrl.split('/')[-1].Trim('v')
     $fileName = "skywire-systray-v$version-windows-amd64"
     $downloadURL = "https://github.com/skycoin/skywire/releases/download/v$version/$filename.zip"
-    Invoke-WebRequest $downloadURL -o archive.zip
+    Invoke-WebRequest $downloadURL -o archive.zip -ErrorAction Stop
 } else {
     $fileName = "skywire-systray-$version-windows-amd64"
     $downloadURL = "https://github.com/skycoin/skywire/releases/download/$version/$filename.zip"
     Invoke-WebRequest $downloadURL -o archive.zip
 }
 
-Write-Output "    3. Extracing Downloaded Archive File..."
+Write-Output "#       3. Extracing Downloaded Archive File...          #"
 Expand-Archive -Path archive.zip
 
-Write-Output "    4. Preparing Environment for Go-MSI..."
+Write-Output "#       4. Preparing Environment for Go-MSI...           #"
 Set-Location .\scripts\win_installer
 mkdir -p ".\build\amd64\apps" > $null
 Move-Item ..\..\archive\skywire-visor.exe .\build\amd64\skywire-visor.exe
@@ -42,20 +50,17 @@ Invoke-WebRequest "https://www.wintun.net/builds/wintun-0.14.1.zip" -o wintun.zi
 Expand-Archive wintun.zip
 Copy-Item .\wintun\wintun\bin\amd64\wintun.dll .\build\amd64\wintun.dll
 
-Write-Output "    4. Building MSI Installer..."
+Write-Output "#       5. Building MSI Installer...                     #"
 go-msi make --msi skywire.msi --version 1.0.0 --arch amd64  > $null
 Move-Item skywire.msi ../../$fileName.msi -Force
 
-Write-Output "       =====> BINGO! Build Completed!"
+Write-Output "#          ==> BINGO! Installer Build Completed!         #"
 
 
-Write-Output "    5. Cleaning Stage..."
+Write-Output "#       6. Cleaning Stage...                             #"
 Set-Location ../../
-if (Test-Path ".\archive.zip") { Remove-Item ".\archive.zip" -Recurse -Force}
-if (Test-Path ".\archive") { Remove-Item ".\archive" -Recurse -Force}
-if (Test-Path ".\scripts\win_installer\build") { Remove-Item ".\scripts\win_installer\build" -Recurse -Force}
-if (Test-Path ".\scripts\win_installer\wintun.zip") { Remove-Item ".\scripts\win_installer\wintun.zip" -Recurse -Force}
-if (Test-Path ".\scripts\win_installer\wintun") { Remove-Item ".\scripts\win_installer\wintun" -Recurse -Force}
-if (Test-Path ".\scripts\win_installer\skywire.msi") { Remove-Item ".\scripts\win_installer\skywire.msi" -Recurse -Force}
+CleanStage
 
-Write-Output "`nYour Installer Ready! Have a Nice Day! Goodbye!"
+Write-Output "#       7. Done!                                         #"
+Write-Output "#                                                        #"
+Write-Output "##########################################################`n"
