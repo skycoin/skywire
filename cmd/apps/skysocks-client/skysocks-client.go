ec12f8a53be2ac3f13b70b3fee8e0fcfe9ce9263
ersonp <ersonpereiracr7@gmail.com>
2022-06-13 16:34:51 +0530

    Update skysocks-client logs

--- cmd/apps/skysocks-client/skysocks-client.go
+++ cmd/apps/skysocks-client/skysocks-client.go
@@ -6,12 +6,11 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"io"
 	"net"
 	"os"
 	"time"
-
-	"github.com/skycoin/skycoin/src/util/logging"
 
 	"github.com/skycoin/skywire-utilities/pkg/buildinfo"
 	"github.com/skycoin/skywire-utilities/pkg/cipher"
@@ -28,9 +27,7 @@ const (
 	socksPort = routing.Port(3)
 )
 
-var log = logging.MustGetLogger("skysocks-client")
-
-var r = netutil.NewRetrier(log, time.Second, netutil.DefaultMaxBackoff, 0, 1)
+var r = netutil.NewRetrier(nil, time.Second, netutil.DefaultMaxBackoff, 0, 1)
 
 func dialServer(ctx context.Context, appCl *app.Client, pk cipher.PubKey) (net.Conn, error) {
 	var conn net.Conn
@@ -54,13 +51,11 @@ func main() {
 	appCl := app.NewClient(nil)
 	defer appCl.Close()
 
-	skysocks.Log = log
-
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	if _, err := buildinfo.Get().WriteTo(os.Stdout); err != nil {
-		log.Printf("Failed to output build info: %v", err)
+		print(fmt.Sprintf("Failed to output build info: %v\n", err))
 	}
 
 	var addr = flag.String("addr", skyenv.SkysocksClientAddr, "Client address to listen on")
@@ -68,39 +63,42 @@ func main() {
 	flag.Parse()
 
 	if *serverPK == "" {
-		log.Warn("Empty server PubKey. Exiting")
+		print("Empty server PubKey. Exiting")
 		return
 	}
 
 	pk := cipher.PubKey{}
 	if err := pk.UnmarshalText([]byte(*serverPK)); err != nil {
-		log.Fatal("Invalid server PubKey: ", err)
+		print(fmt.Sprintf("Invalid server PubKey: %v\n", err))
+		os.Exit(1)
 	}
 
 	for {
 		conn, err := dialServer(ctx, appCl, pk)
 		if err != nil {
-			log.Fatalf("Failed to dial to a server: %v", err)
+			print(fmt.Sprintf("Failed to dial to a server: %v\n", err))
+			os.Exit(1)
 		}
 
-		log.Printf("Connected to %v\n", pk)
+		fmt.Printf("Connected to %v\n", pk)
 
 		client, err := skysocks.NewClient(conn, appCl)
 		if err != nil {
-			log.Fatal("Failed to create a new client: ", err)
+			print(fmt.Sprintf("Failed to create a new client: %v\n", err))
+			os.Exit(1)
 		}
 
-		log.Printf("Serving proxy client %v\n", *addr)
+		fmt.Printf("Serving proxy client %v\n", *addr)
 
 		if err := client.ListenAndServe(*addr); err != nil {
-			log.Errorf("Error serving proxy client: %v\n", err)
+			print(fmt.Sprintf("Error serving proxy client: %v\n", err))
 		}
 
 		// need to filter this out, cause usually client failure means app conn is already closed
 		if err := conn.Close(); err != nil && err != io.ErrClosedPipe {
-			log.Errorf("Error closing app conn: %v\n", err)
+			print(fmt.Sprintf("Error closing app conn: %v\n", err))
 		}
 
-		log.Println("Reconnecting to skysocks server")
+		fmt.Println("Reconnecting to skysocks server")
 	}
 }
