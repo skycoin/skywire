f74c248795d797c7e49fa0c34bf6dba8dbdb89af
ersonp <ersonpereiracr7@gmail.com>
2022-06-13 16:12:12 +0530

    Fix appCl.SetError
    Former-commit-id: 9430685529d68f32d295d71d0cd0b38129a796d8

--- cmd/apps/vpn-server/vpn-server.go
+++ cmd/apps/vpn-server/vpn-server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
@@ -35,11 +36,9 @@ func main() {
 	defer appCl.Close()
 
 	if runtime.GOOS != "linux" {
-		err := "OS is not supported\n"
+		err := errors.New("OS is not supported")
 		print(err)
-		if appErr := appCl.SetError(err); appErr != nil {
-			fmt.Printf("Failed to set error %v: %v\n", err, appErr)
-		}
+		setAppErr(appCl, err)
 		os.Exit(1)
 	}
 
@@ -49,9 +48,7 @@ func main() {
 	if *localPKStr != "" {
 		if err := localPK.UnmarshalText([]byte(*localPKStr)); err != nil {
 			print(fmt.Sprintf("Invalid local PK: %v", err))
-			if appErr := appCl.SetError(err.Error()); appErr != nil {
-				fmt.Printf("Failed to set error %v: %v\n", err, appErr)
-			}
+			setAppErr(appCl, err)
 			os.Exit(1)
 		}
 	}
@@ -60,9 +57,7 @@ func main() {
 	if *localSKStr != "" {
 		if err := localSK.UnmarshalText([]byte(*localSKStr)); err != nil {
 			print(fmt.Sprintf("Invalid local SK: %v", err))
-			if appErr := appCl.SetError(err.Error()); appErr != nil {
-				fmt.Printf("Failed to set error %v: %v\n", err, appErr)
-			}
+			setAppErr(appCl, err)
 			os.Exit(1)
 		}
 	}
@@ -77,9 +72,7 @@ func main() {
 	l, err := appCl.Listen(netType, vpnPort)
 	if err != nil {
 		print(fmt.Sprintf("Error listening network %v on port %d: %v", netType, vpnPort, err))
-		if appErr := appCl.SetError(err.Error()); appErr != nil {
-			fmt.Printf("Failed to set error %v: %v\n", err, appErr)
-		}
+		setAppErr(appCl, err)
 		os.Exit(1)
 	}
 
@@ -93,9 +86,7 @@ func main() {
 	srv, err := vpn.NewServer(srvCfg, appCl)
 	if err != nil {
 		print(fmt.Sprintf("Error creating VPN server: %v", err))
-		if appErr := appCl.SetError(err.Error()); appErr != nil {
-			fmt.Printf("Failed to set error %v: %v\n", err, appErr)
-		}
+		setAppErr(appCl, err)
 		os.Exit(1)
 	}
 	defer func() {
@@ -117,5 +108,11 @@ func main() {
 	case <-osSigs:
 	case err := <-errCh:
 		print(fmt.Sprintf("Error serving: %v", err))
+	}
+}
+
+func setAppErr(appCl *app.Client, err error) {
+	if appErr := appCl.SetError(err.Error()); appErr != nil {
+		fmt.Printf("Failed to set error %v: %v\n", err, appErr)
 	}
 }
