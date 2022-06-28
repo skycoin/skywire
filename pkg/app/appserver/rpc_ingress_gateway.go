74ba926d5ff32fbc9608bd3456ff90f8124b40ce
ersonp <ersonpereiracr7@gmail.com>
2022-06-28 15:21:56 +0530

    Fix comment

--- pkg/app/appserver/rpc_ingress_gateway.go
+++ pkg/app/appserver/rpc_ingress_gateway.go
@@ -276,7 +276,7 @@ func (r *RPCIngressGateway) Read(req *ReadReq, resp *ReadResp) error {
 		copy(resp.B, buf[:resp.N])
 	}
 	if err != nil {
-		// we don't print warning if the conn is ready closed
+		// we don't print warning if the conn is already closed
 		_, ok := r.cm.Get(req.ConnID)
 		if ok {
 			r.log.WithError(err).Warn("Received unexpected error when reading from server.")
