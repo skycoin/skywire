ab3cb54d20700188b64eaf4b949c9f132575e253
Mohammed <79150699+mrpalide@users.noreply.github.com>
2022-06-29 16:07:37 +0430

    fixing wix cleaner path

--- scripts/win_installer/script.ps1
+++ scripts/win_installer/script.ps1
@@ -8,8 +8,8 @@ function CleanStage
     if (Test-Path ".\scripts\win_installer\build") { Remove-Item ".\scripts\win_installer\build" -Recurse -Force}
     if (Test-Path ".\scripts\win_installer\wintun.zip") { Remove-Item ".\scripts\win_installer\wintun.zip" -Recurse -Force}
     if (Test-Path ".\scripts\win_installer\wintun") { Remove-Item ".\scripts\win_installer\wintun" -Recurse -Force}
-    if (Test-Path ".\scripts\win_installer\wix.zip") { Remove-Item ".\scripts\win_installet\wix.zip" -Recurse -Force}
-    if (Test-Path ".\scripts\win_installer\wix") { Remove-Item ".\scripts\win_installerwix" -Recurse -Force}
+    if (Test-Path ".\scripts\win_installer\wix.zip") { Remove-Item ".\scripts\win_installer\wix.zip" -Recurse -Force}
+    if (Test-Path ".\scripts\win_installer\wix") { Remove-Item ".\scripts\win_installer\wix" -Recurse -Force}
     if (Test-Path ".\scripts\win_installer\UI.wixobj") { Remove-Item ".\scripts\win_installer\UI.wixobj" -Recurse -Force}
     if (Test-Path ".\scripts\win_installer\Product.wixobj") { Remove-Item ".\scripts\win_installer\Product.wixobj" -Recurse -Force}
     if (Test-Path ".\scripts\win_installer\skywire.msi") { Remove-Item ".\scripts\win_installer\skywire.msi" -Recurse -Force}
@@ -39,7 +39,7 @@ function BuildInstaller($arch)
     Write-Output "#    => Create Installer for $arch_title                       #"
     Write-Output "#       0. Preparing Stage...                            #"
     CleanStage
-    Write-Output "#       0. Installing Wix...                             #"
+    Write-Output "#       1. Installing Wix...                             #"
     InstallWix
     Write-Output "#       2. Fetching Archive from GitHub...               #"
     if ($version -eq "latest") {
