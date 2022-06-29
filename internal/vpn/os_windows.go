cecddc78538166555a5dbaf36a117594c4478659
MohammadReza Palide <mrpalide@gmail.com>
2022-06-29 08:07:47 +0430

    fix windows problem
    Former-commit-id: 9662d0adf9e2ab5ae9d6e0fb23bb57062bcb5baf

--- internal/vpn/os_windows.go
+++ internal/vpn/os_windows.go
@@ -60,3 +60,23 @@ func modifyRoutingTable(action, ipCIDR, gateway string) error {
 	cmd := fmt.Sprintf(modifyRouteCMDFmt, action, ip, netmask, gateway)
 	return osutil.Run("cmd", "/C", cmd)
 }
+
+// SetupTUN sets the allocated TUN interface up, setting its IP, gateway, netmask and MTU.
+func (s *Server) SetupTUN(ifcName, ipCIDR, gateway string, mtu int) error {
+	ip, netmask, err := parseCIDR(ipCIDR)
+	if err != nil {
+		return fmt.Errorf("error parsing IP CIDR: %w", err)
+	}
+
+	setupCmd := fmt.Sprintf(tunSetupCMDFmt, ifcName, ip, netmask, gateway)
+	if err := osutil.Run("cmd", "/C", setupCmd); err != nil {
+		return fmt.Errorf("error running command %s: %w", setupCmd, err)
+	}
+
+	mtuSetupCmd := fmt.Sprintf(tunMTUSetupCMDFmt, ifcName, mtu)
+	if err := osutil.Run("cmd", "/C", mtuSetupCmd); err != nil {
+		return fmt.Errorf("error running command %s: %w", mtuSetupCmd, err)
+	}
+
+	return nil
+}
