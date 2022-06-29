835cca8a557f488ceaa0ed6d7fa52161ad8fc5a5
MohammadReza Palide <mrpalide@gmail.com>
2022-06-29 13:26:51 +0430

    fix darwin check

--- internal/vpn/os_darwin.go
+++ internal/vpn/os_darwin.go
@@ -18,19 +18,24 @@ func (c *Client) SetupTUN(ifcName, ipCIDR, gateway string, mtu int) error {
 	if err != nil {
 		return fmt.Errorf("error parsing IP CIDR: %w", err)
 	}
-
+	if err := c.setSysPrivileges(); err != nil {
+		print(fmt.Sprintf("Failed to setup system privileges for SetupTUN: %v\n", err))
+		return err
+	}
+	defer c.releaseSysPrivileges()
 	return osutil.Run("ifconfig", ifcName, ip, gateway, "mtu", strconv.Itoa(mtu), "netmask", netmask, "up")
+
 }
 
 // ChangeRoute changes current route to `ipCIDR` to go through the `gateway`
 // in the OS routing table.
 func (c *Client) ChangeRoute(ipCIDR, gateway string) error {
-	return modifyRoutingTable("change", ipCIDR, gateway)
+	return c.modifyRoutingTable("change", ipCIDR, gateway)
 }
 
 // AddRoute adds route to `ipCIDR` through the `gateway` to the OS routing table.
 func (c *Client) AddRoute(ipCIDR, gateway string) error {
-	if err := modifyRoutingTable("add", ipCIDR, gateway); err != nil {
+	if err := c.modifyRoutingTable("add", ipCIDR, gateway); err != nil {
 		var e *osutil.ErrorWithStderr
 		if errors.As(err, &e) {
 			if strings.Contains(string(e.Stderr), "File exists") {
@@ -46,18 +51,24 @@ func (c *Client) AddRoute(ipCIDR, gateway string) error {
 
 // DeleteRoute removes route to `ipCIDR` through the `gateway` from the OS routing table.
 func (c *Client) DeleteRoute(ipCIDR, gateway string) error {
-	return modifyRoutingTable("delete", ipCIDR, gateway)
+	return c.modifyRoutingTable("delete", ipCIDR, gateway)
 }
 
-func modifyRoutingTable(action, ipCIDR, gateway string) error {
+func (c *Client) modifyRoutingTable(action, ipCIDR, gateway string) error {
 	ip, netmask, err := parseCIDR(ipCIDR)
 	if err != nil {
 		return fmt.Errorf("error parsing IP CIDR: %w", err)
 	}
 
+	if err := c.setSysPrivileges(); err != nil {
+		print(fmt.Sprintf("Failed to setup system privileges for %s: %v\n", action, err))
+		return err
+	}
+	defer c.releaseSysPrivileges()
 	return osutil.Run("route", action, "-net", ip, gateway, netmask)
 }
 
+// SetupTUN sets the allocated TUN interface up, setting its IP, gateway, netmask and MTU.
 func (s *Server) SetupTUN(ifcName, ipCIDR, gateway string, mtu int) error {
 	ip, netmask, err := parseCIDR(ipCIDR)
 	if err != nil {
