c4918f8b9d87a80bb5798b3dcfa9f09fc667792f
Moses Narrow <moe_narrow@use.startmail.com>
2022-07-25 09:40:05 -0500

    remove use of bitfield/script

---
+++ pkg/visor/remote_hypervisor.go
@@ -0,0 +1,23 @@
+package visor
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+// FetchHvPk fetches the hypervisor public key from the ip:port passed to it.
+func FetchHvPk(ipPort string) (string, error) {
+	req, err := http.NewRequest(http.MethodGet, "http://"+ipPort, nil)
+	if err != nil {
+		return "", err
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(resBody), nil
+}
