bd73ee4bd836c1a4853c7fcbb81580e90935725b
Moses Narrow <36607567+0pcom@users.noreply.github.com>
2022-08-14 11:08:35 -0500

    `skywire-cli vpn` subcommands + separate-systray (#1317)
    * add flags for skywire-cli vpn list
    
    * remove accidentally included files
    
    * establish transport before starting VPN
    
    * update vpn list cli command logic
    
    * add skywire-cli vpn stop after successful tests with skywire-cli vpn start ; fix make check errors
    
    * improve detection of vpn-client app config in api with range loop instead of hardcoding index ; make format check
    
    * remove extra logging from cmd/skywire-visor/commands/root.go
    
    * make separate-systray work with vpn cli commands
    
    * update cmd/skywire-cli/commands/vpn/vvpn.go
    
    * add version flag for skywire-cli ; filter results of skywire-cli vpn list by buildinfo.Version() instead of hardcoded version by default
    
    * update cmd/skywire-systray/skywire-systray.go to match restructured skywire-cli subcommands
    
    * make format check
    
    * skywire-cli visor vpn list query filtering
    
    * comment out query filtering ; make format check

---
+++ cmd/skywire-cli/commands/vpn/root.go
@@ -0,0 +1,24 @@
+package clivpn
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/skycoin/skywire-utilities/pkg/logging"
+)
+
+var (
+	logger       = logging.MustGetLogger("skywire-cli")
+	path         string
+	isPkg        bool
+	isUnFiltered bool
+	ver          string
+	country      string
+	isSystray    bool
+	isStats      bool
+)
+
+// RootCmd contains commands that interact with the skywire-visor
+var RootCmd = &cobra.Command{
+	Use:   "vpn",
+	Short: "controls for VPN client",
+}
