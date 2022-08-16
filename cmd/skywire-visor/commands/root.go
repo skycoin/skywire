d5a07d3d2959637ae3b30d542d6b59130cbec599
Moses Narrow <moe_narrow@use.startmail.com>
2022-08-16 11:04:08 -0500

    optimize autopeering disable logic
    Former-commit-id: e364bc4a4a1b0914b92c27c49a3268f6796fad6c

--- cmd/skywire-visor/commands/root.go
+++ cmd/skywire-visor/commands/root.go
@@ -290,10 +290,7 @@ func runVisor(conf *visorconfig.V1) {
 		}
 	}
 	//autopeering should only happen when there is no local or remote hypervisor set in the config.
-	if conf.Hypervisor != nil {
-		isAutoPeer = false
-	}
-	if conf.Hypervisors != nil {
+	if conf.Hypervisor != nil || conf.Hypervisors != nil {
 		isAutoPeer = false
 	}
 	if isAutoPeer {
