7725de270c6693d5831640e78f45f9177473077a
MohammadReza Palide <mrpalide@gmail.com>
2022-10-06 02:42:24 +0330

    check dmsg-client ready or not for vpn button |  add retrying when vpn servers couldn't fetch each 10 seconds

--- pkg/visor/rpc_client.go
+++ pkg/visor/rpc_client.go
@@ -439,6 +439,13 @@ func (rc *rpcClient) RemoteVisors() ([]string, error) {
 	return output, nil
 }
 
+// IsDMSGClientReady return availability of dsmg client
+func (rc *rpcClient) IsDMSGClientReady() (bool, error) {
+	var out bool
+	err := rc.Call("IsDMSGClientReady", &struct{}{}, &out)
+	return out, err
+}
+
 // MockRPCClient mocks API.
 type mockRPCClient struct {
 	startedAt time.Time
@@ -1010,4 +1017,9 @@ func (mc *mockRPCClient) VPNServers(_, _ string) ([]servicedisc.Service, error) 
 // RemoteVisors implements API
 func (mc *mockRPCClient) RemoteVisors() ([]string, error) {
 	return []string{}, nil
+}
+
+// IsDMSGClientReady implements API.
+func (mc *mockRPCClient) IsDMSGClientReady() (bool, error) {
+	return false, nil
 }
