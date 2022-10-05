6755833c4a6068c80af18dc81f3a18ba88a80a0a
MohammadReza Palide <mrpalide@gmail.com>
2022-10-06 03:18:17 +0330

    go get dmsg@develop and skywire-utilities@develop
    Former-commit-id: dbf9e178d6926820339545888938b0ad469d49c7

--- vendor/github.com/skycoin/dmsg/pkg/dmsgpty/const_windows.go
+++ vendor/github.com/skycoin/dmsg/pkg/dmsgpty/const_windows.go
@@ -8,18 +8,6 @@ import (
 	"path/filepath"
 )
 
-const (
-	// DefaultCLINet for windows
-	DefaultCLINet = "unix"
-)
-
-// Constants related to dmsg.
-const (
-	DefaultPort     = uint16(22)
-	DefaultCmd      = `C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe`
-	DefaultFlagExec = "-Command"
-)
-
 // DefaultCLIAddr gets the default cli address
 func DefaultCLIAddr() string {
 	homedir, err := os.UserHomeDir()
