c37dbb6361eac9b5c5b2684b454790550a4cbb0b
MohammadReza Palide <mrpalide@gmail.com>
2022-10-10 14:56:24 +0330

    move VPNInfo from skywire-service to skywire
    Former-commit-id: 6878b27664a417c7d9900a9225316fbe4b48bccf

--- pkg/servicedisc/types.go
+++ pkg/servicedisc/types.go
@@ -124,7 +124,14 @@ type Service struct {
 	Geo       *geo.LocationData `json:"geo,omitempty" gorm:"embedded"`
 	Version   string            `json:"version,omitempty"`
 	LocalIPs  pq.StringArray    `json:"local_ips,omitempty" gorm:"type:text[]"`
-	Info      interface{}       `json:"info,omitempty" gorm:"-"`
+	Info      VPNInfo           `json:"info,omitempty" gorm:"-"`
+}
+
+// VPNInfo used for showing VPN metrics info, like latency, uptime and count of connections
+type VPNInfo struct {
+	Latency     float64
+	Uptime      float64
+	Connections int
 }
 
 // MarshalBinary implements encoding.BinaryMarshaller
