235fdd2432e07a2fa99ba75f678952c4ee83dbc0
ersonp <ersonpereiracr7@gmail.com>
2022-10-27 16:51:29 +0530

    comment unused server package

--- cmd/apps/skychat/internal/domain/server/server.go
+++ cmd/apps/skychat/internal/domain/server/server.go
@@ -1,22 +1,12 @@
 package server
 
-import (
-	"sync"
-
-	"github.com/skycoin/skywire-utilities/pkg/cipher"
-	"github.com/skycoin/skywire/cmd/apps/skychat/internal/domain/info"
-	"github.com/skycoin/skywire/cmd/apps/skychat/internal/domain/message"
-	"github.com/skycoin/skywire/cmd/apps/skychat/internal/domain/peer"
-	"github.com/skycoin/skywire/pkg/app"
-)
-
 // Server defies the chat server
-type Server struct {
-	appCl     *app.Client                 // Skywire app client //nolint
-	clientCh  chan string                 //nolint
-	conns     map[cipher.PubKey]peer.Peer // peer connections //nolint
-	info      info.Info                   // the public info of the server //nolint
-	msgs      []message.Message           // all messages send/received //nolint
-	blacklist []cipher.PubKey             // Blacklist to block inocming connections //nolint
-	connsMu   sync.Mutex                  //nolint
-}
+// type Server struct {
+// 	appCl     *app.Client                 // Skywire app client //nolint
+// 	clientCh  chan string                 //nolint
+// 	conns     map[cipher.PubKey]peer.Peer // peer connections //nolint
+// 	info      info.Info                   // the public info of the server //nolint
+// 	msgs      []message.Message           // all messages send/received //nolint
+// 	blacklist []cipher.PubKey             // Blacklist to block inocming connections //nolint
+// 	connsMu   sync.Mutex                  //nolint
+// }
