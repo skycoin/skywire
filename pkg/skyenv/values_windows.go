346483370313e635f6ad3ac41966c265bc3d6f86
MohammadReza Palide <mrpalide@gmail.com>
2022-11-01 20:34:01 +0330

    fix darwin sysinfo
    Former-commit-id: d155acf9ff53fc37e8e0cbf3108013e662eb0426

--- pkg/skyenv/values_windows.go
+++ pkg/skyenv/values_windows.go
@@ -46,3 +46,44 @@ func UserConfig() PkgConfig {
 func UpdateCommand() []string {
 	return []string{`echo "Update not implemented for windows. Download a new version from the release section here: https://github.com/skycoin/skywire/releases"`}
 }
+
+// Survey system hardware survey struct
+type Survey struct {
+	PubKey         cipher.PubKey    `json:"public_key,omitempty"`
+	SkycoinAddress string           `json:"skycoin_address,omitempty"`
+	GOOS           string           `json:"go_os,omitempty"`
+	GOARCH         string           `json:"go_arch,omitempty"`
+	IPInfo         *IPSkycoin       `json:"ip.skycoin.com,omitempty"`
+	IPAddr         *IPAddr          `json:"ip_addr,omitempty"`
+	Disks          *ghw.BlockInfo   `json:"ghw_blockinfo,omitempty"`
+	Product        *ghw.ProductInfo `json:"ghw_productinfo,omitempty"`
+	Memory         *ghw.MemoryInfo  `json:"ghw_memoryinfo,omitempty"`
+	UUID           uuid.UUID        `json:"uuid,omitempty"`
+}
+
+// SystemSurvey returns system survey
+func SystemSurvey() (Survey, error) {
+	disks, err := ghw.Block()
+	if err != nil {
+		return Survey{}, err
+	}
+	product, err := ghw.Product()
+	if err != nil {
+		return Survey{}, err
+	}
+	memory, err := ghw.Memory()
+	if err != nil {
+		return Survey{}, err
+	}
+	s := Survey{
+		IPInfo:  IPSkycoinFetch(),
+		IPAddr:  IPA(),
+		GOOS:    runtime.GOOS,
+		GOARCH:  runtime.GOARCH,
+		UUID:    uuid.New(),
+		Disks:   disks,
+		Product: product,
+		Memory:  memory,
+	}
+	return s, nil
+}
