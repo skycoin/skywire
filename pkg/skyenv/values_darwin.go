5ce42a5d63cca6fd031f54aaf56126e369588206
MohammadReza Palide <mrpalide@gmail.com>
2022-11-02 09:34:07 +0330

    remove Memory and Product info on darwin, not available

--- pkg/skyenv/values_darwin.go
+++ pkg/skyenv/values_darwin.go
@@ -58,30 +58,20 @@ func UpdateCommand() []string {
 
 // Survey system hardware survey struct
 type Survey struct {
-	PubKey         cipher.PubKey    `json:"public_key,omitempty"`
-	SkycoinAddress string           `json:"skycoin_address,omitempty"`
-	GOOS           string           `json:"go_os,omitempty"`
-	GOARCH         string           `json:"go_arch,omitempty"`
-	IPInfo         *IPSkycoin       `json:"ip.skycoin.com,omitempty"`
-	IPAddr         *IPAddr          `json:"ip_addr,omitempty"`
-	Disks          *ghw.BlockInfo   `json:"ghw_blockinfo,omitempty"`
-	Product        *ghw.ProductInfo `json:"ghw_productinfo,omitempty"`
-	Memory         *ghw.MemoryInfo  `json:"ghw_memoryinfo,omitempty"`
-	UUID           uuid.UUID        `json:"uuid,omitempty"`
-	SkywireVersion string           `json:"skywire_version,omitempty"`
+	PubKey         cipher.PubKey  `json:"public_key,omitempty"`
+	SkycoinAddress string         `json:"skycoin_address,omitempty"`
+	GOOS           string         `json:"go_os,omitempty"`
+	GOARCH         string         `json:"go_arch,omitempty"`
+	IPInfo         *IPSkycoin     `json:"ip.skycoin.com,omitempty"`
+	IPAddr         *IPAddr        `json:"ip_addr,omitempty"`
+	Disks          *ghw.BlockInfo `json:"ghw_blockinfo,omitempty"`
+	UUID           uuid.UUID      `json:"uuid,omitempty"`
+	SkywireVersion string         `json:"skywire_version,omitempty"`
 }
 
 // SystemSurvey returns system survey
 func SystemSurvey() (Survey, error) {
 	disks, err := ghw.Block()
-	if err != nil {
-		return Survey{}, err
-	}
-	product, err := ghw.Product()
-	if err != nil {
-		return Survey{}, err
-	}
-	memory, err := ghw.Memory()
 	if err != nil {
 		return Survey{}, err
 	}
@@ -92,8 +82,6 @@ func SystemSurvey() (Survey, error) {
 		GOARCH:         runtime.GOARCH,
 		UUID:           uuid.New(),
 		Disks:          disks,
-		Product:        product,
-		Memory:         memory,
 		SkywireVersion: Version(),
 	}
 	return s, nil
