13a32a7491395e813f2a75be1c828b42183aa9a2
ersonp <ersonpereiracr7@gmail.com>
2022-12-12 22:36:25 +0530

    Update ping subcommand

--- cmd/skywire-cli/commands/visor/ping.go
+++ cmd/skywire-cli/commands/visor/ping.go
@@ -10,23 +10,28 @@ import (
 	"github.com/skycoin/skywire/cmd/skywire-cli/internal"
 )
 
+var no int
+
 func init() {
-	RootCmd.AddCommand(testCmd)
+	RootCmd.AddCommand(pingCmd)
+	pingCmd.Flags().IntVarP(&no, "no", "n", 1, "Number of pings")
 }
 
-var testCmd = &cobra.Command{
+var pingCmd = &cobra.Command{
 	Use:   "ping <pk>",
-	Short: "Return routing rule by route ID key",
-	Long:  "\n	Return routing rule by route ID key",
+	Short: "Ping the visor with given pk",
+	Long:  "\n	Creates a route with the provided pk as a hop and returns latency on the conn",
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		pk := internal.ParsePK(cmd.Flags(), "pk", args[0])
 
 		err := clirpc.Client(cmd.Flags()).DialPing(pk)
 		internal.Catch(cmd.Flags(), err)
-		latency, err := clirpc.Client(cmd.Flags()).Ping(pk)
-		internal.Catch(cmd.Flags(), err)
-		internal.PrintOutput(cmd.Flags(), latency, fmt.Sprintf(latency+"\n"))
+		for i := 1; i <= no; i++ {
+			latency, err := clirpc.Client(cmd.Flags()).Ping(pk)
+			internal.Catch(cmd.Flags(), err)
+			internal.PrintOutput(cmd.Flags(), latency, fmt.Sprintf(latency+"\n"))
+		}
 		err = clirpc.Client(cmd.Flags()).StopPing(pk)
 		internal.Catch(cmd.Flags(), err)
 
