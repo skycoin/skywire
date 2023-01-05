880bb5ffa0e88d693c72e58c57ba0b879fd18412
ersonp <ersonpereiracr7@gmail.com>
2023-01-05 12:37:12 +0530

    Add api to http example

--- example/http-server/server.go
+++ example/http-server/server.go
@@ -3,10 +3,16 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
+	"github.com/skycoin/skywire-utilities/pkg/logging"
 	"github.com/skycoin/skywire/example/http-server/html"
+	"github.com/skycoin/skywire/pkg/visor"
 )
 
 func homepage(w http.ResponseWriter, r *http.Request) {
@@ -20,19 +26,62 @@ func homepage(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+var port int
+
 func main() {
+	port = 9080
+
+	log := logging.MustGetLogger("http-example")
+	osSigs := make(chan os.Signal, 2)
+	sigs := []os.Signal{syscall.SIGTERM, syscall.SIGINT}
+	for _, sig := range sigs {
+		signal.Notify(osSigs, sig)
+	}
 
 	http.HandleFunc("/", homepage)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(html.FS()))))
 	srv := &http.Server{ //nolint gosec
-		Addr:         ":8080",
+		Addr:         fmt.Sprintf(":%v", port),
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
-	fmt.Println("serving on http://localhost:8080")
-	err := srv.ListenAndServe()
+
+	log.Infof("serving on http://localhost:%v", port)
+
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil {
+			fmt.Printf("error serving: %v\n", err)
+		}
+	}()
+
+	rpcClient, err := client()
 	if err != nil {
 		fmt.Printf("error serving: %v\n", err)
 	}
 
+	err = rpcClient.RegisterHTTPPort(port)
+	if err != nil {
+		log.Errorf("error closing server: %v", err)
+	}
+
+	<-osSigs
+	err = srv.Close()
+	if err != nil {
+		log.Errorf("error closing server: %v", err)
+	}
+	err = rpcClient.DeregisterHTTPPort(port)
+	if err != nil {
+		log.Errorf("error closing server: %v", err)
+	}
+}
+
+func client() (visor.API, error) {
+	const rpcDialTimeout = time.Second * 5
+	conn, err := net.DialTimeout("tcp", "localhost:3435", rpcDialTimeout)
+	if err != nil {
+		return nil, err
+	}
+	logger := logging.MustGetLogger("api")
+	return visor.NewRPCClient(logger, conn, visor.RPCPrefix, 0), nil
 }
