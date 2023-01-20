30b7e2633b5e36828a85d40eef4afbf4a37d5267
Moses Narrow <36607567+0pcom@users.noreply.github.com>
2023-01-20 09:59:04 -0600

    fix goimports errors in appveyor

--- cmd/skywire-cli/commands/config/root.go
+++ cmd/skywire-cli/commands/config/root.go
@@ -15,67 +15,67 @@ import (
 var logger = logging.MustGetLogger("skywire-cli")
 
 var (
-	sk                      cipher.SecKey
-	output                  string
-	confPath                string
-	configName              string //nolint Note: configName used, but golangci-lint marked it unused in wrong
-	isStdout                bool
-	isRegen                 bool
-	isRetainHypervisors     bool
-	isTestEnv               bool
-	pText                   string
-	isPkgEnv                bool
-	isUsrEnv                bool
-	isHypervisor            bool
-	hypervisorPKs           string
-	isDmsgHTTP              bool
-	isPublicRPC             bool
-	isVpnServerEnable       bool
-	isDisableAuth           bool
-	isEnableAuth            bool
-	selectedOS              string
-	disableApps             string
-	isBestProtocol          bool
-	serviceConfURL          string
-	services                *visorconfig.Services
-	isForce                 bool
-	isHide                  bool
-	isAll                   bool
-	isOutUnset              bool
-	ver                     string
-	isRoot                  = visorconfig.IsRoot()
-	svcConf                 = strings.ReplaceAll(utilenv.ServiceConfAddr, "http://", "")     //visorconfig.DefaultServiceConfAddr
-	testConf                = strings.ReplaceAll(utilenv.TestServiceConfAddr, "http://", "") //visorconfig.DefaultServiceConfAddr
-	gHiddenFlags            []string
-	uHiddenFlags            []string
-	binPath                 string
-	logLevel                string
-	isPkg                   bool
-	input                   string
-	isUpdateEndpoints       bool
-	addHypervisorPKs        string
-	isResetHypervisor       bool
-	setVPNClientKillswitch  string
-	addVPNClientSrv         string
-	addVPNClientPasscode    string
-	isResetVPNclient        bool
-	addVPNServerPasscode    string
-	setVPNServerSecure      string
-	setVPNServerAutostart   string
-	setVPNServerNetIfc      string
-	isResetVPNServer        bool
-	addSkysocksClientSrv    string
-	isResetSkysocksClient   bool
-	skysocksPasscode        string
-	isResetSkysocks         bool
-	setPublicAutoconnect    string
-	minHops                 int
-	conf                    *visorconfig.V1
-	isUsr                   bool
-	isPublic                bool
-	isDisablePublicAutoConn bool
-	isDisplayNodeIP         bool
-	addExampleApps          bool
+	sk                     cipher.SecKey
+	output                 string
+	confPath               string
+	configName             string //nolint Note: configName used, but golangci-lint marked it unused in wrong
+	isStdout               bool
+	isRegen                bool
+	isRetainHypervisors    bool
+	isTestEnv              bool
+	pText                  string
+	isPkgEnv               bool
+	isUsrEnv               bool
+	isHypervisor           bool
+	hypervisorPKs          string
+	isDmsgHTTP             bool
+	isPublicRPC            bool
+	isVpnServerEnable      bool
+	isDisableAuth          bool
+	isEnableAuth           bool
+	selectedOS             string
+	disableApps            string
+	isBestProtocol         bool
+	serviceConfURL         string
+	services               *visorconfig.Services
+	isForce                bool
+	isHide                 bool
+	isAll                  bool
+	isOutUnset             bool
+	ver                    string
+	isRoot                 = visorconfig.IsRoot()
+	svcConf                = strings.ReplaceAll(utilenv.ServiceConfAddr, "http://", "")     //visorconfig.DefaultServiceConfAddr
+	testConf               = strings.ReplaceAll(utilenv.TestServiceConfAddr, "http://", "") //visorconfig.DefaultServiceConfAddr
+	gHiddenFlags           []string
+	uHiddenFlags           []string
+	binPath                string
+	logLevel               string
+	isPkg                  bool
+	input                  string
+	isUpdateEndpoints      bool
+	addHypervisorPKs       string
+	isResetHypervisor      bool
+	setVPNClientKillswitch string
+	addVPNClientSrv        string
+	addVPNClientPasscode   string
+	isResetVPNclient       bool
+	addVPNServerPasscode   string
+	setVPNServerSecure     string
+	setVPNServerAutostart  string
+	setVPNServerNetIfc     string
+	isResetVPNServer       bool
+	addSkysocksClientSrv   string
+	isResetSkysocksClient  bool
+	skysocksPasscode       string
+	isResetSkysocks        bool
+	setPublicAutoconnect   string
+	minHops                int
+	conf                   *visorconfig.V1
+	isUsr                  bool
+	isPublic               bool
+	isPublicAutoConn       bool
+	isDisplayNodeIP        bool
+	addExampleApps         bool
 )
 
 // RootCmd contains commands that interact with the config of local skywire-visor
