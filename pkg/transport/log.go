45ddd066bd91ed9bdcdb6ac6c35ecaffe4e534e1
Moses Narrow <36607567+0pcom@users.noreply.github.com>
2023-01-23 08:20:32 -0600

    change transport_logs folder to 755 permissions

--- pkg/transport/log.go
+++ pkg/transport/log.go
@@ -180,7 +180,7 @@ type fileTransportLogStore struct {
 
 // FileTransportLogStore implements file TransportLogStore.
 func FileTransportLogStore(ctx context.Context, dir string, rInterval time.Duration, log *logging.Logger) (LogStore, error) {
-	if err := os.MkdirAll(dir, 0744); err != nil {
+	if err := os.MkdirAll(dir, 0755); err != nil {
 		return nil, err
 	}
 
