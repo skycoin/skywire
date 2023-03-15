5b1c560786da337c19f0921ef81fef91fca535ff
Moses Narrow <36607567+0pcom@users.noreply.github.com>
2023-03-15 12:53:36 -0500

    improve skywire-cli ut subcommand with flags

--- cmd/skywire-cli/commands/ut/root.go
+++ cmd/skywire-cli/commands/ut/root.go
@@ -10,23 +10,39 @@ import (
 	"net/http"
 	"strconv"
 	"time"
+	"github.com/skycoin/skywire-utilities/pkg/cipher"
+	"github.com/skycoin/skywire/cmd/skywire-cli/internal"
 
 	"github.com/spf13/cobra"
 )
 
+var pubkey cipher.PubKey
+var pk string
 var thisPk string
 
-const minUT float64 = 75.00
+var minUT int
+func init() {
+	RootCmd.Flags().StringVarP(&pk, "pk", "k", "", "check uptime for the specified key")
+	RootCmd.Flags().IntVarP(&minUT, "min", "n", 75, "list visors meeting minimum uptime")
+}
+
 
 // RootCmd contains commands that interact with the skywire-visor
 var RootCmd = &cobra.Command{
 	Use:   "ut",
 	Short: "query uptime tracker",
 	Run: func(cmd *cobra.Command, _ []string) {
-
-		now := time.Now()
 		url := "http://ut.skywire.skycoin.com/uptimes?v=v2"
 
+		now := time.Now()
+		if pk != "" {
+			err := pubkey.Set(pk)
+			if err != nil {
+					internal.PrintFatalError(cmd.Flags(), fmt.Errorf("Invalid or missing public key"))
+			} else {
+				url = "http://ut.skywire.skycoin.com/uptimes?v=v2&visors="+pubkey.String()
+		}
+	}
 		utClient := http.Client{
 			Timeout: time.Second * 15, // Timeout after 2 seconds
 		}
@@ -49,6 +65,7 @@ var RootCmd = &cobra.Command{
 		if readErr != nil {
 			log.Fatal(readErr)
 		}
+
 		// startDate := time.Date(now.Year(), now.Month()-1, 1, 0, 0, 0, 0, now.Location()).Format("2006-01-02")
 		// endDate := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, now.Location()).Add(-1 * time.Second).Format("2006-01-02")
 		startDate := time.Date(now.Year(), now.Month(), -1, 0, 0, 0, 0, now.Location()).Format("2006-01-02")
@@ -73,7 +90,7 @@ func selectedDaily(data map[string]string, startDate, endDate string) {
 			if err != nil {
 				log.Fatal(err)
 			}
-			if utfloat >= minUT {
+			if utfloat >= float64(minUT) {
 				//        if date == startDate {
 				fmt.Printf(thisPk)
 				fmt.Printf(" ")
