c340119106f01a1c35bac1aeb8a3a4c5ed7de822
ersonp <ersonpereiracr7@gmail.com>
2023-03-17 17:10:22 +0530

    Mgmt CLI

---
+++ cmd/skywire-cli/commands/mgmt/root.go
@@ -0,0 +1,20 @@
+// Package mgmt cmd/skywire-cli/commands/mgmt/root.go
+package mgmt
+
+import (
+	"github.com/spf13/cobra"
+
+	clirpc "github.com/skycoin/skywire/cmd/skywire-cli/commands/rpc"
+)
+
+// var logger = logging.MustGetLogger("skywire-cli")
+
+func init() {
+	RootCmd.PersistentFlags().StringVar(&clirpc.Addr, "rpc", "localhost:3435", "RPC server address")
+}
+
+// RootCmd contains commands that interact with the Manager
+var RootCmd = &cobra.Command{
+	Use:   "mgmt",
+	Short: "Skywire visor manager",
+}
