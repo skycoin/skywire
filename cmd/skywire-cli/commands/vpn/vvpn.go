5aaccffa81eceee1081cff0b355685c7f17faa4a
Moses Narrow <moe_narrow@use.startmail.com>
2023-05-09 16:03:51 -0500

    add direct query method to skywire-cli proxy list and skywire-cli vpn list ; standardize the implementation of these two subcommands

--- cmd/skywire-cli/commands/vpn/vvpn.go
+++ cmd/skywire-cli/commands/vpn/vvpn.go
@@ -3,16 +3,20 @@ package clivpn
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"math/rand"
+	"net/http"
 	"os"
 	"strings"
 	"text/tabwriter"
 	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 
 	"github.com/skycoin/skywire-utilities/pkg/buildinfo"
+	"github.com/skycoin/skywire-utilities/pkg/skyenv"
 	clirpc "github.com/skycoin/skywire/cmd/skywire-cli/commands/rpc"
 	"github.com/skycoin/skywire/cmd/skywire-cli/internal"
 	"github.com/skycoin/skywire/pkg/app/appserver"
@@ -21,89 +25,26 @@ import (
 )
 
 func init() {
+	RootCmd.PersistentFlags().StringVar(&clirpc.Addr, "rpc", "localhost:3435", "RPC server address")
 	RootCmd.AddCommand(
-		vpnStartCmd,
-		vpnStopCmd,
-		vpnStatusCmd,
-		vpnListCmd,
+		startCmd,
+		stopCmd,
+		statusCmd,
+		listCmd,
 	)
 	version := buildinfo.Version()
 	if version == "unknown" {
 		version = ""
 	}
-	vpnStartCmd.Flags().StringVarP(&pk, "pk", "k", "", "server public key")
-	vpnListCmd.Flags().StringVarP(&pk, "pk", "k", "", "check proxy service discovery for public key")
-	vpnListCmd.Flags().IntVarP(&count, "num", "n", 0, "number of results to return")
-	vpnListCmd.Flags().BoolVarP(&isUnFiltered, "unfilter", "u", false, "provide unfiltered results")
-	vpnListCmd.Flags().StringVarP(&ver, "ver", "v", version, "filter results by version")
-	vpnListCmd.Flags().StringVarP(&country, "country", "c", "", "filter results by country")
-	vpnListCmd.Flags().BoolVarP(&isStats, "stats", "s", false, "return only a count of the results")
-}
-
-var vpnListCmd = &cobra.Command{
-	Use:   "list",
-	Short: "List servers",
-	Long:  "List vpn servers from service discovery\n http://sd.skycoin.com/api/services?type=vpn\n http://sd.skycoin.com/api/services?type=vpn&country=US",
-	Run: func(cmd *cobra.Command, args []string) {
-		if pk != "" {
-			err := pubkey.Set(pk)
-			if err != nil {
-				internal.PrintFatalError(cmd.Flags(), fmt.Errorf("Invalid or missing public key"))
-			}
-		}
-		rpcClient, err := clirpc.Client(cmd.Flags())
-		if err != nil {
-			internal.PrintFatalRPCError(cmd.Flags(), err)
-		}
-		if isUnFiltered {
-			ver = ""
-			country = ""
-		}
-		servers, err := rpcClient.VPNServers(ver, country)
-		if err != nil {
-			internal.PrintFatalRPCError(cmd.Flags(), err)
-		}
-		if len(servers) == 0 {
-			internal.PrintFatalError(cmd.Flags(), fmt.Errorf("No Servers found"))
-		}
-		if isStats {
-			internal.PrintOutput(cmd.Flags(), fmt.Sprintf("%d Servers\n", len(servers)), fmt.Sprintf("%d Servers\n", len(servers)))
-		} else {
-			var msg string
-			var results []string
-			limit := len(servers)
-			if count > 0 && count < limit {
-				limit = count
-			}
-			if pk != "" {
-				for _, server := range servers {
-					if strings.Replace(server.Addr.String(), ":3", "", 1) == pk {
-						results = append(results, server.Addr.String())
-					}
-				}
-			} else {
-				for _, server := range servers {
-					results = append(results, server.Addr.String())
-				}
-			}
-			rand.Shuffle(len(results), func(i, j int) {
-				results[i], results[j] = results[j], results[i]
-			})
-			for i := 0; i < limit && i < len(results); i++ {
-				msg += strings.Replace(results[i], ":3", "", 1)
-				if server := findServerByPK(servers, results[i]); server != nil && server.Geo != nil {
-					if server.Geo.Country != "" {
-						msg += fmt.Sprintf(" | %s\n", server.Geo.Country)
-					} else {
-						msg += "\n"
-					}
-				} else {
-					msg += "\n"
-				}
-			}
-			internal.PrintOutput(cmd.Flags(), servers, msg)
-		}
-	},
+	startCmd.Flags().StringVarP(&pk, "pk", "k", "", "server public key")
+	listCmd.Flags().StringVarP(&sdURL, "url", "a", "", "service discovery url default:\n"+skyenv.ServiceDiscAddr)
+	listCmd.Flags().BoolVarP(&directQuery, "direct", "b", false, "query service discovery directly")
+	listCmd.Flags().StringVarP(&pk, "pk", "k", "", "check "+serviceType+" service discovery for public key")
+	listCmd.Flags().IntVarP(&count, "num", "n", 0, "number of results to return")
+	listCmd.Flags().BoolVarP(&isUnFiltered, "unfilter", "u", false, "provide unfiltered results")
+	listCmd.Flags().StringVarP(&ver, "ver", "v", version, "filter results by version")
+	listCmd.Flags().StringVarP(&country, "country", "c", "", "filter results by country")
+	listCmd.Flags().BoolVarP(&isStats, "stats", "s", false, "return only a count of the results")
 }
 
 func findServerByPK(servers []servicedisc.Service, addr string) *servicedisc.Service {
@@ -115,16 +56,16 @@ func findServerByPK(servers []servicedisc.Service, addr string) *servicedisc.Ser
 	return nil
 }
 
-var vpnStartCmd = &cobra.Command{
+var startCmd = &cobra.Command{
 	Use:   "start <public-key>",
-	Short: "start the vpn for <public-key>",
+	Short: "start the " + serviceType + " for <public-key>",
 	//	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		//check that a valid public key is provided
 		err := pubkey.Set(pk)
 		if err != nil {
 			if len(args) > 0 {
-				err := pubkey.Set(args[0])
+				err := pubkey.Set(args[1])
 				if err != nil {
 					internal.PrintFatalError(cmd.Flags(), err)
 				}
@@ -133,10 +74,9 @@ var vpnStartCmd = &cobra.Command{
 			}
 		}
 		//connect to RPC
-		internal.Catch(cmd.Flags(), pubkey.Set(args[0]))
 		rpcClient, err := clirpc.Client(cmd.Flags())
 		if err != nil {
-			os.Exit(1)
+			internal.PrintFatalError(cmd.Flags(), fmt.Errorf("unable to create RPC client: %w", err))
 		}
 		internal.Catch(cmd.Flags(), rpcClient.StartVPNClient(pubkey))
 		internal.PrintOutput(cmd.Flags(), nil, "Starting.")
@@ -153,7 +93,7 @@ var vpnStartCmd = &cobra.Command{
 			}
 
 			for _, state := range states {
-				if state.Name == "vpn-client" {
+				if state.Name == stateName {
 					if state.Status == appserver.AppStatusRunning {
 						startProcess = false
 						internal.PrintOutput(cmd.Flags(), nil, fmt.Sprintln("\nRunning!"))
@@ -178,22 +118,22 @@ var vpnStartCmd = &cobra.Command{
 	},
 }
 
-var vpnStopCmd = &cobra.Command{
+var stopCmd = &cobra.Command{
 	Use:   "stop",
-	Short: "stop the vpn",
+	Short: "stop the " + serviceType + "client",
 	Run: func(cmd *cobra.Command, _ []string) {
 		rpcClient, err := clirpc.Client(cmd.Flags())
 		if err != nil {
-			os.Exit(1)
+			internal.PrintFatalError(cmd.Flags(), fmt.Errorf("unable to create RPC client: %w", err))
 		}
-		internal.Catch(cmd.Flags(), rpcClient.StopVPNClient("vpn-client"))
+		internal.Catch(cmd.Flags(), rpcClient.StopVPNClient(stateName))
 		internal.PrintOutput(cmd.Flags(), "OK", fmt.Sprintln("OK"))
 	},
 }
 
-var vpnStatusCmd = &cobra.Command{
+var statusCmd = &cobra.Command{
 	Use:   "status",
-	Short: "vpn status",
+	Short: serviceType + " client status",
 	Run: func(cmd *cobra.Command, _ []string) {
 		rpcClient, err := clirpc.Client(cmd.Flags())
 		if err != nil {
@@ -210,7 +150,7 @@ var vpnStatusCmd = &cobra.Command{
 		}
 		var jsonAppStatus appState
 		for _, state := range states {
-			if state.Name == "vpn-client" {
+			if state.Name == stateName {
 
 				status := "stopped"
 				if state.Status == appserver.AppStatusRunning {
@@ -229,4 +169,110 @@ var vpnStatusCmd = &cobra.Command{
 		internal.Catch(cmd.Flags(), w.Flush())
 		internal.PrintOutput(cmd.Flags(), jsonAppStatus, b.String())
 	},
+}
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List " + serviceType + " servers",
+	Long:  "List " + serviceType + " servers from service discovery\n " + skyenv.ServiceDiscAddr + "/api/services?type=" + serviceType + "\n " + skyenv.ServiceDiscAddr + "/api/services?type=" + serviceType + "&country=US",
+	Run: func(cmd *cobra.Command, args []string) {
+		//validate any specified public key
+		if pk != "" {
+			err := pubkey.Set(pk)
+			if err != nil {
+				internal.PrintFatalError(cmd.Flags(), fmt.Errorf("Invalid or missing public key"))
+			}
+		}
+		if sdURL == "" {
+			sdURL = skyenv.ServiceDiscAddr
+		}
+		if isUnFiltered {
+			ver = ""
+			country = ""
+		}
+		if directQuery {
+			servers = directQuerySD(cmd.Flags())
+		} else {
+			rpcClient, err := clirpc.Client(cmd.Flags())
+			if err != nil {
+				internal.PrintError(cmd.Flags(), fmt.Errorf("unable to create RPC client: %w", err))
+				internal.PrintOutput(cmd.Flags(), fmt.Sprintf("directly querying service discovery\n%s/api/services?type=%s\n", sdURL, serviceType), fmt.Sprintf("directly querying service discovery\n%s/api/services?type=%s\n", sdURL, serviceType))
+				servers = directQuerySD(cmd.Flags())
+			} else {
+				servers, err = rpcClient.VPNServers(ver, country)
+				if err != nil {
+					internal.PrintError(cmd.Flags(), err)
+					internal.PrintOutput(cmd.Flags(), fmt.Sprintf("directly querying service discovery\n%s/api/services?type=%s\n", sdURL, serviceType), fmt.Sprintf("directly querying service discovery\n%s/api/services?type=%s\n", sdURL, serviceType))
+					servers = directQuerySD(cmd.Flags())
+				}
+			}
+		}
+		if len(servers) == 0 {
+			internal.PrintOutput(cmd.Flags(), "No Servers found", "No Servers found")
+			os.Exit(0)
+		}
+		if isStats {
+			internal.PrintOutput(cmd.Flags(), fmt.Sprintf("%d Servers\n", len(servers)), fmt.Sprintf("%d Servers\n", len(servers)))
+		} else {
+			var msg string
+			var results []string
+			limit := len(servers)
+			if count > 0 && count < limit {
+				limit = count
+			}
+			if pk != "" {
+				for _, server := range servers {
+					if strings.Replace(server.Addr.String(), servicePort, "", 1) == pk {
+						results = append(results, server.Addr.String())
+					}
+				}
+			} else {
+				for _, server := range servers {
+					results = append(results, server.Addr.String())
+				}
+			}
+
+			//randomize the order of the displayed results
+			rand.Shuffle(len(results), func(i, j int) {
+				results[i], results[j] = results[j], results[i]
+			})
+			for i := 0; i < limit && i < len(results); i++ {
+				msg += strings.Replace(results[i], servicePort, "", 1)
+				if server := findServerByPK(servers, results[i]); server != nil && server.Geo != nil {
+					if server.Geo.Country != "" {
+						msg += fmt.Sprintf(" | %s\n", server.Geo.Country)
+					} else {
+						msg += "\n"
+					}
+				} else {
+					msg += "\n"
+				}
+			}
+			internal.PrintOutput(cmd.Flags(), servers, msg)
+		}
+	},
+}
+
+func directQuerySD(cmdFlags *pflag.FlagSet) (s []servicedisc.Service) {
+	//url/uri format
+	//https://sd.skycoin.com/api/services?type=vpn&country=US&version=v1.3.7
+	sdURL += "/api/services?type=" + serviceType
+	if country != "" {
+		sdURL += "&country=" + country
+	}
+	if ver != "" {
+		sdURL += "&version=" + ver
+	}
+	//preform http get request for the service discovery URL
+	resp, err := (&http.Client{Timeout: time.Duration(30 * time.Second)}).Get(sdURL)
+	if err != nil {
+		internal.PrintFatalError(cmdFlags, fmt.Errorf("error fetching servers from service discovery: %w", err))
+	}
+	defer resp.Body.Close()
+	// Decode JSON response into struct
+	err = json.NewDecoder(resp.Body).Decode(&s)
+	if err != nil {
+		internal.PrintFatalError(cmdFlags, fmt.Errorf("error decoding json to struct: %w", err))
+	}
+	return s
 }
