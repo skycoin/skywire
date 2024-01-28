8438128ab3808227c2f5f3545651f4bb41713629
Moses Narrow <moe_narrow@use.startmail.com>
2024-01-27 21:59:18 -0600

    re-implement proxy list ; cache ut and sd files

--- cmd/skywire-cli/commands/proxy/root.go
+++ cmd/skywire-cli/commands/proxy/root.go
@@ -4,28 +4,33 @@ package skysocksc
 import (
 	"github.com/spf13/cobra"
 
+	"github.com/skycoin/skywire-utilities/pkg/buildinfo"
 	"github.com/skycoin/skywire-utilities/pkg/cipher"
 	"github.com/skycoin/skywire/pkg/servicedisc"
 )
 
 var (
-	binaryName   = "skysocks-client"
-	stateName    = "skysocks-client"
-	serviceType  = servicedisc.ServiceTypeProxy
-	servicePort  = ":44"
-	isUnFiltered bool
-	ver          string
-	country      string
-	isStats      bool
-	pubkey       cipher.PubKey
-	pk           string
-	count        int
-	sdURL        string
-	directQuery  bool
-	servers      []servicedisc.Service
-	allClients   bool
-	clientName   string
-	addr         string
+	version        = buildinfo.Version()
+	binaryName     = "skysocks-client"
+	stateName      = "skysocks-client"
+	serviceType    = servicedisc.ServiceTypeProxy
+	servicePort    = ":44"
+	isUnFiltered   bool
+	rawData        bool
+	utURL          string
+	sdURL          string
+	cacheFileSD    string
+	cacheFileUT    string
+	cacheFilesAge  int
+	ver            string
+	country        string
+	isStats        bool
+	pubkey         cipher.PubKey
+	pk             string
+	allClients     bool
+	noFilterOnline bool
+	clientName     string
+	addr           string
 )
 
 // RootCmd contains commands that interact with the skywire-visor
