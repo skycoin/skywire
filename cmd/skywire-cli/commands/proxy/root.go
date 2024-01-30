c2d1025f4041f23a00aa0d4676a6625dbdc3cfa8
Mohammed <79150699+mrpalide@users.noreply.github.com>
2024-01-30 19:58:22 +0330

    Improve VPN and Proxy cli command (#1722)
    * add --timeout (-t) flag to start proxy
    
    * add --timeout (-t) flag to start vpn

--- cmd/skywire-cli/commands/proxy/root.go
+++ cmd/skywire-cli/commands/proxy/root.go
@@ -9,23 +9,24 @@ import (
 )
 
 var (
-	binaryName   = "skysocks-client"
-	stateName    = "skysocks-client"
-	serviceType  = servicedisc.ServiceTypeProxy
-	servicePort  = ":44"
-	isUnFiltered bool
-	ver          string
-	country      string
-	isStats      bool
-	pubkey       cipher.PubKey
-	pk           string
-	count        int
-	sdURL        string
-	directQuery  bool
-	servers      []servicedisc.Service
-	allClients   bool
-	clientName   string
-	addr         string
+	binaryName      = "skysocks-client"
+	stateName       = "skysocks-client"
+	serviceType     = servicedisc.ServiceTypeProxy
+	servicePort     = ":44"
+	isUnFiltered    bool
+	ver             string
+	country         string
+	isStats         bool
+	pubkey          cipher.PubKey
+	pk              string
+	count           int
+	sdURL           string
+	directQuery     bool
+	servers         []servicedisc.Service
+	allClients      bool
+	clientName      string
+	addr            string
+	startingTimeout int
 )
 
 // RootCmd contains commands that interact with the skywire-visor
