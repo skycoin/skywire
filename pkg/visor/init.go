335900509a302fcfc52e31519d1b952132541bb8
Moses Narrow <36607567+0pcom@users.noreply.github.com>
2024-04-13 07:10:00 -0500

    Add module to ensure the visor is transportable (#1807)
    * add module to ensure the visor is transportable or exit if it is not transportable
    
    * vary tick duration on self-transport error
    
    Former-commit-id: 846eeaaf8de47629813c619e7533f49e4d2aa7ef

--- pkg/visor/init.go
+++ pkg/visor/init.go
@@ -93,6 +93,8 @@ var (
 	pty vinit.Module
 	// Dmsg module
 	dmsgC vinit.Module
+	// Transportability checker ensures the visor can accept transports by creating a self-transport or exiting after 3 failed attempts to create one
+	tc vinit.Module
 	// Transport manager
 	tr vinit.Module
 	// Transport setup
@@ -172,8 +174,9 @@ func registerModules(logger *logging.MasterLogger) {
 	pvs = maker("public_visor", initPublicVisor, &tr, &ar, &disc, &stcprC)
 	skyFwd = maker("sky_forward_conn", initSkywireForwardConn, &dmsgC, &dmsgCtrl, &tr, &launch)
 	pi = maker("ping", initPing, &dmsgC, &tm)
+	tc = maker("transportable", initEnsureVisorIsTransportable, &dmsgC, &tm)
 	vis = vinit.MakeModule("visor", vinit.DoNothing, logger, &ebc, &ar, &disc, &pty,
-		&tr, &rt, &launch, &cli, &hvs, &ut, &pv, &pvs, &trs, &stcpC, &stcprC, &skyFwd, &pi, &systemSurvey)
+		&tr, &rt, &launch, &cli, &hvs, &ut, &pv, &pvs, &trs, &stcpC, &stcprC, &skyFwd, &pi, &systemSurvey, &tc)
 
 	hv = maker("hypervisor", initHypervisor, &vis)
 }
@@ -1246,6 +1249,47 @@ func initUptimeTracker(ctx context.Context, v *Visor, log *logging.Logger) error
 	v.initLock.Lock()
 	v.uptimeTracker = ut
 	v.initLock.Unlock()
+
+	return nil
+}
+
+func initEnsureVisorIsTransportable(ctx context.Context, v *Visor, log *logging.Logger) error { //nolint:all
+	const tickDuration = 5 * time.Minute
+	ticker := time.NewTicker(tickDuration)
+	go func() {
+		time.Sleep(time.Minute)
+		tries := 0
+		for range ticker.C {
+			tpsummary, err := v.AddTransport(v.conf.PK, "dmsg", 0)
+			if err != nil {
+				tries++
+				v.isServicesHealthy.unset()
+				log.WithError(err).Warn(fmt.Sprintf("Visor is not transportable! Attempt %v of 3", tries))
+				//reduce tick duration on non nil error
+				ticker.Reset(time.Minute)
+			} else {
+				tries = 0
+				v.isServicesHealthy.set()
+				err = v.RemoveTransport(tpsummary.ID)
+				if err != nil {
+					log.WithError(err).Warn("Failed to remove self-transport")
+				}
+				ticker.Reset(tickDuration)
+			}
+			if tries == 3 {
+				log.WithError(err).Error("Visor is not transportable! 3 failed attempts ; exiting now")
+				err = v.Shutdown()
+				if err != nil {
+					log.WithError(err).Fatal("Failed to shut down gracefully")
+				}
+			}
+		}
+	}()
+
+	v.pushCloseStack("transportable", func() error {
+		ticker.Stop()
+		return nil
+	})
 
 	return nil
 }
