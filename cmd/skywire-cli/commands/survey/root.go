bf9703cbfa434ee583ef168aeb3dcd9a5df0d5ed
Moses Narrow <moe_narrow@use.startmail.com>
2024-06-23 12:30:10 -0500

    update system survey to include more of the deployment configuration

--- cmd/skywire-cli/commands/survey/root.go
+++ cmd/skywire-cli/commands/survey/root.go
@@ -2,26 +2,52 @@
 package clisurvey
 
 import (
-	"crypto/sha256"
 	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 
+	"github.com/skycoin/skywire-utilities/pkg/buildinfo"
+	"github.com/skycoin/skywire-utilities/pkg/logging"
 	"github.com/skycoin/skywire/cmd/skywire-cli/internal"
 	"github.com/skycoin/skywire/pkg/visor/visorconfig"
 )
 
 var (
-	//	pk       cipher.PubKey
-	//	pkString string
-	isCksum bool
+	logger   = logging.MustGetLogger("skywire-visor") //nolint:unused
+	isCksum  bool
+	mLog     = logging.NewMasterLogger()
+	log      = mLog.PackageLogger("survey")
+	confPath string
+	//	stdin                bool
+	//	confArg              string
+	visorBuildInfo   *buildinfo.Info
+	pkg              bool
+	usr              bool
+	pkgconfigexists  bool
+	userconfigexists bool
+	conf             *visorconfig.V1
+	useConf          bool
 )
 
 func init() {
 	surveyCmd.Flags().SortFlags = false
-	surveyCmd.Flags().BoolVarP(&isCksum, "sha", "s", false, "generate checksum of system survey")
+	surveyCmd.Flags().StringVarP(&confPath, "config", "c", "", "optionl config file to use (i.e.: "+visorconfig.ConfigName+")")
+	//	surveyCmd.Flags().StringVarP(&confArg, "confarg", "C", "", "supply config as argument")
+	//	surveyCmd.Flags().BoolVarP(&stdin, "stdin", "n", false, "read config from stdin")
+	if _, err := os.Stat(visorconfig.SkywirePath + "/" + visorconfig.ConfigJSON); err == nil {
+		pkgconfigexists = true
+	}
+	if _, err := os.Stat(visorconfig.HomePath() + "/" + visorconfig.ConfigName); err == nil {
+		userconfigexists = true
+	}
+	if pkgconfigexists {
+		surveyCmd.Flags().BoolVarP(&pkg, "pkg", "p", false, "use package config "+visorconfig.SkywirePath+"/"+visorconfig.ConfigJSON)
+	}
+	if userconfigexists {
+		surveyCmd.Flags().BoolVarP(&usr, "user", "u", false, "use config at: "+visorconfig.HomePath()+"/"+visorconfig.ConfigName)
+	}
 }
 
 // RootCmd is surveyCmd
@@ -33,36 +59,60 @@ var surveyCmd = &cobra.Command{
 	Short:                 "system survey",
 	Long:                  "print the system survey",
 	Run: func(cmd *cobra.Command, args []string) {
+		if pkg {
+			confPath = visorconfig.SkywirePath + "/" + visorconfig.ConfigJSON
+		}
+		if usr {
+			confPath = visorconfig.HomePath() + "/" + visorconfig.ConfigName
+		}
+		//		if confPath != "" && confArg != "" {
+		//			log.Fatal("cannot specify both --config, -c and --confarg, -C")
+		//		}
+		//		if confPath != "" && stdin {
+		//			log.Fatal("cannot specify both --config, -c and --stdin, -n")
+		//		}
+		//		if stdin && confArg != "" {
+		//			log.Fatal("cannot specify both --confarg, -C and --stdin, -n")
+		//		}
+		//		if stdin || confArg != "" || confPath != "" {
+
+		if confPath != "" {
+			//			conf = initConfig()
+			confJSON, err := os.ReadFile(confPath)
+			if err != nil {
+				log.WithError(err).Fatal("Failed to read config file: %v", err)
+			}
+			err = json.Unmarshal(confJSON, &conf)
+			if err != nil {
+				log.WithError(err).Fatal("Failed to unmarshal old config json")
+			}
+		}
 		survey, err := visorconfig.SystemSurvey()
 		if err != nil {
 			internal.Catch(cmd.Flags(), fmt.Errorf("Failed to generate system survey: %v", err))
 		}
-		//		//non-critical logic implemented with bitfield/script
-		//		pkString, err = script.Exec(`skywire-cli visor pk -p`).String()
-		//		//fail silently or proceed on nil error
-		//		if err != nil {
-		//			internal.Catch(cmd.Flags(), fmt.Errorf("failed to fetch visor public key: %v", err))
-		//		} else {
-		//			err = pk.Set(pkString)
-		//			if err != nil {
-		//			internal.Catch(cmd.Flags(), fmt.Errorf("failed to validate visor public key: %v", err))
-		//		} else {
-		//				survey.PubKey = pk
-		//			}
-		//		}
 		skyaddr, err := os.ReadFile(visorconfig.PackageConfig().LocalPath + "/" + visorconfig.RewardFile) //nolint
 		if err == nil {
 			survey.SkycoinAddress = string(skyaddr)
 		}
-
+		if conf != nil {
+			survey.PubKey = conf.PK
+			survey.ServicesURLs.DmsgDiscovery = conf.Dmsg.Discovery
+			survey.ServicesURLs.TransportDiscovery = conf.Transport.Discovery
+			survey.ServicesURLs.AddressResolver = conf.Transport.AddressResolver
+			survey.ServicesURLs.RouteFinder = conf.Routing.RouteFinder
+			survey.ServicesURLs.RouteSetupNodes = conf.Routing.RouteSetupNodes
+			survey.ServicesURLs.TransportSetupPKs = conf.Transport.TransportSetupPKs
+			survey.ServicesURLs.UptimeTracker = conf.UptimeTracker.Addr
+			survey.ServicesURLs.ServiceDiscovery = conf.Launcher.ServiceDisc
+			survey.ServicesURLs.SurveyWhitelist = conf.SurveyWhitelist
+			survey.ServicesURLs.StunServers = conf.StunServers
+			//survey.DmsgServers = v.dmsgC.ConnectedServersPK()
+		}
 		s, err := json.MarshalIndent(survey, "", "\t")
 		if err != nil {
 			internal.PrintFatalError(cmd.Flags(), fmt.Errorf("Could not marshal json: %v", err))
 		}
-		if isCksum {
-			fmt.Printf("%x/n", sha256.Sum256([]byte(s)))
-		} else {
-			fmt.Printf("%s", s)
-		}
+		fmt.Printf("%s", s)
 	},
 }
