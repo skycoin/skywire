19f31bfc4316ad857ee864617e32d8a37a19bf23
Moses Narrow <moe_narrow@use.startmail.com>
2024-09-22 11:08:20 -0500

    revise embedded configuration implementation

--- skywire.go
+++ skywire.go
@@ -4,6 +4,7 @@ package skywire
 import (
 	_ "embed"
 	"encoding/json"
+	"log"
 
 	"github.com/skycoin/skywire-utilities/pkg/cipher"
 )
@@ -14,13 +15,17 @@ Embedded Deployment Defaults
 Change the contents of services-config.json and / or dmsghttp-config.json to embed updated values
 */
 
+// ServicesJSON is the embedded services-config.json file
+//
 //go:embed services-config.json
 var ServicesJSON []byte
 
+// DmsghttpJSON is the embedded dmsghttp-config.json file
+//
 //go:embed dmsghttp-config.json
 var DmsghttpJSON []byte
 
-// Wrapper struct for the outer JSON
+// EnvServices is the wrapper struct for the outer JSON - i.e. 'prod' or 'test' deployment config
 type EnvServices struct {
 	Test json.RawMessage `json:"test"`
 	Prod json.RawMessage `json:"prod"`
@@ -43,41 +48,51 @@ type Services struct {
 
 // Conf is the configuration URL for the deployment which may be fetched on `skywire cli config gen`
 type Conf struct {
-	Conf      string          `json:"conf,omitempty"`
+	Conf string `json:"conf,omitempty"`
 }
 
-//Prod is the production deployment services
+// Prod is the production deployment services
 var Prod Services
 
-//ProdConf is the service configuration address / URL for the skywire production deployment
+// ProdConf is the service configuration address / URL for the skywire production deployment
 var ProdConf Conf
 
-//Test is the test deployment services
+// Test is the test deployment services
 var Test Services
 
-//TestConf is the service configuration address / URL for the skywire test deployment
+// TestConf is the service configuration address / URL for the skywire test deployment
 var TestConf Conf
 
+// initialize the embedded files into variables
 func init() {
-	var envServices EnvServices
-	err := json.Unmarshal(ServicesJSON, &envServices)
+	var js interface{}
+	err := json.Unmarshal([]byte(ServicesJSON), &js)
 	if err != nil {
-		panic(err)
+		log.Panic("services-config.json ", err)
+	}
+	err = json.Unmarshal([]byte(DmsghttpJSON), &js)
+	if err != nil {
+		log.Panic("dmsghttp-config.json ", err)
+	}
+	var envServices EnvServices
+	err = json.Unmarshal(ServicesJSON, &envServices)
+	if err != nil {
+		log.Panic(err)
 	}
 	err = json.Unmarshal(envServices.Prod, &Prod)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 	err = json.Unmarshal(envServices.Prod, &ProdConf)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 	err = json.Unmarshal(envServices.Test, &Test)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 	err = json.Unmarshal(envServices.Test, &TestConf)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 }
