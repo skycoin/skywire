75061321d9c5cef70051ee881d1240fd726b7add
Moses Narrow <36607567+0pcom@users.noreply.github.com>
2024-10-24 15:03:14 -0500

    display embedded mainnet_rules article on reward system UI front page (#1890)
    Former-commit-id: 67b6b1b54421af11f11c7976fc1ae4026235da5e

--- cmd/skywire-cli/commands/reward/rules.go
+++ cmd/skywire-cli/commands/reward/rules.go
@@ -2,18 +2,28 @@
 package clireward
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"regexp"
 
 	markdown "github.com/MichaelMure/go-term-markdown"
 	"github.com/spf13/cobra"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/renderer/html"
 	"golang.org/x/term"
 
 	"github.com/skycoin/skywire"
 )
 
+var asHTML bool
+var rawFile bool
+
 func init() {
 	rewardCmd.AddCommand(rulesCmd)
+	rulesCmd.Flags().BoolVarP(&asHTML, "html", "l", false, "render html from markdown")
+	rulesCmd.Flags().BoolVarP(&rawFile, "raw", "r", false, "print raw the embedded file")
 }
 
 var rulesCmd = &cobra.Command{
@@ -21,6 +31,26 @@ var rulesCmd = &cobra.Command{
 	Short: "display the mainnet rules",
 	Long:  "display the mainnet rules",
 	Run: func(_ *cobra.Command, _ []string) {
+		if rawFile {
+			fmt.Println(skywire.MainnetRules)
+			os.Exit(0)
+		}
+		if asHTML {
+			// Preprocess to replace ~text~ with ~~text~~ for strikethrough
+			re := regexp.MustCompile(`~(.*?)~`)
+			rules := re.ReplaceAllString(skywire.MainnetRules, "~~$1~~")
+			var buf bytes.Buffer
+			md := goldmark.New(
+				goldmark.WithExtensions(extension.Strikethrough),
+				goldmark.WithRendererOptions(html.WithXHTML()), // Optional: add XHTML compatibility
+			)
+			if err := md.Convert([]byte(rules), &buf); err != nil {
+				fmt.Println("Error rendering markdown as HTML:", err)
+				os.Exit(1)
+			}
+			fmt.Println(buf.String())
+			os.Exit(0)
+		}
 		terminalWidth, _, err := term.GetSize(int(os.Stdout.Fd()))
 		if err != nil {
 			terminalWidth = 80
