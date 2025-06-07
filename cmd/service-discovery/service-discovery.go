4aa10dafda74d85b3f9dfc448a92528f94f3f35f
Moses Narrow <36607567+0pcom@users.noreply.github.com>
2025-06-07 14:08:35 -0500

    deduplicate redundant code (#1999)
    * move redundant code to package
    
    * make format
    
    * fix help menu panic
    
    * adjust help menu formatting
    
    * make format
    
    Former-commit-id: 9d1633284e9dcbd14c0492884ffae25a091840a8

--- cmd/service-discovery/service-discovery.go
+++ cmd/service-discovery/service-discovery.go
@@ -1,43 +1,15 @@
-// Package commands cmd/service-discovery.go
+// Package commands cmd/service-discovery/service-discovery.go
 package main
 
 import (
-	cc "github.com/ivanpirog/coloredcobra"
-	"github.com/spf13/cobra"
-
 	"github.com/skycoin/skywire/cmd/service-discovery/commands"
+	"github.com/skycoin/skywire/pkg/skywire-utilities/pkg/flags"
 )
 
 func init() {
-	var helpflag bool
-	commands.RootCmd.SetUsageTemplate(help)
-	commands.RootCmd.PersistentFlags().BoolVarP(&helpflag, "help", "h", false, "help for service-discovery")
-	commands.RootCmd.SetHelpCommand(&cobra.Command{Hidden: true})
-	commands.RootCmd.PersistentFlags().MarkHidden("help") //nolint
+	flags.InitFlags(commands.RootCmd, true)
 }
 
 func main() {
-	cc.Init(&cc.Config{
-		RootCmd:         commands.RootCmd,
-		Headings:        cc.HiBlue + cc.Bold,
-		Commands:        cc.HiBlue + cc.Bold,
-		CmdShortDescr:   cc.HiBlue,
-		Example:         cc.HiBlue + cc.Italic,
-		ExecName:        cc.HiBlue + cc.Bold,
-		Flags:           cc.HiBlue + cc.Bold,
-		FlagsDescr:      cc.HiBlue,
-		NoExtraNewlines: true,
-		NoBottomNewline: true,
-	})
 	commands.Execute()
 }
-
-const help = "Usage:\r\n" +
-	"  {{.UseLine}}{{if .HasAvailableSubCommands}}{{end}} {{if gt (len .Aliases) 0}}\r\n\r\n" +
-	"{{.NameAndAliases}}{{end}}{{if .HasAvailableSubCommands}}\r\n\r\n" +
-	"Available Commands:{{range .Commands}}{{if (or .IsAvailableCommand)}}\r\n  " +
-	"{{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}\r\n\r\n" +
-	"Flags:\r\n" +
-	"{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}\r\n\r\n" +
-	"Global Flags:\r\n" +
-	"{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}\r\n\r\n"
