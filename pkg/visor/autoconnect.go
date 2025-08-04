7e1f2b8c904d3bb4bfa13c03bf9b387b9020786f
Moses Narrow <moe_narrow@use.startmail.com>
2025-08-03 21:57:26 -0500

    make format

--- pkg/visor/autoconnect.go
+++ pkg/visor/autoconnect.go
@@ -166,7 +166,6 @@ func (a *autoconnector) Run(ctx context.Context, v *Visor) (err error) {
 					}
 					v.isServicesHealthy.set()
 
-
 					var entryKeys []cipher.PubKey
 					seen := make(map[cipher.PubKey]struct{})
 					for _, entry := range entries {
