626623c0d5d1390214319e1bc843f46e1df5e85f
Moses Narrow <36607567+0pcom@users.noreply.github.com>
2025-08-10 10:54:28 -0500

    re-implement country and version filtering for proxy & vpn list (#2033)
    * re-implement country and version filtering
    
    * fix CI errors
    
    * add -m --more flag to 'skywire cli tp' to show version, country & services
    
    Former-commit-id: 30d83a379b396376f096a5e2db3113c252cf7586

--- cmd/skywire-cli/commands/tp/tp.go
+++ cmd/skywire-cli/commands/tp/tp.go
@@ -3,6 +3,7 @@ package clitp
 
 import (
 	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
@@ -23,6 +24,7 @@ import (
 	clirpc "github.com/skycoin/skywire/cmd/skywire-cli/commands/rpc"
 	"github.com/skycoin/skywire/cmd/skywire-cli/internal"
 	"github.com/skycoin/skywire/deployment"
+	"github.com/skycoin/skywire/pkg/servicedisc"
 	"github.com/skycoin/skywire/pkg/skywire-utilities/pkg/cipher"
 	"github.com/skycoin/skywire/pkg/skywire-utilities/pkg/logging"
 	"github.com/skycoin/skywire/pkg/transport"
@@ -34,10 +36,12 @@ var (
 	filterTypes   []string
 	filterPubKeys []string
 	showLogs      bool
+	showMore      bool
 	logger        = logging.MustGetLogger("skywire-cli")
 	removeAll     bool
 	tpTypes       bool
 	utURL         string
+	sdURL         string
 )
 
 func init() {
@@ -55,6 +59,12 @@ func init() {
 	tpCmd.Flags().StringSliceVarP(&filterTypes, "types", "t", filterTypes, "show transport(s) type(s) comma-separated")
 	tpCmd.Flags().StringSliceVarP(&filterPubKeys, "pks", "p", filterPubKeys, "show transport(s) for public key(s) comma-separated")
 	tpCmd.Flags().BoolVarP(&showLogs, "logs", "l", true, "show transport logs")
+	tpCmd.Flags().BoolVarP(&showMore, "more", "m", false, "show more info")
+	tpCmd.Flags().StringVar(&cacheFileUT, "cfu", os.TempDir()+"/ut.json", "UT cache file location.")
+	tpCmd.Flags().StringVar(&cacheFileSDProxy, "cfsp", os.TempDir()+"/proxysd.json", "SD cache file location")
+	tpCmd.Flags().StringVar(&cacheFileSDVPN, "cfsv", os.TempDir()+"/vpnsd.json", "SD cache file location")
+	tpCmd.Flags().StringVarP(&sdURL, "sdurl", "a", deployment.Prod.ServiceDiscovery, "service discovery url")
+	tpCmd.Flags().StringVarP(&utURL, "uturl", "w", deployment.Prod.UptimeTracker, "uptime tracker url")
 	tpCmd.Flags().StringVarP(&tpID, "id", "i", "", "display transport matching ID")
 	tpCmd.Flags().BoolVarP(&tpTypes, "tptypes", "u", false, "display transport types used by the local visor")
 	tpCmd.Flags().StringVar(&clirpc.Addr, "rpc", "localhost:3435", "RPC server address")
@@ -104,34 +114,190 @@ var tpCmd = &cobra.Command{
 		}
 		transports, err := rpcClient.Transports(filterTypes, pks, showLogs)
 		internal.Catch(cmd.Flags(), err)
+
+		if showMore {
+			utData = internal.GetData(cacheFileUT, utURL+"/uptimes?v=v2", cacheFilesAge)
+			proxyData = internal.GetData(cacheFileSDProxy, sdURL+"/api/services?type="+servicedisc.ServiceTypeProxy, cacheFilesAge)
+			vpnData = internal.GetData(cacheFileSDVPN, sdURL+"/api/services?type="+servicedisc.ServiceTypeVPN, cacheFilesAge)
+		}
+
 		PrintTransports(cmd.Flags(), transports...)
 	},
 }
 
+var utData string
+var vpnData string
+var proxyData string
+
+// PrintTransports prints transports used by the visor
+func PrintTransports(cmdFlags *pflag.FlagSet, tps ...*visor.TransportSummary) {
+	sortTransports(tps...)
+
+	var versionsByPK map[string]string
+	if showMore && len(utData) > 0 {
+		type uptimeEntry struct {
+			PK      string `json:"pk"`
+			Version string `json:"version"`
+		}
+		var utEntries []uptimeEntry
+		err := json.Unmarshal([]byte(utData), &utEntries)
+		internal.Catch(cmdFlags, err)
+
+		versionsByPK = make(map[string]string, len(utEntries))
+		for _, e := range utEntries {
+			versionsByPK[e.PK] = e.Version
+		}
+	}
+
+	type geoInfo struct {
+		Country string `json:"country"`
+	}
+
+	type serviceEntry struct {
+		Address string  `json:"address"`
+		Geo     geoInfo `json:"geo"`
+	}
+
+	proxyByPK := make(map[string]string)
+	vpnByPK := make(map[string]string)
+
+	if showMore && len(proxyData) > 0 {
+		var proxyEntries []serviceEntry
+		err := json.Unmarshal([]byte(proxyData), &proxyEntries)
+		internal.Catch(cmdFlags, err)
+		for _, e := range proxyEntries {
+			pk := strings.SplitN(e.Address, ":", 2)[0]
+			proxyByPK[pk] = e.Geo.Country
+		}
+	}
+
+	if showMore && len(vpnData) > 0 {
+		var vpnEntries []serviceEntry
+		err := json.Unmarshal([]byte(vpnData), &vpnEntries)
+		internal.Catch(cmdFlags, err)
+		for _, e := range vpnEntries {
+			pk := strings.SplitN(e.Address, ":", 2)[0]
+			vpnByPK[pk] = e.Geo.Country
+		}
+	}
+
+	var b bytes.Buffer
+	w := tabwriter.NewWriter(&b, 0, 0, 5, ' ', tabwriter.TabIndent)
+
+	if showMore {
+		_, err := fmt.Fprintln(w, "type\tid\tremote_pk\tmode\tlabel\tversion\tcountry\tservices")
+		internal.Catch(cmdFlags, err)
+	} else {
+		_, err := fmt.Fprintln(w, "type\tid\tremote_pk\tmode\tlabel")
+		internal.Catch(cmdFlags, err)
+	}
+
+	type outputTP struct {
+		Type     types.Type      `json:"type"`
+		ID       uuid.UUID       `json:"id"`
+		Remote   cipher.PubKey   `json:"remote_pk"`
+		TpMode   string          `json:"mode"`
+		Label    transport.Label `json:"label"`
+		Version  string          `json:"version,omitempty"`
+		Country  string          `json:"country,omitempty"`
+		Services string          `json:"services,omitempty"`
+	}
+
+	var outputTPS []outputTP
+	for _, tp := range tps {
+		if tp == nil {
+			continue
+		}
+		tpMode := "regular"
+		if tp.IsSetup {
+			tpMode = "setup"
+		}
+		tp.Log = nil
+
+		version := ""
+		if showMore && versionsByPK != nil {
+			version = versionsByPK[tp.Remote.String()]
+		}
+
+		var country, services string
+		pk := tp.Remote.String()
+		proxyCountry, inProxy := proxyByPK[pk]
+		vpnCountry, inVPN := vpnByPK[pk]
+
+		if inProxy && inVPN {
+			services = "proxy,vpn"
+			if proxyCountry == vpnCountry {
+				country = proxyCountry
+			} else {
+				country = proxyCountry + "/" + vpnCountry
+			}
+		} else if inProxy {
+			services = "proxy"
+			country = proxyCountry
+		} else if inVPN {
+			services = "vpn"
+			country = vpnCountry
+		}
+
+		oTP := outputTP{
+			Type:     tp.Type,
+			ID:       tp.ID,
+			Remote:   tp.Remote,
+			TpMode:   tpMode,
+			Label:    tp.Label,
+			Version:  version,
+			Country:  country,
+			Services: services,
+		}
+		outputTPS = append(outputTPS, oTP)
+
+		if showMore {
+			_, err := fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
+				tp.Type, tp.ID, tp.Remote, tpMode, tp.Label, version, country, services)
+			internal.Catch(cmdFlags, err)
+		} else {
+			_, err := fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\n",
+				tp.Type, tp.ID, tp.Remote, tpMode, tp.Label)
+			internal.Catch(cmdFlags, err)
+		}
+	}
+
+	internal.Catch(cmdFlags, w.Flush())
+	internal.PrintOutput(cmdFlags, outputTPS, b.String())
+}
+
+func sortTransports(tps ...*visor.TransportSummary) {
+	sort.Slice(tps, func(i, j int) bool {
+		return tps[i].ID.String() < tps[j].ID.String()
+	})
+}
+
 var (
-	sortedEdgeKeys []string
-	tpdURL         string
-	rootNode       string
-	lastNode       string
-	rootnode       cipher.PubKey
-	lastnode       cipher.PubKey
-	cacheFileTPD   string
-	cacheFileDmsgD string
-	cacheFileUT    string
-	padSpaces      int
-	isStats        bool
-	rawData        bool
-	refinedData    bool
-	noFilterOnline bool
-	onlyOnline     bool
-	transportType  string
-	timeout        time.Duration
-	rpk            string
-	cacheFileAR    string
-	cacheFilesAge  int
-	forceAttempt   bool
-	arURL          string
-	dmsgdURL       string
+	sortedEdgeKeys   []string
+	tpdURL           string
+	rootNode         string
+	lastNode         string
+	rootnode         cipher.PubKey
+	lastnode         cipher.PubKey
+	cacheFileTPD     string
+	cacheFileDmsgD   string
+	cacheFileUT      string
+	cacheFileSDProxy string
+	cacheFileSDVPN   string
+	padSpaces        int
+	isStats          bool
+	rawData          bool
+	refinedData      bool
+	noFilterOnline   bool
+	onlyOnline       bool
+	transportType    string
+	timeout          time.Duration
+	rpk              string
+	cacheFileAR      string
+	cacheFilesAge    int
+	forceAttempt     bool
+	arURL            string
+	dmsgdURL         string
 
 // queryHealth	bool
 )
@@ -309,56 +475,6 @@ var rmTpCmd = &cobra.Command{
 			internal.PrintOutput(cmd.Flags(), "", cmd.Help())
 		}
 	},
-}
-
-// PrintTransports prints transports used by the visor
-func PrintTransports(cmdFlags *pflag.FlagSet, tps ...*visor.TransportSummary) {
-	sortTransports(tps...)
-
-	var b bytes.Buffer
-	w := tabwriter.NewWriter(&b, 0, 0, 5, ' ', tabwriter.TabIndent)
-	_, err := fmt.Fprintln(w, "type\tid\tremote_pk\tmode\tlabel")
-	internal.Catch(cmdFlags, err)
-
-	type outputTP struct {
-		Type   types.Type      `json:"type"`
-		ID     uuid.UUID       `json:"id"`
-		Remote cipher.PubKey   `json:"remote_pk"`
-		TpMode string          `json:"mode"`
-		Label  transport.Label `json:"label"`
-	}
-
-	var outputTPS []outputTP
-	for _, tp := range tps {
-		if tp == nil {
-			continue
-		}
-		tpMode := "regular"
-		if tp.IsSetup {
-			tpMode = "setup"
-		}
-		tp.Log = nil
-		oTP := outputTP{
-			Type:   tp.Type,
-			ID:     tp.ID,
-			Remote: tp.Remote,
-			TpMode: tpMode,
-			Label:  tp.Label,
-		}
-		outputTPS = append(outputTPS, oTP)
-
-		_, err = fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\n", tp.Type, tp.ID, tp.Remote, tpMode, tp.Label)
-		internal.Catch(cmdFlags, err)
-
-	}
-	internal.Catch(cmdFlags, w.Flush())
-	internal.PrintOutput(cmdFlags, outputTPS, b.String())
-}
-
-func sortTransports(tps ...*visor.TransportSummary) {
-	sort.Slice(tps, func(i, j int) bool {
-		return tps[i].ID.String() < tps[j].ID.String()
-	})
 }
 
 var (
