f39ebd900cc7fa71c7c9c4eb5ea5eabd27210bbd
MohammadReza P. <mrpalide@gmail.com>
2025-10-02 20:47:21 +0000

    upgrade all go version to 1.25
    Former-commit-id: 5202a192785795db3ec6537fd72a23a30f00416e

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -13,7 +13,7 @@ platform="--platform=linux/amd64"
 registry="$REGISTRY"
 
 # shellcheck disable=SC2153
-base_image=golang:1.24-alpine
+base_image=golang:1.25-alpine
 
 if [[ "$#" != 2 ]]; then
   echo "docker_build.sh <IMAGE_TAG> <GO_BUILDOPTS>"
