6c94ec9de98e97ac78bd4ff58461fe395f857c2b
Moses Narrow <moe_narrow@use.startmail.com>
2025-11-30 18:11:23 -0600

    update env_test.go

--- internal/integration/env_test.go
+++ internal/integration/env_test.go
@@ -386,7 +386,7 @@ func (env *TestEnv) VisorStart(visor string) (string, error) {
 }
 
 func (env *TestEnv) VisorTpType(visor string) ([]tptypes.Type, error) {
-	cmd := fmt.Sprintf("/release/skywire cli --rpc %v:3435 tp type --json", visor)
+	cmd := fmt.Sprintf("/release/skywire cli --rpc %v:3435 tp --tptypes --json", visor)
 	cliOutput := struct {
 		Output []tptypes.Type `json:"output,omitempty"`
 		Err    *string        `json:"error,omitempty"`
@@ -402,7 +402,7 @@ func (env *TestEnv) VisorTpType(visor string) ([]tptypes.Type, error) {
 }
 
 func (env *TestEnv) VisorTpLs(visor string) ([]*skyvisor.TransportSummary, error) {
-	cmd := fmt.Sprintf("/release/skywire cli --rpc %v:3435 tp ls --json", visor)
+	cmd := fmt.Sprintf("/release/skywire cli --rpc %v:3435 tp --json", visor)
 	return env.visorTpExec(cmd)
 }
 
@@ -567,7 +567,7 @@ func (env *TestEnv) VisorGetTransportUUID(tp Transport) ([]*skyvisor.TransportSu
 	if len(env.visorPKs) == 0 {
 		env.GatherVisorPKs(env.visorNames)
 	}
-	cmd := fmt.Sprintf("/release/skywire cli --rpc %v:3435 tp ls --types %s --pks %s --json", tp.FromVisorHostName, tp.Type, env.visorPKs[tp.ToVisorHostName])
+	cmd := fmt.Sprintf("/release/skywire cli --rpc %v:3435 tp --types %s --pks %s --json", tp.FromVisorHostName, tp.Type, env.visorPKs[tp.ToVisorHostName])
 	out, err := env.visorTpExec(cmd)
 	if err != nil {
 		return nil, err
@@ -622,8 +622,18 @@ func (env *TestEnv) ExecJSON(cmd string, output interface{}) error {
 
 	result, err := env.execResult(cmd)
 	if err != nil {
-		env.logger.WithError(err).Errorf("[EXEC FAILED] %s", cmd)
+		env.logger.WithError(err).Errorf("[EXEC FAILED] %s\n", cmd)
+		if stdout := result.Stdout(); stdout != "" {
+			env.logger.Debugf("[STDOUT] %s", stdout)
+		}
+		if stderr := result.Stderr(); stderr != "" {
+			env.logger.Debugf("[STDERR] %s", stderr)
+		}
 		return err
+	}
+
+	if stdout := result.Stdout(); stdout != "" {
+		env.logger.Debugf("[STDOUT] %s", stdout)
 	}
 
 	// Show stderr (debug logs) if present
