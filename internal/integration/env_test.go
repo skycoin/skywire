b6592210c361e651c63fd741f78e1c3f537ed250
Moses Narrow <36607567+0pcom@users.noreply.github.com>
2026-03-03 18:30:22 -0600

    Fix gosec G104: add to nolint directive

--- internal/integration/env_test.go
+++ internal/integration/env_test.go
@@ -155,7 +155,7 @@ func (env *TestEnv) waitForHTTPEndpoint(t *testing.T, host string, port int, tim
 	for time.Now().Before(deadline) {
 		conn, err := net.DialTimeout("tcp", addr, 2*time.Second)
 		if err == nil {
-			conn.Close() //nolint:errcheck
+			conn.Close() //nolint:errcheck,gosec
 			env.logger.Infof("HTTP endpoint %s is ready", addr)
 			return
 		}
