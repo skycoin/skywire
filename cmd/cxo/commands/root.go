0349496f1a2159a4bb219a71b3008fab47ab2d56
Moses Narrow <36607567+0pcom@users.noreply.github.com>
2026-03-21 13:24:35 -0500

    Add ASCII font to CXO command

--- cmd/cxo/commands/root.go
+++ cmd/cxo/commands/root.go
@@ -5,13 +5,15 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+
+	"github.com/skycoin/skywire/pkg/skywire-utilities/pkg/calvin"
 )
 
 // RootCmd is the root cobra command for cxo
 var RootCmd = &cobra.Command{
 	Use:   "cxo",
 	Short: "CXO object distribution system",
-	Long:  "CXO is a P2P content-addressable object distribution system.",
+	Long:  calvin.AsciiFont("cxo") + "\nCXO is a P2P content-addressable object distribution system.",
 }
 
 func init() {
