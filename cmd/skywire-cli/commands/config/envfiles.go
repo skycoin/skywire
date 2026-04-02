5173d0e4785b1df51e8456fcfcc55eea17ca31e2
Moses Narrow <36607567+0pcom@users.noreply.github.com>
2026-04-02 13:33:47 -0500

    Add UPDATE_CHANNEL config, fix restart test timeouts
    - Add UPDATE_CHANNEL to config template (stable/develop/latest/<hash>)
    - Increase restart test routing rule expiry wait from 10s to 15s
    - Reduce SendSkyMessage HTTP timeout from 30s to 15s with 6 retries
      and exponential backoff (3s, 6s, 9s...) for faster failure cycling

--- cmd/skywire-cli/commands/config/envfiles.go
+++ cmd/skywire-cli/commands/config/envfiles.go
@@ -164,6 +164,17 @@ const envfileLinux = `#
 #--	Number of parallel mux routes per connection (default 0)
 #MUXROUTES=0
 
+### Auto-Update (skywire-autoupdate package) ############################
+#	These settings are only used by the skywire-update script
+#	installed by the skywire-autoupdate package.
+
+#--	Update channel:
+#	"stable"  = latest commit where all CI tests passed (default)
+#	"develop" = latest develop branch commit (may be untested)
+#	"latest"  = latest tagged release version
+#	"<hash>"  = pin to a specific commit hash
+#UPDATE_CHANNEL=stable
+
 ### Miscellaneous #######################################################
 
 #--	Set secret key
@@ -340,6 +351,16 @@ const envfileWindows = `#
 
 #--	Number of parallel mux routes per connection (default 0)
 #$MUXROUTES=0
+
+### Auto-Update (skywire-autoupdate package) ############################
+#	These settings are only used by the skywire-update script
+#	installed by the skywire-autoupdate package.
+
+#--	Update channel:
+#	"stable"  = latest commit where all CI tests passed (default)
+#	"develop" = latest develop branch commit (may be untested)
+#	"<hash>"  = pin to a specific commit hash
+#$UPDATE_CHANNEL='stable'
 
 ### Miscellaneous #######################################################
 
