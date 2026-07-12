7fbfdbd2f5dad98264186bf35ec036c85e28fa0e
MohammadReza P. <mrpalide@gmail.com>
2026-07-12 07:02:48 +0330

    make check

--- cmd/apps/exchange-market/commands/ui.go
+++ cmd/apps/exchange-market/commands/ui.go
@@ -32,7 +32,7 @@ func serveUI(ctx context.Context, appCl *app.Client, addr string) {
 		ReadHeaderTimeout: 5 * time.Second,
 	}
 
-	go func() {
+	go func() { //nolint
 		<-ctx.Done()
 		shutdownCtx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 		defer cancel()
