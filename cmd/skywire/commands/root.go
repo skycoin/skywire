57593ac2e49433019f42130174d518ebebe0cb22
MohammadReza P. <mrpalide@gmail.com>
2026-07-20 02:44:54 +0800

    fix gofmt

--- cmd/skywire/commands/root.go
+++ cmd/skywire/commands/root.go
@@ -7,11 +7,11 @@ import (
 
 	"github.com/spf13/cobra"
 
-	xc "github.com/skycoin/skywire/cmd/apps/skydex-client/commands"
-	xm "github.com/skycoin/skywire/cmd/apps/skydex-market/commands"
 	pty "github.com/skycoin/skywire/cmd/apps/pty/commands"
 	sc "github.com/skycoin/skywire/cmd/apps/skychat/commands"
 	scw "github.com/skycoin/skywire/cmd/apps/skycoin-web/commands" // registers skycoin-web as an internal launcher app + `skywire app skycoin web`
+	xc "github.com/skycoin/skywire/cmd/apps/skydex-client/commands"
+	xm "github.com/skycoin/skywire/cmd/apps/skydex-market/commands"
 	snc "github.com/skycoin/skywire/cmd/apps/skynet-client/commands"
 	sn "github.com/skycoin/skywire/cmd/apps/skynet/commands"
 	ssc "github.com/skycoin/skywire/cmd/apps/skysocks-client/commands"
