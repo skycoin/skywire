eaede7645cd243ae513f76422bb34b1757e9d707
Moses Narrow <36607567+0pcom@users.noreply.github.com>
2026-07-31 09:36:24 -0500

    fix: gofmt two skychat CLI files (#3595 rebase fallout — unbreaks the lint lane) (#3640)

--- cmd/skywire-cli/commands/skychat/group.go
+++ cmd/skywire-cli/commands/skychat/group.go
@@ -34,10 +34,10 @@ import (
 
 	"github.com/spf13/cobra"
 
-	skychatgroup "github.com/skycoin/skywire/pkg/skychat/group"
 	internal "github.com/skycoin/skywire/cmd/skywire-cli/cliutil"
 	clirpc "github.com/skycoin/skywire/cmd/skywire-cli/commands/rpc"
 	"github.com/skycoin/skywire/pkg/cipher"
+	skychatgroup "github.com/skycoin/skywire/pkg/skychat/group"
 	"github.com/skycoin/skywire/pkg/visor"
 	"github.com/skycoin/skywire/pkg/visor/rpcgrpc"
 )
--- cmd/skywire-cli/commands/skychat/tui_unified_io.go
+++ cmd/skywire-cli/commands/skychat/tui_unified_io.go
@@ -21,8 +21,8 @@ import (
 	"strings"
 	"time"
 
-	skychatgroup "github.com/skycoin/skywire/pkg/skychat/group"
 	clirpc "github.com/skycoin/skywire/cmd/skywire-cli/commands/rpc"
+	skychatgroup "github.com/skycoin/skywire/pkg/skychat/group"
 	"github.com/skycoin/skywire/pkg/visor"
 )
 
