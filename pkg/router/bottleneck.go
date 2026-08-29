1a5ca4af2a3f946af6c93a41d71e002253b71998
Moses Narrow <36607567+0pcom@users.noreply.github.com>
2026-08-29 09:23:26 -0500

    fix(lint): merge find var decl in bottleneck.go (staticcheck S1021)

--- pkg/router/bottleneck.go
+++ pkg/router/bottleneck.go
@@ -247,8 +247,7 @@ func groupLegsBySBD(stats []sbdStats) []int {
 	for i := range parent {
 		parent[i] = i
 	}
-	var find func(int) int
-	find = func(x int) int {
+	find := func(x int) int {
 		for parent[x] != x {
 			parent[x] = parent[parent[x]]
 			x = parent[x]
